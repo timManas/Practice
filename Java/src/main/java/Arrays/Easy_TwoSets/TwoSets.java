10e93458abcbe228e9caed1d0efe15c9f1f47810
TimManas <timothy.romero.manas@gmail.com>
2019-12-30 14:13:08 -0500

    Restructuring Code

---
+++ Java/src/main/java/Arrays/Easy_TwoSets/TwoSets.java
@@ -0,0 +1,65 @@
+package Arrays.Easy_TwoSets;
+
+import java.util.Arrays;
+
+public class TwoSets {
+
+    public static void main(String [] args) {
+        System.out.println("Hello World");
+        int [] nums = {3,3};
+//        int [] nums = {15, 7, 2, 11};
+        int target = 6;
+
+        // Create new Array which stores it in ascending order
+        int [] sortedArray =  nums.clone();
+        Arrays.sort(sortedArray);
+
+        for(int i = 0; i < sortedArray.length; i++) {
+            System.out.println(sortedArray[i]);
+        }
+
+        int index1 = -1;
+        int index2 = -1;
+
+        outerloop:
+        for (int x=0; x < sortedArray.length; x++) {
+            for (int y=1; y < sortedArray.length; y++) {
+                if (y >= target) {
+                    y++;
+                    continue;
+                }
+
+                if (x >= target) {
+                    x++;
+                    continue;
+                }
+
+                int total = sortedArray[x] + sortedArray[y];
+                if (total == target) {
+                    System.out.println("Target Hit: on " + x + " " + y);
+                    index1 = getOriginalIndex(nums, sortedArray[x]);
+                    index2 = getOriginalIndex(nums, sortedArray[y]);
+                    break outerloop;
+                }
+            }
+        }
+        int [] indexArray = new int[2];
+        indexArray[0] = index1;
+        indexArray[1] = index2;
+        Arrays.sort(indexArray);
+
+        System.out.println("Index: " + indexArray[0] + " " + indexArray[1]);
+
+
+    }
+
+    private static int getOriginalIndex(int[] nums, int value) {
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == value)
+                return i;
+        }
+        return -1;
+    }
+
+
+}
