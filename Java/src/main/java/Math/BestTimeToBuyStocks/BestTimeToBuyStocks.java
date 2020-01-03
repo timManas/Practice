be63066bc0abc5988ed2220356cf30ad3bb822c4
TimManas <timothy.romero.manas@gmail.com>
2020-01-03 13:31:18 -0500

    Refactored BestTimeToBuyStocks

--- Java/src/main/java/Math/BestTimeToBuyStocks/BestTimeToBuyStocks.java
+++ Java/src/main/java/Math/BestTimeToBuyStocks/BestTimeToBuyStocks.java
@@ -3,22 +3,43 @@ package Math.BestTimeToBuyStocks;
 public class BestTimeToBuyStocks {
 
     public static void main(String [] args) {
-//        int [] input = {7,1,5,3,6,4};
-        int [] input = {7,3,5,10,1,2};
+        int [] input = {7,1,5,3,6,4};
+//        int [] input = {7,3,5,100,1,2};
+//        int [] input = {3,7,5,10,1,2};
         System.out.println("Maxiumum Profit: " + maxProfit(input));
     }
 
 
     public static int maxProfit(int[] prices) {
 
-        int buy = -1;       // Buy at lowest
-        int sell = -1;      // Sell at highest
+        // Step #1 - Make sure there are elements in the
+        if (prices.length < 2)
+            return 0;
 
-
-        for (int i=0; i < prices.length; i++) {
+        // TODO :: Find the buy and sell Index
+        int buyIndex = 0;       // Buy has to be lower than Sell Index      BUY -> SELL
+        int sellIndex = 0;      // Sell HAS to be higher than BUY Index
+        int maxProfit = 0;
+        int localProfit;
+        int minPrice = prices[buyIndex];
         
+        for (int i=1; i < prices.length; i++) {
+            minPrice = Math.min(minPrice, prices[i]);
+            localProfit = prices[i] - Math.min(minPrice, prices[i]);
+            maxProfit = Math.max(maxProfit, localProfit);
         }
 
-        return 0;
+        return maxProfit;
     }
+
 }
+
+
+/**
+
+ Note:
+ LocalProfit: BuyIndex - CurrentIndex
+ NaxProfit: BuyIndex - SellIndex
+
+
+ */
