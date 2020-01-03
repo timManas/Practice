908877c3ea3eaf5ece35ca07774de96cf6d43db0
TimManas <timothy.romero.manas@gmail.com>
2020-01-03 11:19:16 -0500

    Adding blue print for new Algo

---
+++ Java/src/main/java/Math/BestTimeToBuyStocks/BestTimeToBuyStocks.java
@@ -0,0 +1,24 @@
+package Math.BestTimeToBuyStocks;
+
+public class BestTimeToBuyStocks {
+
+    public static void main(String [] args) {
+//        int [] input = {7,1,5,3,6,4};
+        int [] input = {7,3,5,10,1,2};
+        System.out.println("Maxiumum Profit: " + maxProfit(input));
+    }
+
+
+    public static int maxProfit(int[] prices) {
+
+        int buy = -1;       // Buy at lowest
+        int sell = -1;      // Sell at highest
+
+
+        for (int i=0; i < prices.length; i++) {
+        
+        }
+
+        return 0;
+    }
+}
