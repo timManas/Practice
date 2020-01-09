61f399c033513b833a80d1e59cca13b0948dede1
TimManas <timothy.romero.manas@gmail.com>
2020-01-09 00:02:10 -0500

    Finished MinStack

--- Java/src/main/java/Stacks/MinStack/MinStack.java
+++ Java/src/main/java/Stacks/MinStack/MinStack.java
@@ -1,27 +1,47 @@
 package Stacks.MinStack;
 
+import java.util.ArrayList;
+
 class MinStack {
 
-    /** initialize your data structure here. */
-    public MinStack() {
+    private ArrayList<Integer> stack;
+    private ArrayList<Integer> sortedList;
 
+    public MinStack() {
+        stack = new ArrayList<Integer>();
+        sortedList = new ArrayList<Integer>();
     }
 
     public void push(int x) {
-
+        stack.add(x);
     }
 
     public void pop() {
-
+        stack.remove(stack.size() - 1);
     }
 
     public int top() {
-
-        return -1;
+        int top = (!stack.isEmpty()) ? stack.get(stack.size() - 1) : 0;
+        return top;
     }
 
     public int getMin() {
-
-        return 0;
+        int min = !stack.isEmpty() ? stack.get(0) : 0;
+        for (int element : stack) {
+            if (min > element)
+                min = element;
+        }
+        return min;
     }
 }
+
+/**
+
+    Design a STACK !
+    -  Use an ArrayList which allows us to monitor the smallest value
+        > But ...it constantly needs to look at  the array again for the smallest element =( O(n) time
+    - Use an ArrayList with a TreeSet which monitors the element from smallest to largest
+        > But what if theres duplicates ?
+
+
+ */
--- Java/src/main/java/Stacks/MinStack/MinStackMain.java
+++ Java/src/main/java/Stacks/MinStack/MinStackMain.java
@@ -7,9 +7,9 @@ public class MinStackMain {
         minStack.push(-2);
         minStack.push(0);
         minStack.push(-3);
-        minStack.getMin();   //--> Returns -3.
+        System.out.println(minStack.getMin());   //--> Returns -3.
         minStack.pop();
-        minStack.top();      //--> Returns 0.
-        minStack.getMin();   //--> Returns -2.
+        System.out.println(minStack.top());      //--> Returns 0.
+        System.out.println(minStack.getMin());   //--> Returns -2.
     }
 }
