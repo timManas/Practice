f24cd4aba4dff78bec5ae74e19014bc070e61173
TimManas <timothy.romero.manas@gmail.com>
2020-01-18 16:54:21 -0500

    Finished Finding Subtree of Another Tree

--- Java/src/main/java/Trees/SubTreeOfAnotherTree/SubTreeOfAnotherTree.java
+++ Java/src/main/java/Trees/SubTreeOfAnotherTree/SubTreeOfAnotherTree.java
@@ -14,22 +14,22 @@ public class SubTreeOfAnotherTree {
         TreeNode treeNode5 = new TreeNode(5);
         TreeNode treeNode1 = new TreeNode(1);
         TreeNode treeNode2 = new TreeNode(2);
-//        TreeNode treeNode0 = new TreeNode(0);
+        TreeNode treeNode0 = new TreeNode(0);
 
         treeNode3.left = treeNode4;
         treeNode3.right = treeNode5;
         treeNode4.left = treeNode1;
         treeNode4.right = treeNode2;
-//        treeNode2.left = treeNode0;
+        treeNode2.left = treeNode0;
 
         TreeNode subTreeNode4 = new TreeNode(4);
         TreeNode subTreeNode1 = new TreeNode(1);
         TreeNode subTreeNode2 = new TreeNode(2);
-//        TreeNode subTreeNode11 = new TreeNode(1);
+        TreeNode subTreeNode11 = new TreeNode(1);
 
         subTreeNode4.left = subTreeNode1;
         subTreeNode4.right = subTreeNode2;
-//        subTreeNode2.left = subTreeNode11;
+        subTreeNode2.left = subTreeNode11;
 
 //        TreeNode treeNode3 = new TreeNode(0);
 //        TreeNode subTreeNode4 = new TreeNode(1);
@@ -40,22 +40,52 @@ public class SubTreeOfAnotherTree {
 
 
     // Working Solution
-    public static boolean isSubtree (TreeNode treeNode, TreeNode subTreeNode)
+    public static boolean isSubtree (TreeNode currentNode, TreeNode subTreeNode)
     {
-        boolean isSubTree = false;
+        return checkTree(currentNode, subTreeNode);
 
+    }
 
+    public static boolean checkTree(TreeNode currentNode, TreeNode subTreeNode) {
+        if (currentNode == null && subTreeNode != null)
+            return false;
+        else if (subTreeNode == null && currentNode != null)
+            return false;
+        else if (currentNode == null && subTreeNode == null)
+            return true;
 
+        // Step 1 - Check if they match
+        if (isMatch(currentNode, subTreeNode))
+            return true;
 
-
-        return isSubTree;
+        // Step 2 - Check if the Left or Right SubTree is a Match
+        return checkTree(currentNode.left, subTreeNode) || checkTree(currentNode.right, subTreeNode);
     }
 
 
+    private static boolean isMatch(TreeNode currentNode, TreeNode subTreeNode) {
 
+        if (currentNode == null && subTreeNode != null)
+            return false;
+        else if (subTreeNode == null && currentNode != null)
+            return false;
+        else if (currentNode == null && subTreeNode == null)
+            return true;
 
+        // If BOTH NOT NULL
 
+        // Step #1 - Check the value
+        if (currentNode.val != subTreeNode.val)
+            return false;
 
+        if (!isMatch(currentNode.left, subTreeNode.left))
+            return false;
+
+        if (!isMatch(currentNode.right, subTreeNode.right))
+            return false;
+
+        return true;
+    }
 
 
 }
