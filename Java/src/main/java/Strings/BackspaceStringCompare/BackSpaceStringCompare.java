7b016494447aa69a1a76fc208079667361b16280
TimManas <timothy.romero.manas@gmail.com>
2020-01-24 18:59:16 -0500

    Finished BackSpace String compare

--- Java/src/main/java/Strings/BackspaceStringCompare/BackSpaceStringCompare.java
+++ Java/src/main/java/Strings/BackspaceStringCompare/BackSpaceStringCompare.java
@@ -1,5 +1,7 @@
 package Strings.BackspaceStringCompare;
 
+import java.util.Stack;
+
 public class BackSpaceStringCompare {
 
     public static void main(String [] args) {
@@ -11,12 +13,41 @@ public class BackSpaceStringCompare {
     }
 
     public static boolean backspaceCompare(String S, String T) {
-        boolean isSame = false;
 
+        Stack<Character> stackS = fetchStackCompare(S);
+        Stack<Character> stackT = fetchStackCompare(T);
 
+        System.out.println("StackS: " + stackS.toString() + "      StackT: " + stackT.toString());
+        StringBuilder sbS = new StringBuilder();
+        StringBuilder sbT = new StringBuilder();
 
+        for (char letter : stackS)
+            sbS.append(letter);
 
-        return isSame;
+        for (char letter : stackT)
+            sbT.append(letter);
+
+        if (sbS.toString().equalsIgnoreCase(sbT.toString()))
+            return true;
+
+        return false;
+    }
+
+    private static Stack<Character> fetchStackCompare(String str) {
+
+        Stack<Character> stack = new Stack<Character>();
+
+        for (int i=0; i < str.length(); i++) {
+            char character = str.charAt(i);
+            if (character == '#') {
+                if (!stack.isEmpty())
+                    stack.pop();
+            } else {
+                stack.push(character);
+            }
+        }
+
+        return stack;
     }
 
 }
