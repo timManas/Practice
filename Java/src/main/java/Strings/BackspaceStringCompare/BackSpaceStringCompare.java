3501727770a0a4e78330326512be0559572517ef
TimManas <timothy.romero.manas@gmail.com>
2020-01-24 18:43:27 -0500

    Adding BackSpace string compare prototype

---
+++ Java/src/main/java/Strings/BackspaceStringCompare/BackSpaceStringCompare.java
@@ -0,0 +1,22 @@
+package Strings.BackspaceStringCompare;
+
+public class BackSpaceStringCompare {
+
+    public static void main(String [] args) {
+        String s = "ab#c";
+        String t = "ad#c";
+
+        System.out.println("IsSame: " + backspaceCompare(s, t));
+
+    }
+
+    public static boolean backspaceCompare(String S, String T) {
+        boolean isSame = false;
+
+
+
+
+        return isSame;
+    }
+
+}
