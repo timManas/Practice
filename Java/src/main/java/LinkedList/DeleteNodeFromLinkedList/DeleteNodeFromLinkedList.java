27599c64d938daba3f0c718a778d0e45a7a45695
TimManas <timothy.romero.manas@gmail.com>
2020-01-29 23:46:33 -0500

    Finished Delete Node from List

--- Java/src/main/java/LinkedList/DeleteNodeFromLinkedList/DeleteNodeFromLinkedList.java
+++ Java/src/main/java/LinkedList/DeleteNodeFromLinkedList/DeleteNodeFromLinkedList.java
@@ -14,7 +14,7 @@ public class DeleteNodeFromLinkedList {
         l2.next = l3;
         l3.next = l4;
 
-        deleteNode(l1);
+        deleteNode(l3);
 
         ListNode node = l1;
         while (node != null) {
@@ -25,13 +25,30 @@ public class DeleteNodeFromLinkedList {
 
     public static  void deleteNode(ListNode node) {
 
+        // Step 1 - For every value, we set the value of the next node to the current node
         while (node != null && node.next != null) {
             node.val = node.next.val;
-            node = node.next;
             System.out.println("CurrentVal: " + node.val);
-//            node = node.next;
+
+            // Step2 - If we reach the last node, we assign to null to avoid duplicates
+            if (node.next.next == null)
+                node.next = null;
+            else
+                node = node.next;
+
         }
 
-        node.next = null;
+
     }
 }
+
+
+/**
+ Note
+ - We are give the node to be deleted ...NOT THE HEADNODE
+
+ Solution:
+ - Since we are given the node to be "deleted"
+ - We set the current value of the "to be deleted node" to the next node
+ - The last node, we set to null to avoid duplicates
+ */
