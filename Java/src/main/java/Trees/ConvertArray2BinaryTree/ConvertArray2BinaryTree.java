cd8862334f414c8bb6cdcab4d07e23c63098c878
TimManas <timothy.romero.manas@gmail.com>
2020-02-13 01:04:04 -0500

    Finished Convert Array to Binary Tree

--- Java/src/main/java/Trees/ConvertArray2BinaryTree/ConvertArray2BinaryTree.java
+++ Java/src/main/java/Trees/ConvertArray2BinaryTree/ConvertArray2BinaryTree.java
@@ -10,12 +10,54 @@ public class ConvertArray2BinaryTree {
         if (node == null)
             return;
 
-        System.out.println(node.val + " -> ");
+        System.out.print(node.val + " -> ");
         printTree(node.left);
         printTree(node.right);
     }
 
     public static TreeNode sortedArrayToBST(int[] nums) {
-        return null;
+        if (nums.length == 0)
+            return null;
+
+        int middle = nums[(nums.length - 1) / 2];
+        TreeNode root = new TreeNode(middle);
+        root.left = createBST(nums, 0, (nums.length - 1) / 2);
+        root.right = createBST(nums, (nums.length + 1) / 2, nums.length);
+
+        return root;
+    }
+
+    private static TreeNode createBST(int[] nums, int startIndex, int endIndex) {
+
+        // If the Start and End Index are the same, We stop the loop
+        if (startIndex >= endIndex)
+            return null;
+
+        // Step2 - Fetch the middle Element
+        int middleIndex = (startIndex + endIndex) / 2;
+        int middle = nums[middleIndex];
+        System.out.println("MiddleIndex: " + middleIndex + "    MiddleElement:  " + middle);
+
+        // Step3 - Create the node
+        TreeNode newNode = new TreeNode(middle);
+
+        // Step4 - Traverse the left and Right Subtree
+        newNode.left = createBST(nums, startIndex, middleIndex);
+        newNode.right = createBST(nums, middleIndex + 1, endIndex);
+
+        return newNode;
     }
 }
+
+/**
+ Solution
+ Basically the idea is that
+ 1. We get the middle
+ 2. Create a node and assign value to the middle
+ 3. Traverse the left and assign it ot the node.left
+ 4. Traverse the Right and assign it to the node.right
+ 5. Return the root node
+
+
+
+ */
