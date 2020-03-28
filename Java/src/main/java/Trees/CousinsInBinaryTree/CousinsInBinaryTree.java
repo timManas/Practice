cd240909ba50412c1fc2fd498eecb81a73d687f3
TimManas <timothy.romero.manas@gmail.com>
2020-03-28 14:48:02 -0400

    Solution not working for Cousins in Binary Tree

--- Java/src/main/java/Trees/CousinsInBinaryTree/CousinsInBinaryTree.java
+++ Java/src/main/java/Trees/CousinsInBinaryTree/CousinsInBinaryTree.java
@@ -2,7 +2,7 @@ package Trees.CousinsInBinaryTree;
 
 public class CousinsInBinaryTree {
 
-    public static void main() {
+    public static void main(String [] args) {
         TreeNode treeNode1 = new TreeNode(1);
         TreeNode treeNode2 = new TreeNode(2);
         TreeNode treeNode3 = new TreeNode(3);
@@ -20,22 +20,65 @@ public class CousinsInBinaryTree {
         treeNode5.right = treeNode6;
 
         int x = 4;
-        int y = 5;
+        int y = 6;
         System.out.println("IsCousins: " + isCousins(treeNode1, x, y));
 
     }
 
     public static boolean isCousins(TreeNode node, int x, int y) {
 
-        int xLevel = 0;
-        int yLevel = 0;
+        int count = 0;
 
-        int xParentNodeVal = 0;
-        int yParentNodeVal = 0;
+        Integer xLevel = 0;
+        Integer yLevel = 0;
 
-        
+        Integer xParentNodeVal = 0;
+        Integer yParentNodeVal = 0;
 
-        return true;
+        traverseNode(node, x, y, count, xLevel, yLevel, xParentNodeVal, yParentNodeVal);
+        System.out.println("xParentNodeVal: " + xParentNodeVal
+                + "     xLevel: " + xLevel
+                + "     yParentNodeVal: " + yParentNodeVal
+                + "     yLevel: " + yLevel);
+        return xLevel == yLevel && xParentNodeVal == yParentNodeVal;
+    }
+
+    private static void traverseNode(TreeNode node, int x, int y, int count, Integer xLevel, Integer yLevel, Integer xParentNodeVal, Integer yParentNodeVal) {
+
+        if (node == null)
+            return;
+
+
+        if (node.left != null) {
+            if (node.left.val == x) {
+                xParentNodeVal = node.val;
+                xLevel = count + 1;
+                System.out.println("Found x: " + xParentNodeVal + "     Level: " + xLevel);
+            } else if (node.left.val == y) {
+                yParentNodeVal = node.val;
+                yLevel = count + 1;
+                System.out.println("Found y: " + yParentNodeVal + "     Level: " + yLevel);
+            }
+        }
+
+        if (node.right != null) {
+            if (node.right.val == x) {
+                xParentNodeVal = node.val;
+                xLevel = count + 1;
+                System.out.println("Found y: " + xParentNodeVal + "     Level: " + xLevel);
+            }
+            if (node.right.val == y) {
+                yParentNodeVal = node.val;
+                yLevel = count + 1;
+                System.out.println("Found y: " + yParentNodeVal + "     Level: " + yLevel);
+            }
+        }
+
+        traverseNode(node.left, x, y, count + 1, xLevel, yLevel, xParentNodeVal, yParentNodeVal);
+        traverseNode(node.right, x, y, count + 1, xLevel, yLevel, xParentNodeVal, yParentNodeVal);
+
+
+
     }
 
 }
