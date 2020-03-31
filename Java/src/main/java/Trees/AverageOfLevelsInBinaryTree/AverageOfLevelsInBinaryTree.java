135d716501604289fb78c5ce831a679aca240edd
TimManas <timothy.romero.manas@gmail.com>
2020-03-30 23:03:29 -0400

    Finished Average levels of binary tree

--- Java/src/main/java/Trees/AverageOfLevelsInBinaryTree/AverageOfLevelsInBinaryTree.java
+++ Java/src/main/java/Trees/AverageOfLevelsInBinaryTree/AverageOfLevelsInBinaryTree.java
@@ -1,10 +1,10 @@
 package Trees.AverageOfLevelsInBinaryTree;
 
-import sun.misc.Queue;
 
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 
 public class AverageOfLevelsInBinaryTree {
@@ -15,8 +15,7 @@ public class AverageOfLevelsInBinaryTree {
         TreeNode treeNode3 = new TreeNode(20);
         TreeNode treeNode4 = new TreeNode(15);
         TreeNode treeNode5 = new TreeNode(7);
-        TreeNode treeNode6 = new TreeNode(15);
-        TreeNode treeNode7 = new TreeNode(7);
+
 
         treeNode1.left = treeNode2;
         treeNode1.right = treeNode3;
@@ -24,28 +23,68 @@ public class AverageOfLevelsInBinaryTree {
         treeNode3.left = treeNode4;
         treeNode3.right = treeNode5;
 
-        treeNode4.left = treeNode6;
-        treeNode4.right = treeNode7;
 
         System.out.println("Avg of Levels: " + averageOfLevels(treeNode1));
     }
 
-    public static List<Double> averageOfLevels(TreeNode node) throws InterruptedException {
+    public static List<Double> averageOfLevels(TreeNode node) {
+
+        // Step1 - Create the list to be outputted
         List<Double> list = new ArrayList<>();
-        Queue<TreeNode> mainQueue = new Queue<>();
-        Queue<TreeNode> tempQueue = new Queue<>();
 
-        mainQueue.enqueue(node);
+        // Step2 - Create the mainQueue and temp Queue
+        // why ? Main Queue will store the parents and put the child nodes on temp Queue.
+        // Then later, tempQueue will be main Queue
+        Queue<TreeNode> mainQueue = new LinkedList<TreeNode>();
+        Queue<TreeNode> tempQueue = new LinkedList<TreeNode>();
 
-        while (!mainQueue.isEmpty()) {
+        // Step3 - Add the first Element
+        mainQueue.add(node);
 
-            TreeNode childNode = mainQueue.dequeue();
-            tempQueue.enqueue(childNode);
+        // Step4 - Traverse both Queues. Only stop when both queues are empty
+        while (!mainQueue.isEmpty() || !tempQueue.isEmpty()) {
+
+            // Step5 - Set the sum and size variables. Need this to calculate average to be put in list arraylist
+            double sum = 0;
+            int size = 0;
+
+            // Step6 - Traverse Main Queue
+            // Place child in Temp Queue
+            while (!mainQueue.isEmpty()) {
+                TreeNode childNode = mainQueue.remove();
+
+                // Step7 - Take the sum of all the mainQueues values
+                sum = sum + childNode.val;
+
+                // Step8 - Place in Temp Queue
+                if (childNode.left != null)
+                    tempQueue.add(childNode.left);
+                if (childNode.right != null)
+                    tempQueue.add(childNode.right);
+
+                ++size;
+            }
+
+            // Step 9 = Calculatet he average and place in arraylist 'list'
+            list.add(sum / size);
+
+
+            // Step10 - Convert tempQueue to main QUeue
+            while (!tempQueue.isEmpty())  {
+                mainQueue.add(tempQueue.remove());
+            }
         }
 
+        // Return list
         return list;
     }
-
-
-
 }
+
+/**
+ Solution
+ - You will need two queues. Main Queue and temp queue.
+ - The main queue will place child node into temp queue
+ - Once we finish the main queue, we calculate the average
+ - Then we make temp queue to tbe main queue
+ - We do this until we traverse every level using BFS
+ */
