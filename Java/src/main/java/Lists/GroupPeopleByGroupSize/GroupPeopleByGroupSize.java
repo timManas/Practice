51eab713c8d949344c277d93dbb57812145b28e8
TimManas <timothy.romero.manas@gmail.com>
2020-04-05 22:57:30 -0400

    Created Group people by group size

---
+++ Java/src/main/java/Lists/GroupPeopleByGroupSize/GroupPeopleByGroupSize.java
@@ -0,0 +1,24 @@
+package Lists.GroupPeopleByGroupSize;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class GroupPeopleByGroupSize {
+
+    public static void main(String [] args) {
+        int [] groupSizes = {3,3,3,3,3,1,3};
+        System.out.println("GroupSize: " + groupThePeople(groupSizes));
+    }
+
+    public static List<List<Integer>> groupThePeople(int[] groupSizes) {
+        List<List<Integer>> list = new ArrayList<>();
+        Map<Integer, Integer> map = new HashMap<>();
+
+        // Populate Map
+
+        return list;
+    }
+
+}
