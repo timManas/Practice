a205783ebe651f07df457f51229f604e2d5332ed
TimManas <timothy.romero.manas@gmail.com>
2020-04-16 19:24:57 -0400

    Created Reveal Cards in Increasing Order

---
+++ Java/src/main/java/Math/RevealCardsIncreasingOrder/RevealCardsIncreasingOrder.java
@@ -0,0 +1,19 @@
+package Math.RevealCardsIncreasingOrder;
+
+public class RevealCardsIncreasingOrder {
+
+    public static void main (String [] args) {
+        int [] deck = {17,13,11,2,3,5,7};
+        int [] result = deckRevealedIncreasing(deck);
+
+        for (int i : result)
+            System.out.println(i);
+    }
+
+    public static int[] deckRevealedIncreasing(int[] deck) {
+        int [] result = new int[deck.length];
+
+        return result;
+    }
+
+}
