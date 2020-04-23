78def98c844fdf6fa2272055710ca809e76be707
TimManas <timothy.romero.manas@gmail.com>
2020-04-22 20:29:15 -0400

    Finished Find Elements Contaiminated in BST

--- Java/src/main/java/Trees/FindElementsInContaminatedBT/FindElements.java
+++ Java/src/main/java/Trees/FindElementsInContaminatedBT/FindElements.java
@@ -1,14 +1,87 @@
 package Trees.FindElementsInContaminatedBT;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class FindElements {
 
+    TreeNode root;
+
+    // We will use BFS to traverse the array top to bottom
     public FindElements(TreeNode root) {
 
+        // Step 1 - Save a reference to the tree node
+        this.root = root;
+
+        // Step 2 - Create Queue for BFS
+        Queue<TreeNode> queue = new LinkedList<>();
+        root.val = 0;
+        queue.add(root);
+
+        // Step3 - Traverse Three iteratively
+        while (!queue.isEmpty()) {
+
+            // Step3 - Remove the head
+            TreeNode node = queue.remove();
+            int x = node.val;
+            System.out.println("Current Node: " + x);
+
+            // Step4 - Add the left child node
+            if (node.left != null) {
+
+                // Step5 - We update the child node left value
+                node.left.val = (2*x) + 1;
+                queue.add(node.left);
+            }
+
+            // Step4 - Add the right child node
+            if (node.right != null) {
+
+                // Step5 - Update the child node right value
+                node.right.val = (2*x) + 2;
+                queue.add(node.right);
+            }
+        }
+        System.out.println("Root: " + root.val);
     }
 
 
+
+    // Traverse using BFS again
+    // Why ? Because the tree created above is NOTTTT a BST. Hence we cant use DFS... ONLY BFS
     public boolean find(int target) {
+
+        // Step1 - Create queue
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+
+        // Traverse Tree
+        while (!queue.isEmpty()) {
+            TreeNode node = queue.remove();
+
+            // Step3 - Check if current node equals target
+            if (node.val == target)
+                return true;
+
+            // Step3 - Add left and right Node to queue
+            if (node.left != null)
+                queue.add(node.left);
+
+            if (node.right != null)
+                queue.add(node.right);
+
+        }
+
+
         return false;
     }
 
 }
+
+/**
+ Solution
+ 1. We use BFS to re-populate the Tree with the correct values
+ 2. We use BFS to find the values ... why ? Because the tree is NOT in a BST, hence we need to traverse from top to bottom
+ 3. We use a private instance of TreeNode root to access the tree for find(int target) method
+    why ? How else are we suppose to fetch the instance with only argument is "target" ???
+ */
--- Java/src/main/java/Trees/FindElementsInContaminatedBT/FindElementsInContaminatedBT.java
+++ Java/src/main/java/Trees/FindElementsInContaminatedBT/FindElementsInContaminatedBT.java
@@ -3,23 +3,24 @@ package Trees.FindElementsInContaminatedBT;
 public class FindElementsInContaminatedBT {
 
 
-    public static void main() {
+    public static void main(String [] args) {
 
-        TreeNode treeNode1 = new TreeNode(7);
-        TreeNode treeNode2 = new TreeNode(4);
-        TreeNode treeNode3 = new TreeNode(3);
-        TreeNode treeNode4 = new TreeNode(6);
-        TreeNode treeNode5 = new TreeNode(19);
+        TreeNode treeNode1 = new TreeNode(-1);
+        TreeNode treeNode2 = new TreeNode(-1);
+        TreeNode treeNode3 = new TreeNode(-1);
+        TreeNode treeNode4 = new TreeNode(-1);
+        TreeNode treeNode5 = new TreeNode(-1);
 
         treeNode1.left = treeNode2;
         treeNode1.right = treeNode3;
-        treeNode3.left = treeNode4;
-        treeNode3.right = treeNode5;
+        treeNode2.left = treeNode4;
+        treeNode2.right = treeNode5;
 
-        int target = 2;
+
+        int target = 10;
 
         FindElements obj = new FindElements(treeNode1);
-        boolean param_1 = obj.find(target);
+        System.out.println("Target: " + obj.find(target));
     }
 
 
