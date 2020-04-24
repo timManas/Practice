f11a6a3af25b53700b836e5cd8a4bc92a993e11c
TimManas <timothy.romero.manas@gmail.com>
2020-04-23 23:32:54 -0400

    Finished Delete leaves with Given values

--- Java/src/main/java/Trees/DeleteLeavesWithGivenValue/DeleteLeavesWithGivenValue.java
+++ Java/src/main/java/Trees/DeleteLeavesWithGivenValue/DeleteLeavesWithGivenValue.java
@@ -19,33 +19,65 @@ public class DeleteLeavesWithGivenValue {
         treeNode3.left = treeNode5;
         treeNode3.right = treeNode6;
 
-        int target = 3;
+        int target = 2;
 
         TreeNode result = removeLeafNodes(treeNode1, target);
         System.out.print("Result: " + result.val);
     }
 
     public static TreeNode removeLeafNodes(TreeNode root, int target) {
+
+        // Step1 - Traverse Tree
         traverseTree(root, target);
+
+        // StepX - Final Check to see if root is equal to target
+        if (root.val == target && root.left == null && root.right == null)
+            return null;
+
+
         return root;
     }
 
     // Traverse Tree using Post Order BFS since we want to go from Lead to Root (Bottom Up approach)
     private static void traverseTree(TreeNode node, int target) {
 
+        // Step1 - Check if node is null ...return and do nothing
         if (node == null)
             return;
 
+        // Step2 - Traverse left and right ...why ? Because we need to do post order traverasl
         traverseTree(node.left, target);
         traverseTree(node.right, target);
 
-        // If Leaf Node
-        if (node.left == null && node.right == null) {
+        // Step3 - Check the left node
+        // Remember ... we only delete the node is LEAF and Equal to target
+        if (node.left != null) {
 
-            if (node.val == target)
-                node = null;
-
+            // Check if the child is leaf
+            if (node.left.left == null &&
+                    node.left.right == null &&
+                    node.left.val == target)
+                node.left = null;
         }
 
+        // Step3b - Check the right node
+        // Remember ... we only delete thenode is LEAF and equal to target
+        if (node.right != null) {
+
+            // Check if the child is leaf
+            if (node.right.left == null &&
+                    node.right.right == null &&
+                    node.right.val == target)
+                node.right = null;
+
+        }
     }
 }
+
+
+/**
+ Solution
+ - We use DFS Post order traversal to traverse bottom to top
+ - If node is a LEAF && equal to target to remove the reference to that node
+ - Once we finish, we also need to check the root node if LEAF and equal to target ..remove that too
+ */
