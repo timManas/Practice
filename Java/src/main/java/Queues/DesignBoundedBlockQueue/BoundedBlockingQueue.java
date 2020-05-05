24651867fd833a7a8ddf526856456f1e7d7ee5e1
TimManas <timothy.romero.manas@gmail.com>
2020-05-04 21:16:20 -0400

    Finished Design Bounded Blocking queues

--- Java/src/main/java/Queues/DesignBoundedBlockQueue/BoundedBlockingQueue.java
+++ Java/src/main/java/Queues/DesignBoundedBlockQueue/BoundedBlockingQueue.java
@@ -1,19 +1,80 @@
 package Queues.DesignBoundedBlockQueue;
 
-public class BoundedBlockingQueue {
-    public BoundedBlockingQueue(int capacity) {
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.ReentrantLock;
 
+public class BoundedBlockingQueue {
+
+    private ReentrantLock lock = new ReentrantLock();
+    private Condition notFull = lock.newCondition();
+    private Condition notEmpty = lock.newCondition();
+
+    private Deque<Integer> queue;
+    private int maxCapaxity;
+
+    public BoundedBlockingQueue(int capacity) {
+        this.queue = new LinkedList<>();
+        this.maxCapaxity = capacity;
     }
 
+    /**
+     * We use lock/unlock to ensure only one thread happens at one time
+     *
+     */
     public void enqueue(int element) throws InterruptedException {
+        lock.lock();
+
+        try {
+            while (size() == maxCapaxity) {
+                notFull.await();
+            }
+            queue.add(element);
+            notEmpty.signalAll();
+
+        } finally {
+            lock.unlock();
+        }
 
     }
 
     public int dequeue() throws InterruptedException {
+        lock.lock();
+        int first;
+        try {
 
+            while (queue.size() == 0) {
+                notEmpty.await();
+            }
+            first = queue.removeFirst();
+            notFull.signalAll();
+
+
+        } finally {
+            lock.unlock();
+        }
+
+        return first;
     }
 
-    public int size() {
+    public int size(){
+        lock.lock();
 
+        try {
+            return queue.size();
+        } finally {
+            lock.unlock();
+        }
     }
 }
+
+/**
+ Solution
+ - Very very tricky ... Still did not solve it. Need alot of help on this one
+ - We have a Reentrant lock which has two conditions inside of it ...notFull & notEmpty
+    > When queue is empty, we block thread and add thread to empty waiting list
+    > When queue is full, we block thread and add thread to full waiting list
+    Why ??? So the order gets preserved of who gets to enqueue/dequeue when they are blocked
+
+ */
--- Java/src/main/java/Queues/DesignBoundedBlockQueue/DesignBoundedBlockQueue.java
+++ Java/src/main/java/Queues/DesignBoundedBlockQueue/DesignBoundedBlockQueue.java
@@ -2,19 +2,25 @@ package Queues.DesignBoundedBlockQueue;
 
 public class DesignBoundedBlockQueue {
 
-    public static void main(String [] args) {
+    public static void main(String [] args) throws InterruptedException {
         BoundedBlockingQueue queue = new BoundedBlockingQueue(2);   // initialize the queue with capacity = 2.
 
         queue.enqueue(1);   // The producer thread enqueues 1 to the queue.
-        queue.dequeue();    // The consumer thread calls dequeue and returns 1 from the queue.
-        queue.dequeue();    // Since the queue is empty, the consumer thread is blocked.
+        System.out.println(queue.dequeue());    // The consumer thread calls dequeue and returns 1 from the queue.
+        System.out.println(queue.dequeue());    // Since the queue is empty, the consumer thread is blocked.
         queue.enqueue(0);   // The producer thread enqueues 0 to the queue. The consumer thread is unblocked and returns 0 from the queue.
         queue.enqueue(2);   // The producer thread enqueues 2 to the queue.
         queue.enqueue(3);   // The producer thread enqueues 3 to the queue.
         queue.enqueue(4);   // The producer thread is blocked because the queue's capacity (2) is reached.
-        queue.dequeue();    // The consumer thread returns 2 from the queue. The producer thread is unblocked and enqueues 4 to the queue.
-        queue.size();       // 2 elements remaining in the queue. size() is always called at the end of each test case.
+        System.out.println(queue.dequeue());    // The consumer thread returns 2 from the queue. The producer thread is unblocked and enqueues 4 to the queue.
+        System.out.println(queue.size());       // 2 elements remaining in the queue. size() is always called at the end of each test case.
     }
-
-
 }
+
+/**
+ Note:
+ - This main class does nothing ... You would need two threads to test this
+ - In order to see it working, you need to go on Leetcode and run this.
+
+
+ */
