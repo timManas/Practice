e7b336f979e1e41ec3cf9c26591d8e6bcbf46d04
TimManas <timothy.romero.manas@gmail.com>
2020-05-14 23:49:14 -0400

    Finished Iterator Combination

--- Java/src/main/java/Strings/IteratorForCombination/CombinationIterator.java
+++ Java/src/main/java/Strings/IteratorForCombination/CombinationIterator.java
@@ -1,30 +1,29 @@
 package Strings.IteratorForCombination;
 
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.LinkedHashMap;
 import java.util.List;
 
 class CombinationIterator {
 
-    private List<Integer> indexList;
+    private List<Integer> referenceList;
     private String characters;
     private int maxLength;
     private StringBuilder sb;
 
+    // Initialize the Object
     public CombinationIterator(String characters, int combinationLength) {
-        this.indexList = new ArrayList<>();
+        this.referenceList = new ArrayList<>();
         this.characters = characters;
         this.maxLength = combinationLength;
         this.sb = new StringBuilder();
-
         initializeList();
     }
 
+    // Add the proper # of references ...aka maxLength of the combination
     private void initializeList() {
         int counter = 0;
         while (counter < maxLength) {
-            indexList.add(counter);
+            referenceList.add(counter);
             counter++;
         }
     }
@@ -32,25 +31,102 @@ class CombinationIterator {
 
     public String next() {
 
-        // Clear sb
+        // Step1 - Clear the StringBuilde
         sb.delete(0, sb.length());
 
-        for (int i=0; i < indexList.size(); i++) {
-            char letter = characters.charAt(indexList.get(i));
+        // Step2 - Fetch the character in the referenceList
+        for (int i=0; i < referenceList.size(); i++) {
+            char letter = characters.charAt(referenceList.get(i));
+
+            // Step3 - Append to String builder
             sb.append(letter);
         }
-        System.out.println(sb.toString());
 
-        incrementIndex();
+        // Step3 - Update the positions
+        updatePositions(referenceList.size() - 1);
 
+        // Step4 - Return the StringBuilders value
         return sb.toString();
     }
 
-    private void incrementIndex() {
+    // This is the trickiest part of the code
+    private int updatePositions(int reference) {
+
+        // Step1 - Check if our recursion went too FAR BACK
+        // Return some value
+        if (reference < 0)
+            return Integer.MAX_VALUE;
+
+        // Step2 - Incrememnt the current reference position
+        int position = referenceList.get(reference) + 1;
+
+        // Step3 - Keep Backtracking if the position is GREATER Or Equal to character length ..aka abcde
+        while (position >= characters.length()) {
+            position = updatePositions(reference - 1);
+
+            // Step3b - Stop the loop if reached toooo far back
+            // If we dont, we end up an infinite loop
+            if (position == Integer.MAX_VALUE)
+                break;
+        }
+
+        // Setp4 - Set the new position with their respective reference
+        referenceList.set(reference, position);
+
+        return position + 1;
     }
 
 
     public boolean hasNext() {
-        return false;
+
+        // Step1 - Fetch the first Element
+        int prevPosition = referenceList.get(0);
+
+        // Step2 - Compare prev element to every single element
+        // Update the prev to current
+        for (int i=1; i < referenceList.size(); i++) {
+
+            if (prevPosition >= referenceList.get(i) || prevPosition == Integer.MAX_VALUE)
+                return false;
+
+            // Update prev to current
+            prevPosition = referenceList.get(i);
+        }
+
+        // Step2 - Check if first value is crayyyyyyy
+        if (prevPosition == Integer.MAX_VALUE)
+            return false;
+
+        return true;
     }
 }
+
+
+/**
+ Note:
+ - Medium difficulty problem
+ - The hardest part for me was keep track of the positions when we update them
+    > Mainly due to the fact that if we reach the end of the string, we need to update the previous stored reference in the list
+    > Hence we use recursion
+    > Have to admit this was alot better than we first started doing this
+ - We use a recursion starting form last stored reference to the 1st
+ - The recursive method will return the position for the next reference stored in positionList
+ - We use an ArrayList to keep track of all the reference
+    why ? So it can be dynamic
+
+ Solution
+  - next()
+  1. Use an ArrayList to keep track of the positions
+    Remember: References and positions are different
+    lol what ? If you have combo of 3, then you have 3 references you need to keep track off
+    So whats is reference  ? It is a pointer to the positions of the characters
+    What is position ? Refers to the actual position ...aka which letter we are looking at
+        > All the references store a  position of letter
+
+  - hasNext()
+    1. We just check if the 1st index position is smaller than (index + 1) position
+    2. Continue till end and return true
+        > Return false if not
+
+
+ */
--- Java/src/main/java/Strings/IteratorForCombination/IteratorForCombination.java
+++ Java/src/main/java/Strings/IteratorForCombination/IteratorForCombination.java
@@ -3,13 +3,46 @@ package Strings.IteratorForCombination;
 public class IteratorForCombination {
 
     public static void main(String [] args) {
-        CombinationIterator iterator = new CombinationIterator("abc", 2); // creates the iterator.
-        System.out.println(iterator.next()); // returns "ab"
-        iterator.hasNext(); // returns true
-        System.out.println(iterator.next()); // returns "ac"
-        iterator.hasNext(); // returns true
-        System.out.println(iterator.next()); // returns "bc"
-        iterator.hasNext(); // returns false
+
+//        String characters = "abcde";
+//        int combo = 3;
+
+        String characters = "chp";
+        int combo = 1;
+
+        CombinationIterator iterator = new CombinationIterator(characters, combo); // creates the iterator.
+//        System.out.println(iterator.next()); // returns "ab"
+//        System.out.println(iterator.next()); // returns "ac"
+//        System.out.println(iterator.next()); // returns "bc"
+//        System.out.println(iterator.next()); // returns "bc"
+//        System.out.println(iterator.next()); // returns "bc"
+//        System.out.println(iterator.next()); // returns "bc"
+//        System.out.println(iterator.hasNext()); // returns true
+//
+//        System.out.println(iterator.next()); // returns "ab"
+//        System.out.println(iterator.next()); // returns "ac"
+//        System.out.println(iterator.next()); // returns "bc"
+//        System.out.println(iterator.next());
+//
+//        System.out.println(iterator.hasNext());
+
+        System.out.println(iterator.hasNext());
+        System.out.println(iterator.next());
+        System.out.println(iterator.hasNext());
+        System.out.println(iterator.hasNext());
+        System.out.println(iterator.next());
+        System.out.println(iterator.next());
+
+        System.out.println(iterator.hasNext());
+        System.out.println(iterator.hasNext());
+        System.out.println(iterator.hasNext());
+        System.out.println(iterator.hasNext());
+
     }
 
 }
+
+/**
+ Real Solution is on the other class
+ */
+
