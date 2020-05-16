10025e3ce3968cf61e0079d4cb0ea217195de76f
TimManas <timothy.romero.manas@gmail.com>
2020-05-16 00:18:10 -0400

    Started Pancake Sort

---
+++ Java/src/main/java/Sorting/PancakeSorting/PancakeSorting.java
@@ -0,0 +1,21 @@
+package Sorting.PancakeSorting;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+
+    public static void main(String [] args) {
+
+        int [] A = {3,2,4,1};
+        System.out.println("Pancake Sort: " + pancakeSort(A));
+    }
+
+    public static List<Integer> pancakeSort(int[] A) {
+        
+
+    }
+
+
+}
+
