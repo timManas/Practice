6d69edf8d571dead70a73998def1707a4fb706db
TimManas <timothy.romero.manas@gmail.com>
2020-05-17 17:23:41 -0400

    Saving work for LCA Deepest Leaf

--- Java/src/main/java/Trees/LowestCommonAncestorDeepestLeaves/LowestCommonAncestorDeepestLeaves.java
+++ Java/src/main/java/Trees/LowestCommonAncestorDeepestLeaves/LowestCommonAncestorDeepestLeaves.java
@@ -4,16 +4,17 @@ import java.util.concurrent.atomic.AtomicInteger;
 
 public class LowestCommonAncestorDeepestLeaves {
 
+    // Private Variables
+    private static TreeNode lcaNode;
+
     public static void main(String [] args) {
-        TreeNode treeNode1 = new TreeNode(6);
+        TreeNode treeNode1 = new TreeNode(1);
         TreeNode treeNode2 = new TreeNode(2);
-        TreeNode treeNode3 = new TreeNode(8);
-        TreeNode treeNode4 = new TreeNode(0);
-        TreeNode treeNode5 = new TreeNode(4);
-        TreeNode treeNode6 = new TreeNode(7);
-        TreeNode treeNode7 = new TreeNode(9);
-        TreeNode treeNode8 = new TreeNode(3);
-        TreeNode treeNode9 = new TreeNode(5);
+        TreeNode treeNode3 = new TreeNode(3);
+        TreeNode treeNode4 = new TreeNode(4);
+        TreeNode treeNode5 = new TreeNode(5);
+        TreeNode treeNode6 = new TreeNode(6);
+        TreeNode treeNode7 = new TreeNode(7);
 
         treeNode1.left = treeNode2;
         treeNode1.right = treeNode3;
@@ -24,43 +25,58 @@ public class LowestCommonAncestorDeepestLeaves {
         treeNode3.left = treeNode6;
         treeNode3.right = treeNode7;
 
-        treeNode5.left = treeNode8;
-        treeNode5.right = treeNode9;
-
         TreeNode deepestLeavesNode = lcaDeepestLeaves(treeNode1);
-        System.out.println("LCA of DeepestLeaves: " + deepestLeavesNode);
+        System.out.println("LCA of DeepestLeaves: " + deepestLeavesNode.val);
 
     }
 
     public static TreeNode lcaDeepestLeaves(TreeNode root) {
-        TreeNode lcaNode = null;
-        AtomicInteger count = new AtomicInteger(0);
+        lcaNode = root;
+        int count = 0;
         AtomicInteger deepestNode = new AtomicInteger(0);
-        traverseTree(root, lcaNode, count, deepestNode);
+        traverseTree(root, count, deepestNode);
 
         return lcaNode;
     }
 
-    private static int traverseTree(TreeNode node, TreeNode lcaNode, AtomicInteger count, AtomicInteger deepestNode) {
+    private static int traverseTree(TreeNode node, int count, AtomicInteger deepestNode) {
 
         if (node == null)
-            return count.get();
+            return count;
 
-        count.set(count.get() + 1);
-        int leftCount = traverseTree(node.left, lcaNode, count, deepestNode);
-        int rightCount = traverseTree(node.right, lcaNode, count, deepestNode);
-        
-        if (count.get() > deepestNode.get()) {
+        int leftCount = traverseTree(node.left, count + 1, deepestNode);
+        int rightCount = traverseTree(node.right, count + 1, deepestNode);
 
-            // Check parent of LeafNode
+        if (count  > deepestNode.get()) {
+
+            if (node.left == null && node.right == null) {  // Leaf
+                System.out.println("Leaf Hit: " + node.val);
 
 
+            } else if (node.left != null && node.right == null) {   // Parent: Left Null
+                if (node.left.left == null && node.left.right == null) // Left Child better be a leaf
+                    setDeepestParentNode(node.left, count, deepestNode);
 
+            } else if (node.left == null && node.right != null) {   // Parent: Right Null
+                if (node.right.left == null && node.right.right == null)   // Right Child better be a leaf
+                    setDeepestParentNode(node.right, count, deepestNode);
+
+
+            } else if (node.left != null && node.right != null) {   // Parent
+                if (node.left.left == null && node.left.right == null && node.right.left == null && node.right.right == null)
+                    setDeepestParentNode(node, count, deepestNode);
+
+            }
         }
 
+
         return Math.max(leftCount, rightCount);
+    }
 
-
+    private static void setDeepestParentNode(TreeNode node, int count, AtomicInteger deepestNode) {
+        lcaNode = node;
+        deepestNode.set(count);
+        System.out.println("DeepParentNode: " + lcaNode.val + "     DeepNodeLevel: " + deepestNode.get());
     }
 
 }
