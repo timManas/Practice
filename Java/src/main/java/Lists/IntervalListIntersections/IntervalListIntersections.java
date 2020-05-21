ed2ea3e01ed526e9a9e569d1e6fe18319e836a7c
TimManas <timothy.romero.manas@gmail.com>
2020-05-20 22:30:07 -0400

    Added comments

--- Java/src/main/java/Lists/IntervalListIntersections/IntervalListIntersections.java
+++ Java/src/main/java/Lists/IntervalListIntersections/IntervalListIntersections.java
@@ -23,25 +23,30 @@ public class IntervalListIntersections {
     }
 
     public static int[][] intervalIntersection(int[][] A, int[][] B) {
+        // Step1 - Create output variable
         List<List<Integer>> list = new ArrayList<>();
 
+        // Step2 - Check if both A & B sizes are bigger than 0. Otherwise no match
         if (A.length == 0 || B.length == 0)
             return new int [0][0];
 
+        // Step3 - Traverse both A & B
         int indexA = 0;
         int indexB = 0;
         ArrayList<Integer> intervalList = null;
         while (indexA < A.length && indexB < B.length) {
 
-            // Fetch intersection
+            // Step3a - Fetch intersection
             intervalList = fetchIntersection(A[indexA], B[indexB]);
 
-            // Add to main list
+            // Step3b - Add to main list
             System.out.println("List[" + indexA + "][" + indexB + "]: " + list);
             if (intervalList.size() > 0)
                 list.add(intervalList);
 
-            // Update Index
+            // Step3c - Update Index
+            // Index which has smaller range gets updated
+            // Why ? Because we want to update the smaller one. Thats it
             if (A[indexA][1] < B[indexB][1]) {
                 indexA++;
             } else if (A[indexA][1] > B[indexB][1]) {
@@ -55,10 +60,11 @@ public class IntervalListIntersections {
         }
         System.out.println("List[" + indexA + "][" + indexB + "]: " + list);
 
-        // Convert to MultiDimensional Array
+        // Step4- Convert to MultiDimensional Array
         return convertList2Arr(list);
     }
 
+    // Converts two dimensional list to two dimensional Array
     private static int[][] convertList2Arr(List<List<Integer>> list) {
         int [][] array = new int[list.size()][list.get(0).size()];
 
@@ -80,11 +86,13 @@ public class IntervalListIntersections {
         return array;
     }
 
+    // Fetches the intersection between two arrays
     private static ArrayList<Integer> fetchIntersection(int[] A, int[] B) {
         ArrayList<Integer> list;
         Set<Integer> setA = new TreeSet<>();
         Set<Integer> setB = new TreeSet<>();
 
+        // Convert to Sets
         for (int i = A[0]; i <= A[1]; i++) {
             setA.add(i);
         }
@@ -92,9 +100,24 @@ public class IntervalListIntersections {
             setB.add(i);
         }
 
+        // Finds the intersection
         setA.retainAll(setB);
 
         list = new ArrayList<>(setA);
         return list;
     }
 }
+
+
+/**
+ Note
+ - This code only passes 76/86 Test cases
+
+
+ Solution
+ 1. Traverse both A & B at the same time using index
+ 2. Find the intersection between A and B for its respective index
+ 3. Update indexes - Whichever has smaller range, we increment it's index
+ 4. Add to Arraylist
+ 5. Convert ArrayList to Multi-Dim Array
+ */
