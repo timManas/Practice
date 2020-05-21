c81dc1dee5ad6f58a15b90a507d2bf60ea083a8f
TimManas <timothy.romero.manas@gmail.com>
2020-05-20 20:59:16 -0400

    Made some progress on Interval List Intersections

--- Java/src/main/java/Lists/IntervalListIntersections/IntervalListIntersections.java
+++ Java/src/main/java/Lists/IntervalListIntersections/IntervalListIntersections.java
@@ -7,8 +7,8 @@ public class IntervalListIntersections {
 
     public static void main(String [] args) {
 
-        int [][] A = {};
-        int [][] B = {};
+        int [][] A = {{0,2},{5,10},{13,23},{24,25}};
+        int [][] B = {{1,5},{8,12},{15,24},{25,26}};
         int [][] intersection = intervalIntersection(A, B);
         for (int i=0; i < intersection.length; i++) {
             for (int j=0; j < intersection[i].length; j++) {
@@ -21,7 +21,32 @@ public class IntervalListIntersections {
 
     public static int[][] intervalIntersection(int[][] A, int[][] B) {
         List<List<Integer>> list = new ArrayList<>();
-        
-        return (int[][]) list.toArray();
+
+        int indexA = 0;
+        int indexB = 0;
+        while (indexA < A.length - 1 || indexB < B.length - 1) {
+            ArrayList<Integer> intervalList = fetchIntersection(A[indexA], B[indexB]);
+
+            if (A[indexA][1] <= B[indexB][1])
+                indexA++;
+            else if (A[indexA][1] > B[indexB][1])
+                indexB++;
+
+            list.add(intervalList);
+        }
+
+        return convertList2Arr(list);
+    }
+
+    private static int[][] convertList2Arr(List<List<Integer>> list) {
+        int [][] array = new int[list.size()][list.get(0).size()];
+
+        return array;
+    }
+
+    private static ArrayList<Integer> fetchIntersection(int[] ints, int[] ints1) {
+        ArrayList<Integer> list = new ArrayList<>();
+
+        return list;
     }
 }
