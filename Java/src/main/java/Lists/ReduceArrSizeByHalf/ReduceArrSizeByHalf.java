2a8801264a8a0d04748fc37b00bb2efcb3e04090
TimManas <timothy.romero.manas@gmail.com>
2020-05-23 20:27:37 -0400

    Starte Reduce Array Size by Half

---
+++ Java/src/main/java/Lists/ReduceArrSizeByHalf/ReduceArrSizeByHalf.java
@@ -0,0 +1,20 @@
+package Lists.ReduceArrSizeByHalf;
+
+public class ReduceArrSizeByHalf {
+
+
+    public static void main(String [] args) {
+        int [] arr = {3,3,3,3,5,5,5,2,2,7};
+//        int [] arr = {7,7,7,7,7,7};
+//        int [] arr = {1,3};
+//        int [] arr = {1,2,3,4,5,6,7,8,9};
+
+        System.out.println("MinSetSize: " + minSetSize(arr));
+    }
+
+    public static int minSetSize(int[] arr) {
+        int size = 0;
+
+        return size;
+    }
+}
