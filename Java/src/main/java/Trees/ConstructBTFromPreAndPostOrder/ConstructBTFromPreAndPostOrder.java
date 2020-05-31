6bfd37302f78cf9aa7d8bf73266040814cd72299
TimManas <timothy.romero.manas@gmail.com>
2020-05-31 18:58:14 -0400

    Started contructed BST from preorder to post order

---
+++ Java/src/main/java/Trees/ConstructBTFromPreAndPostOrder/ConstructBTFromPreAndPostOrder.java
@@ -0,0 +1,17 @@
+package Trees.ConstructBTFromPreAndPostOrder;
+
+public class ConstructBTFromPreAndPostOrder {
+
+    public static void main(String [] args) {
+        int [] pre = {1,2,4,5,3,6,7};
+        int [] post = {4,5,2,6,7,3,1};
+
+        System.out.println("Root Node: " + constructFromPrePost(pre, post));
+    }
+
+    public static TreeNode constructFromPrePost(int[] pre, int[] post) {
+        return  null;
+    }
+
+
+}
---
+++ Java/src/main/java/Trees/ConstructBTFromPreAndPostOrder/TreeNode.java
@@ -0,0 +1,8 @@
+package Trees.ConstructBTFromPreAndPostOrder;
+
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) { val = x; }
+}
