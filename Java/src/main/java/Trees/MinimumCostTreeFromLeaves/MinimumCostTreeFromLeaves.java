a87afb3ebc754934dbacd524e9ca46aacb05bf65
TimManas <timothy.romero.manas@gmail.com>
2020-06-04 22:34:28 -0400

    Made some progress on Min cost Tree of leaves

--- Java/src/main/java/Trees/MinimumCostTreeFromLeaves/MinimumCostTreeFromLeaves.java
+++ Java/src/main/java/Trees/MinimumCostTreeFromLeaves/MinimumCostTreeFromLeaves.java
@@ -7,36 +7,46 @@ import java.util.List;
 public class MinimumCostTreeFromLeaves {
 
     public static void main(String [] args) {
-        int [] arr = {6,2,4};
+//        int [] arr = {6,2,4,9};
+        int [] arr = {1,2,3,4};
         System.out.println("Minimum Cost Tree: " +  mctFromLeafValues(arr));
     }
 
+    /**
+     * Hold the fuck up... Currently you are Using Iteration
+     * Use recursion
+     */
     public static int mctFromLeafValues(int[] arr) {
         if (arr.length == 0)
             return 0;
-
         List<Integer> costList = new ArrayList<>();
-        List<Integer> treeNodeHolder = new ArrayList<>();
+        List<Integer> nodeList = new ArrayList<>();
+        List<Integer> list = new ArrayList<>();
+        for (int i : arr)
+            list.add(i);
 
-        int max = 2;                // Max # of iterations
-        if (arr.length % 2 == 0)
-            max = 1;
-
-        int count = 0;
-        while (count < max ) {
-            int min = 0;
-
-            for (int i=0; i<arr.length; i++) {
-
-                
-
-            }
-
-        }
+        System.out.println("Max: " + createTree(list, nodeList));
 
         // Sort to get the smallest cost
         Collections.sort(costList);
-        return costList.get(0);
+        return 0;
+    }
+
+    private static int createTree(List<Integer> list, List<Integer> nodeList) {
+
+        if (list.size() == 0)
+            return 0;
+
+        if (list.size() == 1)
+            return list.get(0);
+
+
+        int left = createTree(list.subList(0, (list.size() / 2)), nodeList);
+        int right = createTree(list.subList((list.size() / 2), list.size()), nodeList);
+
+        nodeList.add(left * right);
+
+        return Math.max(left, right);
     }
 
 }
