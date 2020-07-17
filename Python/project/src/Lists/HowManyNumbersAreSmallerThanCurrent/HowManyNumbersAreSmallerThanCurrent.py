2966c51fbc2215a64d9de805340220c0efc54c29
TimManas <timothy.romero.manas@gmail.com>
2020-07-16 22:15:25 -0400

    Started to hit me

---
+++ Python/project/src/Lists/HowManyNumbersAreSmallerThanCurrent/HowManyNumbersAreSmallerThanCurrent.py
@@ -0,0 +1,25 @@
+def main():
+
+    nums = [8,1,2,2,3]
+    solution = Solution()
+    output = solution.smallerNumbersThanCurrent(nums)
+    print("# Nums: ", output)
+
+
+class Solution(object):
+    def smallerNumbersThanCurrent(self, nums):
+        print("Starting Find # of Smaller Numbers")
+        output = []
+        sortedNums = sorted(nums)
+        print("Sorted: ", sortedNums)
+        print("UnSorted: ", nums)
+
+        for i in range():
+            print(i)
+
+
+        return output
+
+
+if __name__ == '__main__':
+    main()
