5a0b6fef7b0ea394c8b61dd26fec3c10b6209123
TimManas <timothy.romero.manas@gmail.com>
2020-07-19 20:40:30 -0400

    Finished How many # are smaller

--- Python/project/src/Lists/HowManyNumbersAreSmallerThanCurrent/HowManyNumbersAreSmallerThanCurrent.py
+++ Python/project/src/Lists/HowManyNumbersAreSmallerThanCurrent/HowManyNumbersAreSmallerThanCurrent.py
@@ -9,17 +9,36 @@ def main():
 class Solution(object):
     def smallerNumbersThanCurrent(self, nums):
         print("Starting Find # of Smaller Numbers")
+
+        # Step1: Create output variable to store # of smaller numbers
         output = []
+
+        # Step2: Sort the array
         sortedNums = sorted(nums)
         print("Sorted: ", sortedNums)
         print("UnSorted: ", nums)
 
-        for i in range():
-            print(i)
+        # Step3: Travere the input list
+        for i in range(nums.__len__()):
 
+            # Step4: Find the Index of the sorted list and place them in output list
+            print(sortedNums.index(nums[i]))
+            output.append(sortedNums.index(nums[i]))
 
+        # Step4: Return the output
         return output
 
 
 if __name__ == '__main__':
     main()
+
+'''
+Solution
+1. Sort the input list
+    > Why ? Because the sorted list will contain how many Elements are Smaller !!!
+2. Traverse the input list
+3. For each value in the inputlist, we find the element in the sortted list and find its index
+4. Place the index in the output list =)
+
+
+'''
