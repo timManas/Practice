c4aac8ad332a41015fc9eb4c9f9449a74934cb95
TimManas <timothy.romero.manas@gmail.com>
2020-07-26 18:26:52 -0400

    Finished Split String In Balanced String

--- Python/project/src/Strings/SplitStringInBalancedString/SplitStringInBalancedString.py
+++ Python/project/src/Strings/SplitStringInBalancedString/SplitStringInBalancedString.py
@@ -8,28 +8,57 @@ def main():
 
 class Solution(object):
     def balancedStringSplit(self, s):
-        output = 0
 
-        # Create Stack
-        stack = []
-        subString = ""
 
+        # Step1 - Create Stack
+        stackL = []
+        stackR = []
+
+        # Step2 - Create list to store all Balanced String of L & R
+        subString = []
+        currentString = ""
+
+        # Step3 - Traverse the string s
         for letter in s:
             print("Letter: " + letter)
 
-            if stack.__len__() == 0:
-                stack.append(letter)
-                continue
-
-            if stack[stack.__len__() - 1] == letter:
-                stack.append(letter)
-
+            # Step4 - Add Letter to respective stack
+            if letter == 'L':
+                stackL.append(letter)
             else:
-                stack.pop(stack.__len__() - 1)
-                print("Current Stack: ", stack)
+                stackR.append(letter)
 
-        return output
+            # Step5- Add letter to current String.
+            # Why do we need this ? Because we want to keep track of the FullString
+            currentString += letter
+
+            # Step6 - Check if both stacks have the same size.
+            # Reset both stacks and add currentString to list of SubString
+            if (stackL.__len__() == stackR.__len__()):
+                print("Stacks are Same Size .. now Empty Both Stack | CurrentString: " + currentString)
+                subString.append(currentString)
+                stackL.clear()
+                stackR.clear()
+                currentString = ""
+
+        print("List of All SubString: ", subString)
+
+        return subString.__len__()
 
 
 if __name__ == '__main__':
     main()
+
+
+'''
+Notes
+- Tricky but not difficult
+- Involves a bit of thinking first
+
+Solution
+1. Create two stacks to store L and R seperately
+2. Use a string to keep track of the letters
+3. If both stacks have the same size, we clear and reset all the parameters and Add them to list of SubString array
+4. Repeat until we go through entire String S
+
+'''
