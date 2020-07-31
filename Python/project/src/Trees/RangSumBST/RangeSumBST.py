13727a9b31097476cd22a82409769837310731c6
TimManas <timothy.romero.manas@gmail.com>
2020-07-31 17:14:40 -0400

    Started Range Sum of BST

---
+++ Python/project/src/Trees/RangSumBST/RangeSumBST.py
@@ -0,0 +1,24 @@
+sys.path.append(".")
+from TreeNode-RangeSumBST.py import TreeNode
+
+def main():
+    # root = [10, 5, 15, 3, 7, null, 18]
+    TreeNode-RangeSumBST
+    L = 7
+    R = 15
+    solution = Solution()
+    output = solution.rangeSumBST(root, L, R)
+    print("Range Sum of BST: " + str(output))
+
+
+
+class Solution(object):
+    def rangeSumBST(self, root, L, R):
+        output = traverse
+
+
+        return output
+
+
+if __name__ == '__main__':
+    main()
