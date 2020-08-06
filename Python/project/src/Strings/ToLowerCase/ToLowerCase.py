58f2c013a0ee9f8eae12774749179d6eec13d390
TimManas <timothy.romero.manas@gmail.com>
2020-08-06 18:36:02 -0400

    Reworked the To Lower case()

--- Python/project/src/Strings/ToLowerCase/ToLowerCase.py
+++ Python/project/src/Strings/ToLowerCase/ToLowerCase.py
@@ -6,9 +6,13 @@ class Solution:
         # Step2 - Traverse String
         for letter in str:
 
+            if letter.islower() or not letter.isalpha() :
+                output += letter
+                continue
+
             # Step3 - Convert each letter to char and add 32
-            number = ord(letter) + 32
-            upper = chr(number)
+            number = ord(letter) + 32       # Conver to UnitCode
+            upper = chr(number)             # Convert from Unicode to char
 
             # Step4 - Append the lower case letter to the output variable
             output += upper
@@ -31,11 +35,14 @@ if __name__ == '__main__':
 '''
 Notes
 - Not tricky
+- ord() - Given a string representing one Unicode character, return an integer representing the Unicode code point of that character.
 
 Solution
 1. Traverse the string 
 2. For each letter, convert to char and add 32 ...why ? because the ascii chart, if we add 32 we convert the char to lowercase
 3. Append the char to the output variable in lowercase
 4. Return output variable
+
+
 
 '''
