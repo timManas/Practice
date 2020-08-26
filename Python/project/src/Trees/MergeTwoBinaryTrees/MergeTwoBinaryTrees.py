cd63855e4eaadb7becd808a16b55f615cc59901e
TimManas <timothy.romero.manas@gmail.com>
2020-08-25 21:50:42 -0400

    Partial Pass - Merge two binary treees

--- Python/project/src/Trees/MergeTwoBinaryTrees/MergeTwoBinaryTrees.py
+++ Python/project/src/Trees/MergeTwoBinaryTrees/MergeTwoBinaryTrees.py
@@ -1,44 +1,59 @@
 from TreeNodeObject import TreeNode
+from collections import deque
 
 class Solution(object):
     def mergeTrees(self, treeNode1, treeNode2):
 
-        outputNode = None
+        if treeNode1 == None and treeNode2 == None:
+            return
+        elif treeNode1 == None and treeNode2 != None:
+            mergedTree = TreeNode(treeNode2.val)
+        elif treeNode1 != None and treeNode2 == None:
+            mergedTree = TreeNode(treeNode1.val)
+        else:
+            mergedTree = TreeNode(treeNode1.val + treeNode2.val)
+
 
         # Create Queues for Tree
-        queue1 = []
-        queue2 = []
+        queue1 = deque()
+        queue2 = deque()
+        queueMain = deque()
 
-        head1 = TreeNode()
-        head1.left = treeNode1
-
-        head2 = TreeNode()
-        head2.left = treeNode2
-
-        queue1.append(head1)
-        queue2.append(head2)
+        queue1.append(treeNode1)
+        queue2.append(treeNode2)
+        queueMain.append(mergedTree)
 
         while queue1.__len__() > 0 or queue2.__len__() > 0:
-            print("Queue1: ", queue1,  "    Queue2: ", queue2)
 
-            node1 = queue1.pop() if queue1.__len__() > 0 else None
+            node1 = queue1.popleft() if queue1.__len__() > 0 else None
             node1Left = node1.left.val if node1 != None and node1.left != None else 0
             node1Right = node1.right.val if node1 != None and node1.right != None else 0
 
-            node2 = queue2.pop() if queue2.__len__() > 0 else None
+            node2 = queue2.popleft() if queue2.__len__() > 0 else None
             node2Left = node2.left.val if node2 != None and node2.left != None else 0
             node2Right = node2.right.val if node2 != None and node2.right != None else 0
 
+            if queueMain.__len__() > 0:
+                nodeMain = queueMain.popleft()
+            else:
+                nodeMain = TreeNode()
+                mergedTree = nodeMain
+
+
+
             print("Node1: ", node1, "    Node2: ", node2)
 
-            # if outputNode == None:
-            newNode = TreeNode()
 
-            newNode.left = TreeNode(node1Left + node2Left)
-            newNode.right = TreeNode(node1Right + node2Right)
+            # Create child Nodes
+            if (node1Left + node2Left) > 0:
+                nodeMain.left = TreeNode(node1Left + node2Left)
+                queueMain.append(nodeMain.left)
+
+            if (node1Right + node2Right) > 0:
+                nodeMain.right = TreeNode(node1Right + node2Right)
+                queueMain.append(nodeMain.right)
 
             # Add Children to Node
-
             if node1 != None:
                 if node1.left != None:
                     queue1.append(node1.left)
@@ -51,39 +66,43 @@ class Solution(object):
                 if node2.right != None:
                     queue2.append(node2.right)
 
-            outputNode = newNode
+            print("QueMain: ", nodeMain.val, "\n")
 
-        print("Output: ", outputNode)
+        print("Output: ", mergedTree)
 
+        return mergedTree
 
 
 
 def main():
-    t11 = TreeNode(1)
-    t12 = TreeNode(3)
-    t13 = TreeNode(2)
-    t14 = TreeNode(5)
+    # t11 = TreeNode(1)
+    # t12 = TreeNode(3)
+    # t13 = TreeNode(2)
+    # t14 = TreeNode(5)
+    #
+    # t11.left = t12
+    # t11.right = t13
+    # t12.left = t14
+    #
+    # t21 = TreeNode(2)
+    # t22 = TreeNode(1)
+    # t23 = TreeNode(3)
+    # t24 = TreeNode(4)
+    # t25 = TreeNode(7)
+    #
+    # t21.left = t22
+    # t21.right = t23
+    # t22.right = t24
+    # t23.right = t25
 
-    t11.left = t12
-    t11.right = t13
-    t12.left = t14
-
-    t21 = TreeNode(2)
-    t22 = TreeNode(1)
-    t23 = TreeNode(3)
-    t24 = TreeNode(4)
-    t25 = TreeNode(7)
-
-    t21.left = t22
-    t21.right = t23
-    t22.right = t24
-    t23.right = t25
+    t11 = TreeNode()
+    t21 = TreeNode()
 
 
 
     solution = Solution()
     output = solution.mergeTrees(t11, t21)
-    print("MergedTrees: ", output)
+    print("MergedTrees: ", output.val)
 
 
 if __name__ == '__main__':
