812080549896478446bf4de6ef412579ec713b71
TimManas <timothy.romero.manas@gmail.com>
2020-10-01 18:38:37 -0400

    Started Find Common Characters

---
+++ Python/project/src/Lists/FindCommonCharacters/FindCommonCharacters.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def commonChars(self, A):
+        
+
+def main():
+    A = ["bella","label","roller"]
+    solution = Solution()
+    output = solution.commonChars(A)
+    print("Common Characters: ", output)
+
+
+if __name__ == '__main__':
+    main()
