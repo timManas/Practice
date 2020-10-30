cbfc33043da70b0bde60fe362dd56ade0f20802c
TimManas <timothy.romero.manas@gmail.com>
2020-10-29 20:04:53 -0400

    Still working on Pivot Index

--- Python/project/src/Lists/FindPivotIndex/FindPivotIndex.py
+++ Python/project/src/Lists/FindPivotIndex/FindPivotIndex.py
@@ -1,6 +1,8 @@
 class Solution(object):
     def pivotIndex(self, nums):
 
+        totalSum = 0
+
         leftSum = 0
         rightSum = 0
 
@@ -10,37 +12,37 @@ class Solution(object):
         if nums.__len__() <= 2:
             return -1
 
+        # Find the total
+        # Need this to calculate the Right Sum
+        i = 0
+        while i < nums.__len__():
+            totalSum += nums[i]
+            i += 1
+
         while leftIndex < rightIndex:
+            left = nums[leftIndex]
+            leftSum += left
 
-            if leftSum == rightSum and leftSum != 0 and rightSum != 0:
-
-                if leftIndex + 1 >= rightIndex:
-                    return -1
-
-                return leftIndex + 1
-
-            elif nums[leftIndex] == 0:
-                leftIndex += 1
-
-            elif nums[rightIndex] == 0:
-                rightIndex -= 1
-
-            elif leftSum < rightSum:
-                leftSum += nums[leftIndex]
-                leftIndex += 1
-
-            elif leftSum > rightSum:
-                rightSum += nums[rightIndex]
-                rightIndex -= 1
+            right = nums[rightIndex]
+            rightSum += right
 
             print("Left Sum: ", leftSum, "      Right Sum: ", rightSum)
+
+            if leftSum == (totalSum - rightSum):
+                return leftIndex + 1
+
+
+            leftIndex += 1
+            rightIndex -= 1
+
+
+
 
         return -1
 
 def main():
     # nums = [1, 7, 3, 6, 5, 6]
     # nums = [1, 2, 3]
-
     nums = [-1, -1, -1, -1, -1, 0]
 
     solution = Solution()
