f30f4a894c186ffd48b23ba1c382852983381b27
TimManas <timothy.romero.manas@gmail.com>
2020-11-01 17:42:26 -0500

    Serilized Binary Tree

--- Java/src/main/java/Trees/SerializeAndDeserializeBinaryTree/Codec.java
+++ Java/src/main/java/Trees/SerializeAndDeserializeBinaryTree/Codec.java
@@ -2,8 +2,24 @@ package Trees.SerializeAndDeserializeBinaryTree;
 
 public class Codec {
     // Encodes a tree to a single string.
-    public String serialize(TreeNode root) {
-        return null;
+    public String serialize(TreeNode node) {
+        StringBuilder sb = new StringBuilder();
+        traverseTree(node, sb);
+        System.out.println("Serialized: " + sb.toString());
+
+        return sb.toString();
+    }
+
+    private void traverseTree(TreeNode node, StringBuilder sb) {
+        if (node == null) {
+            sb.append("X,");
+            return;
+        }
+
+        System.out.println("Node: " + node.val);
+        sb.append(node.val + ",");
+        traverseTree(node.left, sb);
+        traverseTree(node.right, sb);
     }
 
     // Decodes your encoded data to tree.
