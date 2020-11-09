b2bde8b57089c04c11e442a3fa98d87944a0977b
TimManas <timothy.romero.manas@gmail.com>
2020-11-09 18:20:13 -0500

    Finished Product of arrays except Self

--- Python/project/src/Math/ProductOfArrayExceptSelf/ProductOfArrayExceptSelf.py
+++ Python/project/src/Math/ProductOfArrayExceptSelf/ProductOfArrayExceptSelf.py
@@ -1,15 +1,49 @@
 class Solution(object):
     def productExceptSelf(self, nums):
+
+        # Step1 - Create output List, leftSumList, RightSumList
         output = []
+        leftSumList = []
+        rightSumList = []
 
-        # Find the total
+        # Step2 - Initialize the Sum Totals
+        leftSum = 1
+        rightSum = 1
+
+        # Step3 - Populate the leftSumList by Moving from Left to Right
         index = 0
-        total = nums[0]
         while index < nums.__len__():
-            total *= nums[index]
-            index += 1
-        print("Total: ", total)
+            print("Current: ", nums[index])
 
+            if index == 0:
+                leftSumList.append(1)
+            else:
+                leftSum = nums[index - 1] * leftSum
+                leftSumList.append(leftSum)
+
+            index += 1
+        print("LeftSumList: ", leftSumList)
+
+        # Step4 - Populate the rightSumList by Moving from Right to Left
+        index = nums.__len__() - 1
+        while index >= 0:
+            print("Current: ", nums[index])
+
+            if index == nums.__len__() - 1:
+                rightSumList.append(1)
+            else:
+                rightSum = nums[index + 1] * rightSum
+                rightSumList.append(rightSum)
+
+            index -= 1
+        rightSumList.reverse()
+        print("RightSumList: ", rightSumList)
+
+        # Step5 - Calculate the Product Together and append to outputList
+        index = 0
+        while index < nums.__len__():
+            output.append(leftSumList[index] * rightSumList[index])
+            index += 1
 
         return output
 
@@ -21,3 +55,28 @@ def main():
 
 if __name__ == '__main__':
     main()
+
+
+'''
+Notes
+- Very difficult Logic to understand
+- Basically, the trick is to create TWO list which stores the sum of the elemenets from the left and right
+- Then we multiple leftSumList elements with the RightSumList Elements
+
+Ex:
+Input:      1   2   3   4
+-----------------------------
+LeftSum:    1   1   2   6   
+RightSum:   24  12  8   6
+-----------------------------
+Product:    24  12  8   6
+
+Solution
+1. Create output List,  list for storing Left Products, List for storign Right Products
+2. Populate the LeftSumList by moving from left to right
+3. Populate the RightSumList by moving from right to left
+4. Multipy the elements of LeftSumList and RightSumList and store in OutputList
+5. Return outputList
+
+
+'''
