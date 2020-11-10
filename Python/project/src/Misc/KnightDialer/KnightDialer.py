ac2abd30c343f754bef453cd156de4d4165b10d3
TimManas <timothy.romero.manas@gmail.com>
2020-11-10 17:36:16 -0500

    Working model

--- Python/project/src/Misc/KnightDialer/KnightDialer.py
+++ Python/project/src/Misc/KnightDialer/KnightDialer.py
@@ -15,41 +15,61 @@ class Solution(object):
                 if current == '*' or current == '!':
                     continue
 
-                self.traverseGrid(grid, row, col, current, input, numberSet)
+                self.traverseGrid(grid, row, col, "", input, numberSet)
 
         return numberSet.__len__()
 
     def traverseGrid(self, grid, row, col, current, input, numberSet):
 
-        
+        if current.__len__() == input:
+            return current
+
+        if row < 0 or col < 0:
+            return None
+        if row > 3 or col > 2:
+            return None
+
+
+        traversedNum = grid[row][col]
+        if traversedNum == '*' or traversedNum == '!':
+            return current
+
+
+        current = "".join([current, traversedNum])
+        print("Position: ", traversedNum, "     Current: ", current)
+
 
         # Find the total phone #'s
-        upRight = self.traverseGrid(grid, row, col, current, input)
-        rightUp = self.traverseGrid(grid, row, col, current, input)
-        rightDown = self.traverseGrid(grid, row, col, current, input)
-        downRight = self.traverseGrid(grid, row, col, current, input)
-        downLeft = self.traverseGrid(grid, row, col, current, input)
-        leftDown = self.traverseGrid(grid, row, col, current, input)
-        leftUp = self.traverseGrid(grid, row, col, current, input)
-        upLeft = self.traverseGrid(grid, row, col, current, input)
+        upRight = self.traverseGrid(grid, row - 2, col + 1, current, input, numberSet)
+        rightUp = self.traverseGrid(grid, row - 1, col + 2, current, input, numberSet)
+        rightDown = self.traverseGrid(grid, row + 1, col + 2, current, input, numberSet)
+        downRight = self.traverseGrid(grid, row + 2, col + 1, current, input, numberSet)
+        downLeft = self.traverseGrid(grid, row + 2, col - 1, current, input, numberSet)
+        leftDown = self.traverseGrid(grid, row + 1, col - 2, current, input, numberSet)
+        leftUp = self.traverseGrid(grid, row - 1, col - 2, current, input, numberSet)
+        upLeft = self.traverseGrid(grid, row - 2, col - 1, current, input, numberSet)
 
-        numberSet.add(upRight)
-        numberSet.add(rightUp)
-        numberSet.add(rightDown)
-        numberSet.add(downRight)
-        numberSet.add(downLeft)
-        numberSet.add(leftDown)
-        numberSet.add(leftUp)
-        numberSet.add(upLeft)
+
+        if current.__len__() == input:
+            numberSet.add(upRight)
+            numberSet.add(rightUp)
+            numberSet.add(rightDown)
+            numberSet.add(downRight)
+            numberSet.add(downLeft)
+            numberSet.add(leftDown)
+            numberSet.add(leftUp)
+            numberSet.add(upLeft)
 
 
 def main():
     inputList = [1,2,3,4,3131]
     solution = Solution()
-    for i in inputList:
-        output = solution.knightDialer(i)
-        print("Output: ", output, "\n")
 
+    # for i in inputList:
+    #     output = solution.knightDialer(i)
+    #     print("Output: ", output, "\n")
+
+    print("Output: ", solution.knightDialer(4))
 
 if __name__ == '__main__':
     main()
