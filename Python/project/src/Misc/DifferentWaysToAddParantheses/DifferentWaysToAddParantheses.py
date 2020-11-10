382873f9479e945704e4e548d12b49886cbb161c
TimManas <timothy.romero.manas@gmail.com>
2020-11-10 10:10:09 -0500

    Started Different ways to add paranthesis

---
+++ Python/project/src/Misc/DifferentWaysToAddParantheses/DifferentWaysToAddParantheses.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def diffWaysToCompute(self, input):
+
+
+def main():
+    input = "2-1-1"
+    intput2 = "2*3-4*5"
+    solution = Solution()
+    output = solution.diffWaysToCompute(input)
+    print("Num Ways To Add Parantheses: ", output)
+
+if __name__ == '__main__':
+    main()
