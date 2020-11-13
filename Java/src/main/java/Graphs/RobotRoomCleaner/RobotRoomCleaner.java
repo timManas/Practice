ee6e9711b19c236bf1dcb6f929aa8e605ccc638f
TimManas <timothy.romero.manas@gmail.com>
2020-11-13 11:29:56 -0500

    Started Robot RoomCleaner

---
+++ Java/src/main/java/Graphs/RobotRoomCleaner/RobotRoomCleaner.java
@@ -0,0 +1,21 @@
+package Graphs.RobotRoomCleaner;
+
+public class RobotRoomCleaner {
+
+    public static void main(String [] args) {
+        int [] []room = {
+                {1, 1, 1, 1, 1, 0, 1, 1},
+                {1, 1, 1, 1, 1, 0, 1, 1},
+                {1, 0, 1, 1, 1, 1, 1, 1},
+                {0, 0, 0, 1, 0, 0, 0, 0},
+                {1, 1, 1, 1, 1, 1, 1, 1}};
+
+        int row =1;
+        int col = 3;
+
+    }
+
+    public static void cleanRoom(Robot robot) {
+
+    }
+}
