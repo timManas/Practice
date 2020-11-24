a3bba18e43e4d89bf8df1bf4c03271efa26c150f
TimManas <timothy.romero.manas@gmail.com>
2020-11-24 17:55:48 -0500

    Started Maximal Square

---
+++ Java/src/main/java/Math/MaximalSquare/MaximalSquare.java
@@ -0,0 +1,17 @@
+package Math.MaximalSquare;
+
+public class MaximalSquare {
+
+    public static void main(String[] args) {
+        char [][] input =  {{'1', '0', '1', '0', '0'},
+                            {'1', '0', '1', '1', '1'},
+                            {'1', '1', '1', '1', '1'},
+                            {'1', '0', '0', '1', '0'}};
+        System.out.println("Maximal Squares: " + maximalSquare(input));
+    }
+
+    public static int maximalSquare(char[][] matrix) {
+        return 0;
+    }
+
+}
