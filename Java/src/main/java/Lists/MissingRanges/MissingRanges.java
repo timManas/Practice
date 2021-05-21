c3be9830c12478937ec8e45dfb471d8302ddb796
Tim M <timothy.romero.manas@gmail.com>
2021-05-21 00:12:33 -0400

    Missing range is at 90%

--- Java/src/main/java/Lists/MissingRanges/MissingRanges.java
+++ Java/src/main/java/Lists/MissingRanges/MissingRanges.java
@@ -9,6 +9,22 @@ public class MissingRanges {
         int lower = 0;
         int upper = 99;
 
+//        int [] nums = {};
+//        int lower = 1;
+//        int upper = 1;
+
+//        int [] nums = {};
+//        int lower = -3;
+//        int upper = -1;
+
+//        int [] nums = {-1};
+//        int lower = -1;
+//        int upper = -1;
+
+//        int [] nums = {-1};
+//        int lower = -2;
+//        int upper = -1;
+
         List<String> missingRanges = findMissingRanges(nums, lower, upper);
         System.out.println("Missing Ranges: " + missingRanges);
     }
@@ -16,31 +32,53 @@ public class MissingRanges {
     public static List<String> findMissingRanges(int[] nums, int lower, int upper) {
         List<String> output = new ArrayList<>();
 
-        int missingStart = -1;
-        int missingEnd = -1;
+        int missingStart = Integer.MIN_VALUE;
+        int missingEnd = Integer.MIN_VALUE;
         for (int current=lower; current <= upper; current++) {
             int indexInNums = findElementExists(nums, current, 0, nums.length - 1);
             if (indexInNums != -1) {
-                System.out.println("MATCH FOUND: current: " + current);
+                System.out.println("MATCH FOUND: current: " + current + "   Start: " + missingStart + "     End: " + missingEnd);
+                if (getStartEnd(missingStart, missingEnd) != null) {
+                    output.add(getStartEnd(missingStart, missingEnd));
+                }
+                missingStart = Integer.MIN_VALUE;
+                missingEnd = Integer.MIN_VALUE;
                 continue;
             }
 
-            if (missingStart == -1) {
+            if (missingStart == Integer.MIN_VALUE) {
                 missingStart = current;
-            } else if (missingEnd == -1) {
+            } else if (missingEnd == Integer.MIN_VALUE) {
                 missingEnd = current;
-            } else if (missingEnd != -1 && missingEnd+1 == current) {
+            } else if (missingEnd != Integer.MIN_VALUE && missingEnd+1 == current) {
                 missingEnd = current;
             } else {
                 System.out.println("Start: " + missingStart + "     End: " + missingEnd);
-                missingStart = -1;
-                missingEnd = -1;
+                if (getStartEnd(missingStart, missingEnd) != null) {
+                    output.add(getStartEnd(missingStart, missingEnd));
+                }
+                missingStart = Integer.MIN_VALUE;
+                missingEnd = Integer.MIN_VALUE;
             }
 
 
         }
+        System.out.println("Start: " + missingStart + "     End: " + missingEnd);
+        if (getStartEnd(missingStart, missingEnd) != null) {
+            output.add(getStartEnd(missingStart, missingEnd));
+        }
 
         return output;
+    }
+
+    private static String getStartEnd(int missingStart, int missingEnd) {
+        if (missingStart == Integer.MIN_VALUE && missingEnd == Integer.MIN_VALUE)
+            return null;
+
+        if (missingEnd == Integer.MIN_VALUE)
+            return String.valueOf(missingStart);
+
+        return (missingStart + "->" + missingEnd);
     }
 
     public static int findElementExists(int [] nums, int element, int startIndex, int endIndex) {
