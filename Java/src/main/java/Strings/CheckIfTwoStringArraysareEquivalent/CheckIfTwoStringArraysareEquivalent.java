9fb08d2c98955541219e2068eb98bbb2ca3aca93
Tim M <timothy.romero.manas@gmail.com>
2021-06-13 17:40:34 -0400

    Adding CHeck if two string arrays are equivalent

---
+++ Java/src/main/java/Strings/CheckIfTwoStringArraysareEquivalent/CheckIfTwoStringArraysareEquivalent.java
@@ -0,0 +1,26 @@
+package Strings.CheckIfTwoStringArraysareEquivalent;
+
+public class CheckIfTwoStringArraysareEquivalent {
+
+    public static void main(String [] args) {
+        String [] word1 = {"ab", "c"};
+        String [] word2 = {"a", "bc"};
+
+        System.out.println("String are equal: " + arrayStringsAreEqual(word1, word2));
+    }
+
+    public static boolean arrayStringsAreEqual(String[] word1, String[] word2) {
+        StringBuilder sb1 = new StringBuilder();
+        StringBuilder sb2 = new StringBuilder();
+
+        for (String word: word1) {
+            sb1.append(word);
+        }
+
+        for (String word : word2) {
+            sb2.append(word);
+        }
+
+        return sb1.toString().equals(sb2.toString());
+    }
+}
