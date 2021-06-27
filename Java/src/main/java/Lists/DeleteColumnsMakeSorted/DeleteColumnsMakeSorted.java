e3f2769f0218603e15c2661a934d02d2c475072c
Tim M <timothy.romero.manas@gmail.com>
2021-06-27 12:10:06 -0400

    adding Delete columns makes sorted

---
+++ Java/src/main/java/Lists/DeleteColumnsMakeSorted/DeleteColumnsMakeSorted.java
@@ -0,0 +1,34 @@
+package Lists.DeleteColumnsMakeSorted;
+
+public class DeleteColumnsMakeSorted {
+    public static void main (String [] args) {
+//        String [][] inputs = {{"cba","daf","ghi"}, {"a","b"}, {"zyx","wvu","tsr"}, {"rrjk","furt","guzm"}};
+        String [][] inputs = { {"rrjk","furt","guzm"}};
+        for (String [] strs : inputs)
+            System.out.println("Min Deletion size: " + minDeletionSize(strs));
+    }
+
+    public static int minDeletionSize(String[] strs) {
+        int minDeletetionSize = 0;
+
+        // Step1 - Loop through each letter
+        for (int i=0; i < strs[0].length(); i++) {
+            char lastChar = '\n';
+
+            // Step2 - Loop through each word
+            for (String word : strs) {
+                char currentLetter = word.charAt(i);
+
+                // Step3 - Break loop if current letter is smaller than the last
+                if (lastChar > currentLetter) {
+                    minDeletetionSize++;
+                    break;
+                }
+                lastChar = currentLetter;
+            }
+        }
+
+
+        return minDeletetionSize;
+    }
+}
