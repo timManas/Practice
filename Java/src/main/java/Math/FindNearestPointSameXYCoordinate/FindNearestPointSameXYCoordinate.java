9428fd34ffab2441db55c88e708b3412d7b8d5de
Tim M <timothy.romero.manas@gmail.com>
2021-07-02 23:54:11 -0400

    Adding find nearest point same XY Coordinate

---
+++ Java/src/main/java/Math/FindNearestPointSameXYCoordinate/FindNearestPointSameXYCoordinate.java
@@ -0,0 +1,13 @@
+package Math.FindNearestPointSameXYCoordinate;
+
+public class FindNearestPointSameXYCoordinate {
+    public static void main(String [] args) {
+        int x = 3, y = 4;
+        int [][] points = {{1,2}, {3,1}, {2,4}, {2,3}, {4,4}};
+        System.out.println("Neatest Valid Point: " + nearestValidPoint(x,y, points));
+    }
+
+    public static int nearestValidPoint(int x, int y, int[][] points) {
+
+    }
+}
