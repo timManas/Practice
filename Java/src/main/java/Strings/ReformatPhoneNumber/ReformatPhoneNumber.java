3a68873a417e1e0ee3f469e270879a36c6d6f697
Tim M <timothy.romero.manas@gmail.com>
2021-07-06 21:16:38 -0400

    Finished Reformat phone number

--- Java/src/main/java/Strings/ReformatPhoneNumber/ReformatPhoneNumber.java
+++ Java/src/main/java/Strings/ReformatPhoneNumber/ReformatPhoneNumber.java
@@ -7,43 +7,53 @@ public class ReformatPhoneNumber {
             System.out.println("Reformat Number: " + reformatNumber(number));
     }
 
-    public static String reformatNumber(String number) {
+    private static String reformatNumber(String number) {
+
+        // Step1 - Create Strinbuilder to store the reformatted string
         StringBuilder sb = new StringBuilder();
+
+        // Step2 - Filer out all dash and spaces
         String unFormattedNum = number.replaceAll(" ", "").replaceAll("-", "");
         System.out.println("Starting number: " + unFormattedNum);
 
-        for (int i=0; i < unFormattedNum.length(); i++) {
-            int remaining = (unFormattedNum.length() - 1) - i;
+        // Step3 - Traverse the unformattedNum
+        for (int i=0; i < unFormattedNum.length(); i+=3) {
 
-            sb.append(unFormattedNum.charAt(i));
-            if ((i+1) % 3 == 0)
-                sb.append('-');
+            // Step4 - Calculate the remaining number of digits
+            int remaining = unFormattedNum.length()  - i;
+            System.out.println("Current: " + unFormattedNum.charAt(i) + "       Rem: " + remaining);
 
-
-            // Split
-            if (remaining == 4) {
-                System.out.println("4 Rem");
-                sb.append(unFormattedNum.charAt(i+1)).append(unFormattedNum.charAt(i+2)).append('-').append(unFormattedNum.charAt(i+3)).append(unFormattedNum.charAt(i+4));
-                break;
+            // Add in increments of three
+            if (remaining >= 5) {
+                sb.append(unFormattedNum.charAt(i)).append(unFormattedNum.charAt(i+1)).append(unFormattedNum.charAt(i+2)).append('-');
+                continue;
+            } else if (remaining == 4) {
+                //4 digits: Two blocks of length 2 each
+                sb.append(unFormattedNum.charAt(i)).append(unFormattedNum.charAt(i+1)).append('-').append(unFormattedNum.charAt(i+2)).append(unFormattedNum.charAt(i+3));
             } else if (remaining == 3) {
-                System.out.println("3 Rem");
-                sb.append(unFormattedNum.charAt(i+1)).append(unFormattedNum.charAt(i+2)).append(unFormattedNum.charAt(i+3));
-                break;
+                // 3 digits: A single block of length 3
+                sb.append(unFormattedNum.charAt(i)).append(unFormattedNum.charAt(i+1)).append(unFormattedNum.charAt(i+2));
             } else if (remaining == 2) {
-                System.out.println("2 Rem");
-                sb.append(unFormattedNum.charAt(i+1)).append(unFormattedNum.charAt(i+2));
-                break;
-            } else if (remaining == 1) {
-                System.out.println("Error");
-                break;
+                // 2 digits: A single block of length 2
+                sb.append(unFormattedNum.charAt(i)).append(unFormattedNum.charAt(i+1));
             }
 
-            System.out.println("Current: " + unFormattedNum.charAt(i) + "       Rem: " + remaining + "      " + sb.toString());
-
-
+            System.out.println(sb.toString());
         }
-
 
         return sb.toString();
     }
 }
+
+/**
+ Notes
+ - Tricker than expected but not impossible
+
+ Solution
+ 1. Filter out dash and spaces
+ 2. Traverse unFormattedNumber by incrementing in groups of 3
+ 3. If there are 5 or more, we add the next three
+    //4 digits: Two blocks of length 2 each
+    // 3 digits: A single block of length 3
+    // 2 digits: A single block of length 2
+ */
