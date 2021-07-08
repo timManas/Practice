ee8700dc2f93a091b79088893fb89228e01353cb
Tim M <timothy.romero.manas@gmail.com>
2021-07-08 18:50:16 -0400

    Finished Max Asc Sub array

--- Java/src/main/java/Lists/MaxAscSubArraySum/MaxAscSubArraySum.java
+++ Java/src/main/java/Lists/MaxAscSubArraySum/MaxAscSubArraySum.java
@@ -10,25 +10,29 @@ public class MaxAscSubArraySum {
     }
 
     public static int maxAscendingSum(int[] nums) {
+
+        //Step1 - Create map which stores the key= total, value=Sets
         Map<Integer, List<Integer>> map = new TreeMap<>();
 
-        int subTotal = 0;
+        int subTotal = 0; // subtotal calculate the subtotal of the subarray
+
+        // Step2 - Traverse the input array
         List<Integer> tempList = new ArrayList<>();
         for (int i=0; i < nums.length; i++) {
             int current = nums[i];
 
-            // Do nothing for the 1st element in the array
+            // Step3 - Do nothing for the 1st element in the array
             if (i == 0) {
                 subTotal += current;
                 tempList.add(current);
                 continue;
             }
 
-            // Check if Ascending. Clear temp set if descending
-            if (nums[i-1] > current) {
-                map.put(subTotal, tempList);
-                tempList.clear();
+            // Step4 - Check if Ascending. Clear temp set if descending
+            if (nums[i-1] >= current) {
+                map.put(subTotal, tempList);        // Add to map
 
+                tempList.clear();                   // Reset
                 subTotal = current;
                 tempList.add(current);  // Start from scatch
             } else {
@@ -38,9 +42,12 @@ public class MaxAscSubArraySum {
 
 
         }
+
+        // Step5 - Add last tempList to map
         map.put(subTotal, tempList);
         System.out.println("Map: " + map);
 
+        // Step6 - Find the last key since this is the largest
         List<Integer> list = new ArrayList<>(map.keySet());
 
         return list.get(list.size()-1);
