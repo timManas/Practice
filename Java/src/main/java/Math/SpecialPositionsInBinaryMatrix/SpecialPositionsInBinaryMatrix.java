6084199380178e528cf90da5ed491ba63f94aa68
Tim M <timothy.romero.manas@gmail.com>
2021-07-10 13:57:27 -0400

    Finished Special positions in binary matrix

--- Java/src/main/java/Math/SpecialPositionsInBinaryMatrix/SpecialPositionsInBinaryMatrix.java
+++ Java/src/main/java/Math/SpecialPositionsInBinaryMatrix/SpecialPositionsInBinaryMatrix.java
@@ -3,66 +3,69 @@ package Math.SpecialPositionsInBinaryMatrix;
 public class SpecialPositionsInBinaryMatrix {
 
     public static void main(String args []) {
-//        int [][] mat1 = {{1,0,0}, {0,0,1}, {1,0,0}};
-//        System.out.println("Num Special: " + numSpecial(mat1));
+        int [][] mat1 = {{1,0,0}, {0,0,1}, {1,0,0}};
+        System.out.println("Num Special: " + numSpecial(mat1) + "\n");
 
         int [][] mat2 = {{1,0,0}, {0,1,0}, {0,0,1}};
-        System.out.println("Num Special: " + numSpecial(mat2));
+        System.out.println("Num Special: " + numSpecial(mat2) + "\n");
 
         int [][] mat3 = {{0,0,0,1}, {1,0,0,0}, {0,1,1,0}, {0,0,0,0}};
-        System.out.println("Num Special: " + numSpecial(mat3));
+        System.out.println("Num Special: " + numSpecial(mat3) + "\n");
+
+        int [][] mat4 = {{0,0,0,0,0}, {1,0,0,0,0}, {0,1,0,0,0}, {0,0,1,0,0}, {0,0,0,1,1}};
+        System.out.println("Num Special: " + numSpecial(mat4) + "\n");
     }
 
-    public static int numSpecial(int[][] mat) {
+    private static int numSpecial(int[][] mat) {
         int count = 0;
 
-        boolean isFound1 = false;
-        int index = 0;
-        while (index < mat.length) {
+        for (int row=0; row < mat.length; row++) {
+            for (int col=0; col < mat[row].length; col++) {
+                int current = mat[row][col];
+                System.out.println("--------- Row: " + row + "    Col:" + col + "   Current: " + current);
 
-            // Traverse Top Down
-            for (int i=0; i < mat.length; i++) {
-                int currentX = mat[i][index];
-                System.out.println("row:" + i + "   col: " + index + "      Current: " + currentX);
-                if (currentX == 0)
+                // Ignore all 0's
+                if (current == 0)
                     continue;
 
-                if (isFound1) {
-                    isFound1 = false;
-                    break;
+                // At this point it is 1 ------
+                boolean isFound = false;
+
+                // Traverse top down
+                for (int i=0; i < mat.length; i++) {
+                    int elementToBeChecked = mat[i][col];
+
+                    // Move on if we same element
+                    if (i == row)
+                        continue;
+
+
+                    if (elementToBeChecked == 1) {
+                        isFound = true;
+                    }
                 }
 
-                isFound1 = true;
-            }
+                // Traverse left right
+                for (int i=0; i < mat[0].length; i++) {
+                    int elementToBeChecked = mat[row][i];
 
-            // Traverse Left Right
-            for (int i=0; i < mat[0].length; i++) {
-                int currentY = mat[index][i];
-                System.out.println("row:" + index + "   col: " + i + "      Current: " + currentY);
+                    // Move on if we same element
+                    if (i == col)
+                        continue;
 
-                if (currentY == 0)
-                    continue;
+                    if (elementToBeChecked == 1)
+                        isFound = true;
 
-                if (isFound1) {
-                    isFound1 = false;
-                    break;
                 }
 
-                isFound1 = true;
+                if (!isFound)
+                    count++;
+
             }
-
-            System.out.println();
-
-
-
-            if (isFound1)
-                count++;
-
-            isFound1 = false;   // Reset the values
-
-            index++;
         }
 
         return count;
     }
+
+
 }
