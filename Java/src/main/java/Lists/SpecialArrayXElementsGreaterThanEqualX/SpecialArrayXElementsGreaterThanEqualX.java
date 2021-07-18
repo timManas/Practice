bc1ac27eca8fa8c7116415a26302bc530c67b967
Tim M <timothy.romero.manas@gmail.com>
2021-07-18 15:54:28 -0400

    Special k

--- Java/src/main/java/Lists/SpecialArrayXElementsGreaterThanEqualX/SpecialArrayXElementsGreaterThanEqualX.java
+++ Java/src/main/java/Lists/SpecialArrayXElementsGreaterThanEqualX/SpecialArrayXElementsGreaterThanEqualX.java
@@ -5,12 +5,11 @@ import java.util.Arrays;
 public class SpecialArrayXElementsGreaterThanEqualX {
 
     public static void main(String [] args) {
-//        int [][] input = {{3,5}, {0,0}, {0,0,3,4,4}, {3,6,7,7,0}};
-//        for (int [] nums : input) {
-//            System.out.println("Special Array: " + specialArray(nums));
-//        }
 
-        System.out.println("Special Array: " + specialArray(new int[]{3, 6, 7, 7, 0}));
+        System.out.println("Special Array: " + specialArray(new int[]{3,5}));
+        System.out.println("Special Array: " + specialArray(new int[]{0,0}));
+        System.out.println("Special Array: " + specialArray(new int[]{0,0,3,4,4}));
+        System.out.println("Special Array: " + specialArray(new int[]{3,6,7,7,0}));
     }
 
     public static int specialArray(int[] nums) {
@@ -19,16 +18,44 @@ public class SpecialArrayXElementsGreaterThanEqualX {
         // Sort the array for easier parsing
         Arrays.sort(nums);
 
+        // Step2 - Start from num.length till 0
+        // Every step, we compare with each value of nums
+        outLoop:
         while (x > 0) {
-            if (x <= nums[nums.length - x]) {
-                return x;
-            }
 
+            innerLoop:
+            for (int i=0; i<nums.length;i++) {
+
+                // Step3 - If x is smaller than the current Element
+                // We check if its a special array: True, we break out of the outerloop, False we continue by decrementing x
+                if (x <= nums[i]) {
+                    int numElementsEqualGreater = nums.length - i;
+
+                    // Check if special array
+                    if (x == numElementsEqualGreater)
+                        break outLoop;
+
+                    break innerLoop;        // Break if NOT special array
+                }
+            }
             x--;
         }
-
-
-        return -1;
+        return x == 0 ? -1 : x;
     }
-
 }
+
+/**
+ Note
+ - Tricky as FUCK but i got it !!
+ - Trick is to work backwards from length of array to 0
+
+ Solution
+ 1. Sort the input array. Helps with determining how many elements are bigger than current
+ 2. Create while loop to traverse from nums.lengh to 0
+ 3. Create inner loop to traverse each element in array
+ 4. Check if special array:
+    True: We break out of the Enter loop and return x
+    False: We stop inner loop and continue decrementing x
+ 5. Return x if found and -1 if not found
+
+ */
