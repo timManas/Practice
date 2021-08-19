f392336fa81c7f76d47e12c68a1644fc1fc580e0
Tim M <timothy.romero.manas@gmail.com>
2021-08-18 22:22:22 -0400

    Adding largest perimeter triangle

---
+++ Java/src/main/java/Misc/LargestPerimeterTriangle/LargestPerimeterTriangle.java
@@ -0,0 +1,13 @@
+package Misc.LargestPerimeterTriangle;
+
+public class LargestPerimeterTriangle {
+    public static void main(String [] args) {
+        int [][] input = {{2,1,2}, {1,2,1}, {3,2,3,4}, {3,6,2,3} };
+        for (int[] i : input)
+            System.out.println("Largest Perimeter: " + largestPerimeter(i));
+    }
+
+    public static int largestPerimeter(int[] nums) {
+
+    }
+}
