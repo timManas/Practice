03bdd396e0ab3f26e4f0285037ea12e1bbf68b11
Tim M <timothy.romero.manas@gmail.com>
2021-08-19 11:30:05 -0400

    Finished largest perimeter triangle

--- Java/src/main/java/Misc/LargestPerimeterTriangle/LargestPerimeterTriangle.java
+++ Java/src/main/java/Misc/LargestPerimeterTriangle/LargestPerimeterTriangle.java
@@ -1,13 +1,32 @@
 package Misc.LargestPerimeterTriangle;
 
+import java.util.Arrays;
+
 public class LargestPerimeterTriangle {
     public static void main(String [] args) {
         int [][] input = {{2,1,2}, {1,2,1}, {3,2,3,4}, {3,6,2,3} };
         for (int[] i : input)
-            System.out.println("Largest Perimeter: " + largestPerimeter(i));
+            System.out.println("\nLargest Perimeter: " + largestPerimeter(i));
     }
 
     public static int largestPerimeter(int[] nums) {
+        // Step1 - Sort the input array
+        Arrays.sort(nums);
 
+        // Step2 - Traverse backwards to find the largest perimeter
+        for (int i=(nums.length) - 3; i >= 0; i--) {
+            System.out.println("Current: " + nums[i]);
+            int a = nums[i];
+            int b = nums[i+1];
+            int c = nums[i+2];
+
+            // Step3 - Check if triangle possible
+            // If so, return perimeter
+            // Why does this work ? Because when we sort, we get the largest values
+            if ((a + b > c) && (a+c>b) && (b+c>a))
+                return (a+b+c);
+        }
+
+        return 0;
     }
 }
