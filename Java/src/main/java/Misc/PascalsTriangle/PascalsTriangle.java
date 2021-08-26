fa3f931b53e400c565d2cf1c623a5a12c9b7a344
Tim M <timothy.romero.manas@gmail.com>
2021-08-26 18:37:26 -0400

    Adding Pascals triangle

---
+++ Java/src/main/java/Misc/PascalsTriangle/PascalsTriangle.java
@@ -0,0 +1,19 @@
+package Misc.PascalsTriangle;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PascalsTriangle {
+    public static void main(String [] args) {
+        int [] input = {5,1};
+        for (int i : input)
+            System.out.println("Triangle: " + generate(i));
+    }
+
+    public static List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> output = new ArrayList<>();
+
+
+        return output;
+    }
+}
