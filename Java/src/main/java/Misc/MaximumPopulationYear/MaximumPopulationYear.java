187e4373a38575920a540bd4e8c7a9c428a2b02e
Tim M <timothy.romero.manas@gmail.com>
2021-09-08 22:17:06 -0400

    Finished maximum population year

--- Java/src/main/java/Misc/MaximumPopulationYear/MaximumPopulationYear.java
+++ Java/src/main/java/Misc/MaximumPopulationYear/MaximumPopulationYear.java
@@ -10,44 +10,35 @@ public class MaximumPopulationYear {
 //        int [][] logs2 = {{1950,1961}, {1960,1971}, {1970,1981}};
 //        System.out.println("Max Population: " + maximumPopulation(logs2));
 
-        int [][] logs3 = {{2025,2041},{1988,2007},{2003,2046},{2045,2049},{2025,2027},{2014,2040},{2014,2027},{2011,2027},{1972,2019}};
-        System.out.println("Max Population: " + maximumPopulation(logs3));
+//        int [][] logs3 = {{2025,2041},{1988,2007},{2003,2046},{2045,2049},{2025,2027},{2014,2040},{2014,2027},{2011,2027},{1972,2019}};
+//        System.out.println("Max Population: " + maximumPopulation(logs3));
+
+        int [][] logs4 = {{2008,2026},{2004,2008},{2034,2035},{1999,2050},{2049,2050},{2011,2035},{1966,2033},{2044,2049}};
+        System.out.println("Max Population: " + maximumPopulation(logs4));
     }
 
     public static int maximumPopulation(int[][] logs) {
-
-        List<List<Integer>> populationLogs = new ArrayList<>();
-
+        // Step1 - Create map to store population
         Map<Integer, Integer> map = new TreeMap<>();
 
+        // Step2 - Traverse through
         for (int i=0; i < logs.length; i++) {
            int [] person = logs[i];
            int birth = person[0];
            int death = person[1];
 
-            List<Integer> tempList = new ArrayList<>();
-            tempList.add(birth);
-            tempList.add(death);
-            populationLogs.add(tempList);
-
-
-            for (int x=0; x < populationLogs.size(); x++) {
-               int start = populationLogs.get(x).get(0);
-               int end = populationLogs.get(x).get(1);
-
-               if (start <= birth && birth <= end) {
-                   if (map.containsKey(birth)) {
-                       map.put(birth, map.get(birth) + 1);
-                   } else {
-                       map.put(birth, 1);
-                   }
+           for (int x=birth; x < death; x++) {
+               if (map.containsKey(x)) {
+                   map.put(x, map.get(x) + 1);
+               } else {
+                   map.put(x,1);
                }
            }
         }
 
-        // Traverse Map to find largest year
+        // Step3 - Traverse Map to find largest year
         int largestPopulation = 0;
-        int earliestYear = 0;
+        int earliestYear = Integer.MAX_VALUE;
         for (Map.Entry<Integer, Integer> mapEntry : map.entrySet()) {
             if (largestPopulation < mapEntry.getValue()) {
                 largestPopulation = mapEntry.getValue();
