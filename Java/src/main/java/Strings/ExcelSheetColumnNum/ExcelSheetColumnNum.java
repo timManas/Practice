c01c48ec3605e5eac6f7808c7ff883f82a2ec98f
Tim M <timothy.romero.manas@gmail.com>
2021-10-11 21:19:01 -0400

    Adding Excel Sheet column number

---
+++ Java/src/main/java/Strings/ExcelSheetColumnNum/ExcelSheetColumnNum.java
@@ -0,0 +1,17 @@
+package Strings.ExcelSheetColumnNum;
+
+public class ExcelSheetColumnNum {
+
+    public static void main(String [] args) {
+        String [] input = {"A", "AB", "ZY", "FXSHRXW"};
+        for (String i : input)
+            System.out.println("Title To Number: " + titleToNumber(i));
+
+    }
+
+    public static int titleToNumber(String columnTitle) {
+        int num = 0;
+
+        return num;
+    }
+}
