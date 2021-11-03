d2896b167b3c0dbad4693350c90706df27958599
Tim M <timothy.romero.manas@gmail.com>
2021-11-02 22:42:28 -0400

    Floodfil

--- Java/src/main/java/Misc/FloodFill/FloodFill.java
+++ Java/src/main/java/Misc/FloodFill/FloodFill.java
@@ -2,9 +2,10 @@ package Misc.FloodFill;
 
 public class FloodFill {
     public static void main(String [] args) {
-        int [] [] image = {{1,1,1}, {1,1,0}, {1,0,1}};
-        int sr = 1;
-        int sc = 1;
+//        int [] [] image = {{1,1,1}, {1,1,0}, {1,0,1}};
+        int [] [] image = {{1,2,3}, {4,5,6}, {7,8,9}};
+        int sr = 2;
+        int sc = 2;
         int newColor = 2;
         int [][] output = floodFill(image, sr, sc, newColor);
 
@@ -16,6 +17,14 @@ public class FloodFill {
     }
 
     public static int[][] floodFill(int[][] image, int sr, int sc, int newColor) {
+
+        // Find the coordinates
+        int top =  (sr <= 0) ? -1 : image[sr-1][sc] ;
+        int bottom = (sr >= image.length-1) ? -1 : image[sr+1][sc];
+        int left = (sc <= 0) ? -1 : image[sr][sc-1];
+        int right = (sc >= image[sr].length-1) ? -1 : image[sr][sc+1];
+
+        System.out.println("  left: " + left + "     top:" + top + "      right: " + right + "  bottom: " + bottom);
 
 
         return image;
