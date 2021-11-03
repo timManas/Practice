dae462c8b82b5cc22e1758a6b789270d2b9dffb0
Tim M <timothy.romero.manas@gmail.com>
2021-11-02 23:07:56 -0400

    Adding more functionality

--- Java/src/main/java/Misc/FloodFill/FloodFill.java
+++ Java/src/main/java/Misc/FloodFill/FloodFill.java
@@ -2,10 +2,10 @@ package Misc.FloodFill;
 
 public class FloodFill {
     public static void main(String [] args) {
-//        int [] [] image = {{1,1,1}, {1,1,0}, {1,0,1}};
-        int [] [] image = {{1,2,3}, {4,5,6}, {7,8,9}};
-        int sr = 2;
-        int sc = 2;
+        int [] [] image = {{1,1,1}, {1,1,0}, {1,0,1}};
+//        int [] [] image = {{1,2,3}, {4,5,6}, {7,8,9}};
+        int sr = 1;
+        int sc = 1;
         int newColor = 2;
         int [][] output = floodFill(image, sr, sc, newColor);
 
@@ -17,8 +17,11 @@ public class FloodFill {
     }
 
     public static int[][] floodFill(int[][] image, int sr, int sc, int newColor) {
+        return performFloodFill(image, sr, sc, newColor, image[sr][sc]);
+    }
 
-        // Find the coordinates
+    private static int[][] performFloodFill(int[][] image, int sr, int sc, int newColor, int oldColour) {
+        // Find the coordinates top, bottom, left, right
         int top =  (sr <= 0) ? -1 : image[sr-1][sc] ;
         int bottom = (sr >= image.length-1) ? -1 : image[sr+1][sc];
         int left = (sc <= 0) ? -1 : image[sr][sc-1];
@@ -26,7 +29,29 @@ public class FloodFill {
 
         System.out.println("  left: " + left + "     top:" + top + "      right: " + right + "  bottom: " + bottom);
 
+        image[sr][sc] = newColor;
+
+        if (top == oldColour) {
+            image[sr - 1][sc] = newColor;
+            performFloodFill(image, sr-1, sc, newColor, oldColour);
+        }
+
+        if (bottom == oldColour) {
+            image[sr + 1][sc] = newColor;
+            performFloodFill(image, sr+1, sc, newColor, oldColour);
+        }
+
+        if (left == oldColour) {
+            image[sr][sc - 1] = newColor;
+            performFloodFill(image, sr, sc-1, newColor, oldColour);
+        }
+        if (right == oldColour) {
+            image[sr][sc + 1] = newColor;
+            performFloodFill(image, sr, sc+1, newColor, oldColour);
+        }
 
         return image;
     }
+
+
 }
