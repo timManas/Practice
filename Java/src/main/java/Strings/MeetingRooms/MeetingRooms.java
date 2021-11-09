cec15c13848f7ff16fb49c294662c502fb2afeb5
Tim M <timothy.romero.manas@gmail.com>
2021-11-08 23:36:55 -0500

    Adding Meeting Rooms

---
+++ Java/src/main/java/Strings/MeetingRooms/MeetingRooms.java
@@ -0,0 +1,17 @@
+package Strings.MeetingRooms;
+
+public class MeetingRooms {
+    public static void main(String [] args) {
+        int [][] intervals = {{0,30}, {5,10}, {15,20}};
+        System.out.println("AttendAllMeetings: " + canAttendMeetings(intervals));
+
+        int [][] intervals2 = {{7,10}, {2,4}};
+        System.out.println("AttendAllMeetings: " + canAttendMeetings(intervals2));
+    }
+
+    public static boolean canAttendMeetings(int[][] intervals) {
+
+
+        return true;
+    }
+}
