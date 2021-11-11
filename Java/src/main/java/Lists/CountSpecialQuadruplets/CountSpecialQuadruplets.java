c6c67c68263a5f8f22b965dd1fc0d3484e2d99a1
Tim M <timothy.romero.manas@gmail.com>
2021-11-10 23:43:11 -0500

    Starting count special quadruplets

---
+++ Java/src/main/java/Lists/CountSpecialQuadruplets/CountSpecialQuadruplets.java
@@ -0,0 +1,15 @@
+package Lists.CountSpecialQuadruplets;
+
+public class CountSpecialQuadruplets {
+    public static void main(String [] args) {
+        int [][] inputs = {{1,2,3,6}, {3,3,6,4,5}, {1,1,1,3,5}};
+        for (int [] i : inputs)
+            System.out.println("Num Quadruplets: " + countQuadruplets(i));
+    }
+
+    public static int countQuadruplets(int[] nums) {
+        int count = 0;
+
+        return count;
+    }
+}
