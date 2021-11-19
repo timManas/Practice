f7c965c9700b68563993f980a46634ca14295c98
Tim M <timothy.romero.manas@gmail.com>
2021-11-19 01:09:43 -0500

    Adding Count special qudruplets

--- Java/src/main/java/Lists/CountSpecialQuadruplets/CountSpecialQuadruplets.java
+++ Java/src/main/java/Lists/CountSpecialQuadruplets/CountSpecialQuadruplets.java
@@ -2,9 +2,9 @@ package Lists.CountSpecialQuadruplets;
 
 public class CountSpecialQuadruplets {
     public static void main(String [] args) {
-//        int [][] inputs = {{1,2,3,6}, {3,3,6,4,5}, {1,1,1,3,5}, {1,2,3,4,5,6,7,8,9,10}};
+//        int [][] inputs = {{1,2,3,6}, {3,3,6,4,5}, {1,1,1,3,5}, {28,8,49,85,37,90,20,8}};
 //        int [][] inputs = {{1,2,3,4,5,6,7,8,9,10}};
-        int [][] inputs = {{25,69,18,24,60,7,49}};
+        int [][] inputs = {{28,8,49,85,37,90,20,8}};
         for (int [] i : inputs)
             System.out.println("Num Quadruplets: " + countQuadruplets(i));
     }
@@ -15,41 +15,31 @@ public class CountSpecialQuadruplets {
         if (nums.length <= 3)
             return count;
 
-        for (int i=3; i<nums.length; i++) {
-            int indexA = 0;
-            int indexB = 1;
-            int indexC = 2;
-            int target = i;
 
-            while (indexA != nums.length - 3 && indexB != nums.length - 2 && indexC != nums.length - 1) {
-                System.out.println("indexA: " + nums[indexA] + "    indexB: " + nums[indexB] + "    indexC: " + nums[indexC] + "    target: " + nums[target]);
+        for (int x=0; x < nums.length; x++) {
+            for (int y= x+1; y < nums.length; y++) {
 
-                if (nums[indexA] + nums[indexB] + nums[indexC] == nums[target]) {
-//                    System.out.println("Quadruplet Found");
-                    count++;
-                }
+//                if (nums[x] >= nums[y])
+//                    break;
 
-                if (indexC == target - 1) {
+                for (int z=y+1; z < nums.length; z++) {
 
-                    if (indexB == target - 2) {
+//                    if (nums[y] >= nums[z])
+//                        break;
 
-                        if (indexA == target-3) {
-                            break;
-                        } else {
-                            indexA++;
+                    for (int target=x+1; target < nums.length; target++) {
+//
+//                        if (nums[z] >= nums[target])
+//                            break;
+
+                        if (nums[x] + nums[y] + nums[z] == nums[target]) {
+                            System.out.println("x: " + nums[x] + "  y: " + nums[y] + "  z: " + nums[z] + "  targer: " + nums[target]);
+
+                            count++;
                         }
-
-                    } else {
-                        indexB++;
                     }
-
-                } else {
-                    indexC++;
                 }
             }
-
-            System.out.println();
-
         }
 
         return count;
