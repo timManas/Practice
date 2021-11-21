0dccf686d9ccb784eb99742e3488d648de99d165
Tim M <timothy.romero.manas@gmail.com>
2021-11-21 17:48:04 -0500

    Adding MinCost Climbers

---
+++ Java/src/main/java/Misc/MinCostClimbStairs/MinCostClimbStairs.java
@@ -0,0 +1,18 @@
+package Misc.MinCostClimbStairs;
+
+public class MinCostClimbStairs {
+
+    public static void main(String [] args) {
+        int [][] input = {{10,15,20}, {1,100,1,1,1,100,1,1,100,1}};
+        for (int [] i : input)
+            System.out.println("Min Cost: " + minCostClimbingStairs(i));
+    }
+
+    public static int minCostClimbingStairs(int[] cost) {
+        int minCost = 0;
+
+
+
+        return minCost;
+    }
+}
