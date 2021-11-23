f52a91f5f6226349ea8832f282ff6896a5666592
Tim M <timothy.romero.manas@gmail.com>
2021-11-22 23:43:09 -0500

    Adding min cost climb stairs

--- Java/src/main/java/Misc/MinCostClimbStairs/MinCostClimbStairs.java
+++ Java/src/main/java/Misc/MinCostClimbStairs/MinCostClimbStairs.java
@@ -3,14 +3,17 @@ package Misc.MinCostClimbStairs;
 public class MinCostClimbStairs {
 
     public static void main(String [] args) {
-//        int [][] input = {{10,15,20}, {1,100,1,1,1,100,1,1,100,1}};
-        int [][] input = { {1,100,2,2,2,100,5,6,100,7}};
+        int [][] input = {{10,15,20}, {1,100,1,1,1,100,1,1,100,1}};
+//        int [][] input = { {1,100,2,2,2,100,5,6,100,7}};
         for (int [] i : input)
             System.out.println("Min Cost: " + minCostClimbingStairs(i));
     }
 
     public static int minCostClimbingStairs(int[] cost) {
-        return findMinCost(cost, 0);
+        int one = findMinCost(cost, 0);
+        int two = findMinCost(cost, 1);
+
+        return Math.min(one + cost[0], two + cost[1]);
     }
 
     private static int findMinCost(int[] cost, int currentIndex) {
@@ -27,12 +30,12 @@ public class MinCostClimbStairs {
             int one = findMinCost(cost, currentIndex + 1);
             int two = findMinCost(cost, currentIndex + 2);
 
-            return Math.min(one, two);
+            return Math.min(one, two) + cost[currentIndex];
 
         } else if (climbOneCost > climbTwoCost) {
-            return findMinCost(cost, currentIndex + 2);
+            return findMinCost(cost, currentIndex + 2) + cost[currentIndex];
         } else if (climbOneCost < climbTwoCost) {
-            return findMinCost(cost, currentIndex + 1);
+            return findMinCost(cost, currentIndex + 1) + cost[currentIndex];
         }
 
 
