c10447fec6a56c96b99efadacdd63b215e306592
Tim M <timothy.romero.manas@gmail.com>
2021-12-02 09:01:10 -0500

    Finished Find tic tac toe winner

--- Java/src/main/java/Misc/FindWinnerTicTacToe/FindWinnerTicTacToe.java
+++ Java/src/main/java/Misc/FindWinnerTicTacToe/FindWinnerTicTacToe.java
@@ -16,13 +16,17 @@ public class FindWinnerTicTacToe {
         int [][] moves4 = {{0,0},{1,1}};
         System.out.println("Winner: " + tictactoe(moves4));
 
+        int [][] moves5 = {{2,2},{0,2},{1,0},{0,1},{2,0},{0,0}};
+        System.out.println("Winner: " + tictactoe(moves5));
+
     }
 
     public static String tictactoe(int[][] moves) {
-        // Seperate into two A and B
+        // Step1 - Seperate into two list: A and B
         List<List<Integer>> listA = new ArrayList<>();
         List<List<Integer>> listB = new ArrayList<>();
 
+        // Step2 - Populate the list
         for (int i=0; i < moves.length; i++) {
             List<Integer> tempList = new ArrayList<>();
             tempList.add(moves[i][0]);
@@ -33,73 +37,65 @@ public class FindWinnerTicTacToe {
                 listB.add(tempList);
         }
 
-        Collections.sort(listA, (list1, list2) -> list1.get(0).compareTo(list2.get(0)));
-        Collections.sort(listB, (list1, list2) -> list1.get(0).compareTo(list2.get(0)));
-
         System.out.println("A: " + listA);
         System.out.println("B: " + listB);
 
-
-
+        // Step3 - Analyze which player Won
         String movesA = checkMoves(listA, "A");
         String movesB = checkMoves(listB, "B");
 
+        // Step4 - Return player who won or draw or pending
         if (movesA.equals("A")) {
             return "A";
         } else if (movesB.equals("B"))
             return "B";
 
-        if (listA.size() + listB.size() == 9)
-            return "Draw";
-
-
-        return "Pending";
+        return listA.size() + listB.size() == 9 ? "Draw": "Pending";
     }
 
     private static String checkMoves(List<List<Integer>> list, String player) {
 
-        // Check Row
+        // Step1 - Check Row from top to bottom
         Map<Integer,Integer> rowMap = new LinkedHashMap<>();
         for (int x=0; x < list.size(); x++) {
-            System.out.println("Row:" + list.get(x).get(0));
             if (rowMap.containsKey(list.get(x).get(0)))
-                rowMap.put(list.get(x).get(0), rowMap.get(x) + 1);
+                rowMap.put(list.get(x).get(0), rowMap.get(list.get(x).get(0)) + 1);
             else
                 rowMap.put(list.get(x).get(0), 1);
 
-        }
-        for (Map.Entry<Integer, Integer> mapEntry : rowMap.entrySet()) {
-            if (mapEntry.getKey() == 3)
+            // Check if each row was filled
+            if (rowMap.get(list.get(x).get(0)) == 3)
                 return player;
+
         }
 
-        // Check Column
+
+        // Step2 - Check Column
         Map<Integer,Integer> colMap = new LinkedHashMap<>();
         for (int y=0; y < list.size(); y++) {
-            System.out.println("Column: " + list.get(y).get(1));
-            if (rowMap.containsKey(list.get(y).get(1)))
-                rowMap.put(list.get(y).get(1), rowMap.get(y) + 1);
+            if (colMap.containsKey(list.get(y).get(1)))
+                colMap.put(list.get(y).get(1), colMap.get(list.get(y).get(1)) + 1);
             else
-                rowMap.put(list.get(y).get(1), 1);
-        }
+                colMap.put(list.get(y).get(1), 1);
 
-        for (Map.Entry<Integer, Integer> mapEntry : colMap.entrySet()) {
-            if (mapEntry.getKey() == 3)
+            if (colMap.get(list.get(y).get(1)) == 3)
                 return player;
         }
 
 
-        // Check Diagonal TopLeft-BottomRight
+        // Step3 - Check Diagonal TopLeft-BottomRight
         Set<Integer> setD_TLBR = new TreeSet<>();
         for (int a=0; a<list.size(); a++) {
             if (list.get(a).get(0) != list.get(a).get(1))
                 continue;
             setD_TLBR.add(list.get(a).get(0));
-        }
-        if (setD_TLBR.size() >= 3)
-            return player;
 
-        // Check Diagonal BottomLeft-TopRight
+            if (setD_TLBR.size() >= 3)
+                return player;
+        }
+
+
+        // Step4 - Check Diagonal BottomLeft-TopRight
         Set<Integer> setD_BLTR = new TreeSet<>();
         for (int b=0; b<list.size(); b++) {
             if (list.get(b).get(0) == 0 && list.get(b).get(1) == 2) {
@@ -109,9 +105,11 @@ public class FindWinnerTicTacToe {
             } else if (list.get(b).get(0) == 2 && list.get(b).get(1) == 0) {
                 setD_BLTR.add(list.get(b).get(0));
             }
+
+            if (setD_BLTR.size() >= 3)
+                return player;
         }
-        if (setD_BLTR.size() >= 3)
-            return player;
+
 
 
 
