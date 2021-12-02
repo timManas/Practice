7ce1f946d097d91c54f49b6dabe9f7c91514c22c
Tim M <timothy.romero.manas@gmail.com>
2021-12-01 22:45:46 -0500

    Adding Find Winner tic tac toee

--- Java/src/main/java/Misc/FindWinnerTicTacToe/FindWinnerTicTacToe.java
+++ Java/src/main/java/Misc/FindWinnerTicTacToe/FindWinnerTicTacToe.java
@@ -2,6 +2,8 @@ package Misc.FindWinnerTicTacToe;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
 
 public class FindWinnerTicTacToe {
     public static void main(String [] args) {
@@ -21,8 +23,8 @@ public class FindWinnerTicTacToe {
 
     public static String tictactoe(int[][] moves) {
         // Seperate into two A and B
-        List<List> listA = new ArrayList<>();
-        List<List> listB = new ArrayList<>();
+        List<List<Integer>> listA = new ArrayList<>();
+        List<List<Integer>> listB = new ArrayList<>();
 
         for (int i=0; i < moves.length; i++) {
             List<Integer> tempList = new ArrayList<>();
@@ -38,32 +40,67 @@ public class FindWinnerTicTacToe {
 
 
 
-        String movesA = checkMoves(listA);
-        String movesB = checkMoves(listB);
+        String movesA = checkMoves(listA, "A");
+        String movesB = checkMoves(listB, "B");
 
-        // Check Row
-        for (int i=0; i)
+        if (movesA.equals("A")) {
+            return "A";
+        } else if (movesB.equals("B"))
+            return "B";
 
-        // Check Column
+        if (listA.size() + listB.size() == 9)
+            return "Draw";
 
-        return "";
+
+        return "Pending";
     }
 
-    private static String checkMoves(List<List> list) {
+    private static String checkMoves(List<List<Integer>> list, String player) {
+
 
         // Check Row
+        Set<Integer> setRow = new TreeSet<>();
         for (int x=0; x < list.size(); x++) {
-            for (int y=0; y < list.get(x).size(); y++) {
-
-            }
+            System.out.println("Row:" + list.get(x).get(0));
+            setRow.add(list.get(x).get(0));
         }
+        if (setRow.size() >= 3)
+            return player;
 
         // Check Column
-        for (int y=0; y < list)
+        Set<Integer> setCol = new TreeSet<>();
+        for (int y=0; y < list.size(); y++) {
+            System.out.println("Column: " + list.get(y).get(1));
+            setCol.add(list.get(y).get(1));
+        }
+        if (setRow.size() >= 3)
+            return player;
 
         // Check Diagonal TopLeft-BottomRight
+        Set<Integer> setD_TLBR = new TreeSet<>();
+        for (int a=0; a<list.size(); a++) {
+            if (list.get(a).get(0) != list.get(a).get(1))
+                continue;
+            setD_TLBR.add(list.get(a).get(0));
+        }
+        if (setD_TLBR.size() >= 3)
+            return player;
 
         // Check Diagonal BottomLeft-TopRight
+        Set<Integer> setD_BLTR = new TreeSet<>();
+        for (int b=0; b<list.size(); b++) {
+            if (list.get(b).get(0) == 0 && list.get(b).get(1) == 2) {
+                setD_BLTR.add(list.get(b).get(0));
+            } else if (list.get(b).get(0) == 1 && list.get(b).get(1) == 1) {
+                setD_BLTR.add(list.get(b).get(0));
+            } else if (list.get(b).get(0) == 2 && list.get(b).get(1) == 0) {
+                setD_BLTR.add(list.get(b).get(0));
+            }
+        }
+        if (setD_BLTR.size() >= 3)
+            return player;
+
+
 
         return "Pending";
     }
