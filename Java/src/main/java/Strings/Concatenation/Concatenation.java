5dffb5b4aac7b03309b016b5c529d31860258724
Tim M <timothy.romero.manas@gmail.com>
2021-12-05 09:57:05 -0500

    Adding concatenation

---
+++ Java/src/main/java/Strings/Concatenation/Concatenation.java
@@ -0,0 +1,32 @@
+package Strings.Concatenation;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Concatenation {
+    public static void main(String [] args) {
+        int [] arr = {85};
+        int [][] pieces = {{85}};
+        System.out.println("Can Form Array: " + canFormArray(arr, pieces));
+
+        int [] arr1 = {85};
+        int [][] pieces1 = {{85}};
+        System.out.println("Can Form Array: " + canFormArray(arr1, pieces1));
+
+        int [] arr2 = {85};
+        int [][] pieces2 = {{85}};
+        System.out.println("Can Form Array: " + canFormArray(arr2, pieces2));
+
+        int [] arr3 = {85};
+        int [][] pieces3 = {{85}};
+        System.out.println("Can Form Array: " + canFormArray(arr3, pieces3));
+
+    }
+
+    public static boolean canFormArray(int[] arr, int[][] pieces) {
+        List<Integer> list = new ArrayList<>();
+        Arrays.
+
+        return true;
+    }
+}
