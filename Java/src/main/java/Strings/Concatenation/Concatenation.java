d7093d37c1883335805e9fbf9b1d50989e7dc884
Tim M <timothy.romero.manas@gmail.com>
2021-12-06 08:13:56 -0500

    Adding Concatenation map object

--- Java/src/main/java/Strings/Concatenation/Concatenation.java
+++ Java/src/main/java/Strings/Concatenation/Concatenation.java
@@ -1,7 +1,6 @@
 package Strings.Concatenation;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class Concatenation {
     public static void main(String [] args) {
@@ -9,23 +8,34 @@ public class Concatenation {
         int [][] pieces = {{85}};
         System.out.println("Can Form Array: " + canFormArray(arr, pieces));
 
-        int [] arr1 = {85};
-        int [][] pieces1 = {{85}};
+        int [] arr1 = {15,88};
+        int [][] pieces1 = {{88}};
         System.out.println("Can Form Array: " + canFormArray(arr1, pieces1));
 
-        int [] arr2 = {85};
-        int [][] pieces2 = {{85}};
+        int [] arr2 = {49,18,16};
+        int [][] pieces2 = {{16,18,49}};
         System.out.println("Can Form Array: " + canFormArray(arr2, pieces2));
 
-        int [] arr3 = {85};
-        int [][] pieces3 = {{85}};
+        int [] arr3 = {91,4,64,78};
+        int [][] pieces3 = {{78}, {4,64}, {91}};
         System.out.println("Can Form Array: " + canFormArray(arr3, pieces3));
 
     }
 
     public static boolean canFormArray(int[] arr, int[][] pieces) {
-        List<Integer> list = new ArrayList<>();
-        Arrays.
+        // Create Map to store pieces array
+        Map<String, List<Integer>> map = new TreeMap<>();
+        for (int i=0; i < pieces.length; i++) {
+            List<Integer> tempList = new ArrayList<>();
+            StringBuilder sb = new StringBuilder();
+            for (int j=0; j < pieces[i].length; j++) {
+                tempList.add(pieces[i][j]);
+                sb.append(pieces[i][j] + " ");
+            }
+
+            map.put(sb.toString().trim(), tempList);
+        }
+        System.out.println("Map:" + map);
 
         return true;
     }
