553475d9fb48947009b0264cd919c87c64a8a5a1
Tim M <timothy.romero.manas@gmail.com>
2021-12-09 09:02:44 -0500

    Adding Degree of array

--- Java/src/main/java/Lists/DegreeOfArray/DegreeOfArray.java
+++ Java/src/main/java/Lists/DegreeOfArray/DegreeOfArray.java
@@ -1,9 +1,6 @@
 package Lists.DegreeOfArray;
 
-import java.util.ArrayList;
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class DegreeOfArray {
     public static void main(String [] args) {
@@ -13,76 +10,34 @@ public class DegreeOfArray {
     }
 
     public static int findShortestSubArray(int[] nums) {
+        Map<Integer, Integer> occurenceMap = new LinkedHashMap<>();
+        Map<Integer, Integer> startIndexMap = new LinkedHashMap<>();
+        Map<Integer, Integer> lastIndexMap = new LinkedHashMap<>();
 
-        // Create map and populate
-        Map<Integer, Integer> map = new LinkedHashMap<>();
-        for (int i : nums){
-            if (map.containsKey(i))
-                map.put(i, map.get(i) + 1);
-            else
-                map.put(i, 1);
-        }
-        // Find largest occurence
-        List<Integer> largestElementList = new ArrayList<>();
-        int largestOccurence = 0;
-        for (Map.Entry<Integer, Integer> mapEntry : map.entrySet()) {
-
-            if (mapEntry.getValue() > largestOccurence) {
-                largestOccurence = mapEntry.getValue();
-                largestElementList.clear();
-                largestElementList.add(mapEntry.getKey());
-
-            } else if (mapEntry.getValue() == largestOccurence) {
-                largestElementList.add(mapEntry.getKey());
+        // Traverse nums List
+        for (int i=0; i<nums.length; i++) {
+            int current = nums[i];
+            if (occurenceMap.containsKey(current)) {
+                occurenceMap.put(current, occurenceMap.get(current) + 1);
+            } else {
+                occurenceMap.put(current, 1);
+                startIndexMap.put(current, i);
             }
+
+            // Add to lastIndex Map
+            lastIndexMap.put(current, i);
         }
 
-        System.out.println("Largest Element: " + largestElementList + "    Count: " + largestOccurence);
-        int minSubArrayLength = Integer.MAX_VALUE;
-        // Find the largest SubArray Length
-        List<Integer> subList = new ArrayList<>();
-        for (int i=0; i < largestElementList.size(); i++) {
-            int target = largestElementList.get(i);
-
-            System.out.println("Looking for Element: " + target);
-
-            outLoop:
-            for (int start=0; start < nums.length; start++) {
-                subList.clear();
-                int count = 0;
-
-                System.out.println("Start: " + nums[start]);
-                subList.add(nums[start]);
-                if (nums[start] == target)
-                    ++count;
-
-                if (count == largestOccurence) {
-                    System.out.println("SubList: " + subList + " | Count: " + count);
-                    minSubArrayLength = Math.min(minSubArrayLength, subList.size());
-                    break;
-                }
-
-                inLoop:
-                for (int end=start + 1; end < nums.length; end++) {
-                    System.out.println("End: " + nums[end]);
-
-                    subList.add(nums[end]);
-                    if (nums[end] == target)
-                        ++count;
-
-                    if (count == largestOccurence) {
-                        System.out.println("SubList: " + subList + " | Count: " + count);
-                        minSubArrayLength = Math.min(minSubArrayLength, subList.size());
-                        break inLoop;
-                    }
+        System.out.println("Occurence Map: " + occurenceMap);
+        System.out.println("StartIndex Map: " + startIndexMap);
+        System.out.println("EndIndex Map: " + lastIndexMap);
 
 
-                }
-            }
-        }
+        // Traverse Occurence Map and find smallest subarray
 
 
 
-        return minSubArrayLength;
+
+        return 0;
     }
 }
