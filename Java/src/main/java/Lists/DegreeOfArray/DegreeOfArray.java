6ac664d328df3f3ffd82ff838951f5a8ff503b5b
Tim M <timothy.romero.manas@gmail.com>
2021-12-09 19:51:51 -0500

    Finished Degree of Array

--- Java/src/main/java/Lists/DegreeOfArray/DegreeOfArray.java
+++ Java/src/main/java/Lists/DegreeOfArray/DegreeOfArray.java
@@ -10,13 +10,20 @@ public class DegreeOfArray {
     }
 
     public static int findShortestSubArray(int[] nums) {
+        // Step1 - Create variable to store the largest occurence & shortestSubArrayLength
+        int largestOccurence = 0;
+        int shortestSubArrayLength = Integer.MAX_VALUE;
+
+        // Step2 - Create map to store the occurence, the start position, end position list
         Map<Integer, Integer> occurenceMap = new LinkedHashMap<>();
         Map<Integer, Integer> startIndexMap = new LinkedHashMap<>();
         Map<Integer, Integer> lastIndexMap = new LinkedHashMap<>();
 
-        // Traverse nums List
+        // Step3 - Traverse nums List & populate maps
         for (int i=0; i<nums.length; i++) {
             int current = nums[i];
+
+            // If contains add to counter
             if (occurenceMap.containsKey(current)) {
                 occurenceMap.put(current, occurenceMap.get(current) + 1);
             } else {
@@ -24,20 +31,31 @@ public class DegreeOfArray {
                 startIndexMap.put(current, i);
             }
 
-            // Add to lastIndex Map
+            // keep track of the largest occurence
+            largestOccurence = occurenceMap.get(current) > largestOccurence ? occurenceMap.get(current) : largestOccurence;
+
+            // Add last known position of this element
             lastIndexMap.put(current, i);
         }
 
-        System.out.println("Occurence Map: " + occurenceMap);
+        System.out.println("Occurence Map: " + occurenceMap + "     LargestOccurence: " + largestOccurence);
         System.out.println("StartIndex Map: " + startIndexMap);
         System.out.println("EndIndex Map: " + lastIndexMap);
 
+        // Step4 - Traverse Occurence Map and find smallest subarray using startIndex and endIndex list
+        for (Map.Entry<Integer, Integer> mapEntry : occurenceMap.entrySet()) {
+            int key = mapEntry.getKey();
+            int value = mapEntry.getValue();
 
-        // Traverse Occurence Map and find smallest subarray
+            if (value != largestOccurence)
+                continue;
+
+            int lengthBetweenStartEnd = Math.abs(lastIndexMap.get(key) - startIndexMap.get(key));
+            shortestSubArrayLength = (shortestSubArrayLength > lengthBetweenStartEnd) ? lengthBetweenStartEnd : shortestSubArrayLength;
+        }
 
 
-
-
-        return 0;
+        // Add one since it is length
+        return shortestSubArrayLength + 1;
     }
 }
