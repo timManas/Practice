1def5f304ea2bbc82d920a35d229142a2731b8a9
Tim M <timothy.romero.manas@gmail.com>
2021-12-14 09:16:06 -0500

    Adding Max Cons Ones

--- Java/src/main/java/Trees/MaxConsecutiveOnes/MaxConsecutiveOnes.java
+++ Java/src/main/java/Trees/MaxConsecutiveOnes/MaxConsecutiveOnes.java
@@ -1,22 +1,26 @@
 package Trees.MaxConsecutiveOnes;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 public class MaxConsecutiveOnes {
     public static void main(String [] args) {
-        int [][] input = {{1,2,3,4,5,6,7,8,9,10,11,12,13,14,15},{1,1,0,1,1,1}, {1,0,1,1,0,1}};
+        int [][] input = {{1,1,0,1,1,1}, {1,0,1,1,0,1}, {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15}};
         for (int [] i : input)
-            System.out.println("Find Max Consecutive Ones: " + findMaxConsecutiveOnes(i));
+            System.out.println("Find Max Consecutive Ones: " + findMaxConsecutiveOnes(i) + "\n");
     }
 
     public static int findMaxConsecutiveOnes(int[] nums) {
-        int max = 0;
+        Map<String, Integer> map = new LinkedHashMap<>();
+        map.put("max", 0);
+        map.put("consecutive", 0);
+        map.put("prevElement", -1);
+        traverseArray(nums, 0, map);
 
-        int consecutiveOnes = 0;
-        traverseArray(nums, 0);
-
-        return max;
+        return map.get("max");
     }
 
-    private static void traverseArray(int[] nums, int index) {
+    private static void traverseArray(int[] nums, int index, Map<String, Integer> map) {
 
         if (index >= nums.length)
             return;
@@ -24,11 +28,27 @@ public class MaxConsecutiveOnes {
         int leftIndex = (2*index) + 1;
         int rightIndex = (2*index) + 2;
 
-
-        traverseArray(nums, leftIndex);
         System.out.println("Current: " + nums[index]);
-        traverseArray(nums, rightIndex);
 
+        if (nums[index] == 1) {
+
+            if (map.get("prevElement") == 1) {
+                map.put("consecutive", map.get("consecutive") + 1);
+            } else {
+                map.put("consecutive", 1);
+            }
+        } else {
+            if (map.get("prevElement") == 1) {
+                if (map.get("max") < map.get("consecutive")) {
+                    map.put("max", map.get("consecutive"));
+                    map.put("consecutive", 0);      // Reset
+                }
+            }
+        }
+        map.put("prevElement", nums[index]);
+
+        traverseArray(nums, leftIndex, map);
+        traverseArray(nums, rightIndex, map);
 
     }
 }
