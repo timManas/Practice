239a8792cfeafba294b02d2b3f63c63c286bed77
Tim M <timothy.romero.manas@gmail.com>
2021-12-15 08:10:28 -0500

    Adding Matrix obtained by Rotation

---
+++ Java/src/main/java/Lists/DetermineMatrixObtainedByRotation/DetermineMatrixObtainedByRotation.java
@@ -0,0 +1,45 @@
+package Lists.DetermineMatrixObtainedByRotation;
+
+import java.util.Map;
+import java.util.TreeMap;
+
+public class DetermineMatrixObtainedByRotation {
+    public static void main(String [] args) {
+        int [][] mat = {{0,1}, {1,0}};
+        int [][] target = {{1,0}, {0,1}};
+        System.out.println("Find Rotation: " + findRotation(mat, target));
+
+        int [][] mat1 = {{0,1}, {1,1}};
+        int [][] target1 = {{1,0}, {0,1}};
+        System.out.println("Find Rotation: " + findRotation(mat1, target1));
+    }
+
+    public static boolean findRotation(int[][] mat, int[][] target) {
+
+        // Check if there is the same amount of 1's
+        Map<Integer, Integer> matMap = populateMap(mat);
+        Map<Integer, Integer> targetMap = populateMap(target);
+
+        if (matMap.size() != targetMap.size() || matMap.get(0) != targetMap.get(0) || matMap.get(1) != targetMap.get(1))
+            return false;
+
+
+
+        return true;
+    }
+
+    private static Map<Integer, Integer> populateMap(int[][] mat) {
+        Map<Integer, Integer> matMap = new TreeMap<>();
+
+        for (int i=0; i<mat.length; i++) {
+            for (int j=0; j<mat[i].length; j++) {
+                if (matMap.containsKey(mat[i][j]))
+                    matMap.put(mat[i][j], matMap.get(mat[i][j]) + 1);
+                else
+                    matMap.put(mat[i][j], 1);
+            }
+        }
+
+        return matMap;
+    }
+}
