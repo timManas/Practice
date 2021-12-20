7a5d7842e0f9621587337038d1733110b3fb4c0e
Tim M <timothy.romero.manas@gmail.com>
2021-12-19 23:05:53 -0500

    adding Check if string is prefix array

---
+++ Java/src/main/java/Strings/CheckIfStringIsPrefixArray/CheckIfStringIsPrefixArray.java
@@ -0,0 +1,20 @@
+package Strings.CheckIfStringIsPrefixArray;
+
+public class CheckIfStringIsPrefixArray {
+    public static void main(String [] args) {
+        String s = "iloveleetcode";
+        String [] words = {"i","love","leetcode","apples"};
+        System.out.println("IsPrefixString: " + isPrefixString(s, words));
+
+        String s1 = "iloveleetcode";
+        String [] words1 = {"apples","i","love","leetcode"};
+        System.out.println("IsPrefixString: " + isPrefixString(s1, words1));
+    }
+
+    public static boolean isPrefixString(String s, String[] words) {
+
+
+
+        return true;
+    }
+}
