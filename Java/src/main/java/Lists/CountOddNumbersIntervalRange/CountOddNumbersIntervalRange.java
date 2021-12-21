e5fbe5a7516585303ace8bbf689dc20c95a8e793
Tim M <timothy.romero.manas@gmail.com>
2021-12-21 09:59:10 -0500

    Finished count odd numbers intervals range

--- Java/src/main/java/Lists/CountOddNumbersIntervalRange/CountOddNumbersIntervalRange.java
+++ Java/src/main/java/Lists/CountOddNumbersIntervalRange/CountOddNumbersIntervalRange.java
@@ -13,26 +13,30 @@ public class CountOddNumbersIntervalRange {
         int low2 = 327296043;
         int high2 = 769434803;
         System.out.println("CountOdds: " + countOdds(low2,high2));
+
+        int low3 = 21;
+        int high3 = 22;
+        System.out.println("CountOdds: " + countOdds(low3,high3));
     }
 
     public static int countOdds(int low, int high) {
         int count = 0;
 
-        while (low <= high) {
+        // Solution # 2 - Math
+        int numElementsBetweenLowHigh = (high - low) + 1;
 
-            if (low == high && low % 2 == 1 && high % 2 == 1) {
-                ++count;
-                break;
-            }
-
-            if (low % 2 == 1)
-                ++count;
-            if (high % 2 == 1)
-                ++count;
-
-            low++;
-            high--;
+        if (low % 2 == 0 && high % 2 == 0) {        // Both Odd
+           return (numElementsBetweenLowHigh / 2);
+        } else if (low % 2 == 0 && high % 2 == 1) {     // One of them is Odd and the other even
+            return (numElementsBetweenLowHigh / 2);
+        } else if (low % 2 == 1 && high % 2 == 0) {     // One of them is Odd and the other even
+            return (numElementsBetweenLowHigh / 2);
+        } else if (low % 2 == 1 && high % 2 == 1) {     // Both Even
+            return (numElementsBetweenLowHigh / 2) + 1;
         }
+
+
+
 
         return count;
     }
