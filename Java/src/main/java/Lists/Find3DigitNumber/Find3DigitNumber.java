5978ce2a6c6d6362feeb7abd44fb3d9da3624c65
Tim M <timothy.romero.manas@gmail.com>
2021-12-23 12:16:51 -0500

    Adding 3 Digit numbers

--- Java/src/main/java/Lists/Find3DigitNumber/Find3DigitNumber.java
+++ Java/src/main/java/Lists/Find3DigitNumber/Find3DigitNumber.java
@@ -5,7 +5,7 @@ import java.util.*;
 
 public class Find3DigitNumber {
     public static void main(String [] args) {
-        int [][] input = {{2,1,3,0}, {2,2,8,8,2}, {3,7,5}};
+        int [][] input = {{0,2,0,0}, {2,1,3,0}, {2,2,8,8,2}, {3,7,5} };
         for (int[] i : input) {
             int [] output = findEvenNumbers(i);
             printArray(output);
@@ -15,23 +15,31 @@ public class Find3DigitNumber {
 
 
     public static int[] findEvenNumbers(int[] digits) {
+        // Step1 - Create set to store unique numbers
         Set<Integer> set = new TreeSet<>();
 
-        // Sort digits
+        // Step2 - Sort digits array
         Arrays.sort(digits);
         printArray(digits);
 
-        StringBuilder sb = new StringBuilder();
+        // Step3 - Traverse Digits array
         int start = digits[0] == 0 ? 1 : 0;
-
         for (int i=start; i < digits.length; i++) {
             for (int j=0; j < digits.length; j++) {
                 for (int k=0; k < digits.length; k++) {
 
-                    if (digits[i] == digits[j] || digits[i] == digits[k] || digits[j] == digits[k])
+                    // Make sure the indexes are NOT the same ...
+                    if (i == j || i == k || j == k)
                         continue;
 
+                    // Find the number created
                     int number = 100 * digits[i] + 10 * digits[j] + digits[k];
+
+                    // Check if the number is 3 digits
+                    if (99 > number || number > 999)
+                        continue;
+
+                    // Add to set
                     if (number % 2 == 0)
                         set.add(number);
                     System.out.println(number);
@@ -40,7 +48,7 @@ public class Find3DigitNumber {
         }
 
 
-        // Convert to array and return
+        // Step4 - Convert to array and return
         List<Integer> list = new ArrayList<>(set);
         int [] output = new int[list.size()];
         for (int i=0; i<list.size(); i++){
