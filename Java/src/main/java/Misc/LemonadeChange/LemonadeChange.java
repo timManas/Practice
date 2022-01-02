ae0c6cfcccc42be508ffdffe7bfab4abdba4c4ea
Tim M <timothy.romero.manas@gmail.com>
2022-01-02 18:56:46 -0500

    Adding change

--- Java/src/main/java/Misc/LemonadeChange/LemonadeChange.java
+++ Java/src/main/java/Misc/LemonadeChange/LemonadeChange.java
@@ -1,7 +1,6 @@
 package Misc.LemonadeChange;
 
-import java.util.Map;
-import java.util.TreeMap;
+import java.util.*;
 
 public class LemonadeChange {
     public static void main(String [] args) {
@@ -14,9 +13,29 @@ public class LemonadeChange {
         int cost = 5;
 
         Map<Integer, Integer> ledgerMap = new TreeMap<>();
+
         for (int i=0; i<bills.length; i++) {
             int payment = bills[i];
             int change = payment - cost;
+
+            // Return Change to customer
+            List<Integer> billList = new ArrayList<>(ledgerMap.keySet());
+            System.out.println("Bill List: " + billList);
+            for (int j=billList.size()-1; j >= 0; j--) {
+                change = ledgerMap[i] `
+            }
+
+            if (change != 0)
+                return false;
+
+
+
+            // Add Bill to Ledger
+            if (ledgerMap.containsKey(payment))
+                ledgerMap.put(payment, ledgerMap.get(payment) + 1);
+            else
+                ledgerMap.put(payment,1);
+            System.out.println("Ledger: " + ledgerMap);
 
 
 
