c068232de8dba5b479d17a92972391e3d31940c9
Tim M <timothy.romero.manas@gmail.com>
2022-01-03 20:45:18 -0500

    Modifying code

--- Java/src/main/java/Misc/LemonadeChange/LemonadeChange.java
+++ Java/src/main/java/Misc/LemonadeChange/LemonadeChange.java
@@ -17,17 +17,26 @@ public class LemonadeChange {
         for (int i=0; i<bills.length; i++) {
             int payment = bills[i];
             int change = payment - cost;
+            System.out.println("Initial Change: " + change);
 
-            // Return Change to customer
-            List<Integer> billList = new ArrayList<>(ledgerMap.keySet());
-            System.out.println("Bill List: " + billList);
-            for (int j=billList.size()-1; j >= 0; j--) {
-                change = ledgerMap[i] `
+
+            if (change > 0) {
+                // Return Change to customer
+                List<Integer> billList = new ArrayList<>(ledgerMap.keySet());
+                for (int x=billList.size()-1; x >= 0; x--) {
+                    int rounded = (change / billList.get(x)) * billList.get(x);
+                    int billTotal = billList.get(x) * ledgerMap.get(billList.get(x));
+                    int billsUsed = 0;
+                    if (rounded > billTotal) {
+                        billsUsed = (rounded / billList.get(x));
+                    } else {
+                        billsUsed = ledgerMap.get(billList.get(x));
+                    }
+                    ledgerMap.put(billList.get(x), billsUsed - 1);
+                    change = change - (billsUsed * billList.get(x));
+                    System.out.println("Change: " + change);
+                }
             }
-
-            if (change != 0)
-                return false;
-
 
 
             // Add Bill to Ledger
@@ -38,9 +47,8 @@ public class LemonadeChange {
             System.out.println("Ledger: " + ledgerMap);
 
 
-
-
-
+            if (change != 0)
+                return false;
 
         }
 
