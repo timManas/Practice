228bac6603ca564f0dc60c91c9ddd40c44294564
Tim M <timothy.romero.manas@gmail.com>
2022-01-07 01:06:58 -0500

    Close one for GCD

--- Java/src/main/java/Strings/GreatestCommonDivisorStrings/GreatestCommonDivisorStrings.java
+++ Java/src/main/java/Strings/GreatestCommonDivisorStrings/GreatestCommonDivisorStrings.java
@@ -2,17 +2,25 @@ package Strings.GreatestCommonDivisorStrings;
 
 public class GreatestCommonDivisorStrings {
     public static void main(String [] args) {
-//        String str1 = "ABCABC";
-//        String str2 = "ABC";
-//        System.out.println("GCD Strings: " + gcdOfStrings(str1, str2) + "\n");
-//
-//        String str3 = "ABABAB";
-//        String str4 = "ABAB";
-//        System.out.println("GCD Strings: " + gcdOfStrings(str3, str4) + "\n");
+        String str1 = "ABCABC";
+        String str2 = "ABC";
+        System.out.println("GCD Strings: " + gcdOfStrings(str1, str2) + "\n");
+
+        String str3 = "ABABAB";
+        String str4 = "ABAB";
+        System.out.println("GCD Strings: " + gcdOfStrings(str3, str4) + "\n");
 
         String str5 = "LEET";
         String str6 = "CODE";
         System.out.println("GCD Strings: " + gcdOfStrings(str5, str6) + "\n");
+
+        String str7 = "AA";
+        String str8 = "A";
+        System.out.println("GCD Strings: " + gcdOfStrings(str7, str8) + "\n");
+
+        String str9 = "ABABABAB";
+        String str10 = "ABAB";
+        System.out.println("GCD Strings: " + gcdOfStrings(str9, str10) + "\n");
     }
 
     public static String gcdOfStrings(String str1, String str2) {
@@ -20,8 +28,8 @@ public class GreatestCommonDivisorStrings {
         String longStr = str1.length() <= str2.length() ? str2 : str1;
         String shortStr = str1.length() <= str2.length() ? str1 : str2;
 
-        int divider = 1;
-        while (divider < longStr.length()) {
+        int divider = longStr.length() - 1;
+        while (divider >= 0 ) {
             String subStr = longStr.substring(0, divider + 1);
 
             String[] split = longStr.split(subStr, -1);
@@ -38,7 +46,7 @@ public class GreatestCommonDivisorStrings {
                 isBlank = true;
             }
 
-            if (isBlank) {
+            if (isBlank && shortStr.indexOf(subStr) != -1) {
                 System.out.println("SubString Found: " + subStr);
 
                 String [] splitShort = shortStr.split(subStr, -1);
@@ -50,7 +58,7 @@ public class GreatestCommonDivisorStrings {
 
                 return subStr;
             }
-            ++divider;
+            --divider;
         }
 
         return gcd;
