97d817a50dd0aa4cd6d40496d8e81ce8ccb59ef3
Tim M <timothy.romero.manas@gmail.com>
2022-01-10 20:58:18 -0500

    Finished Check if String Decomposable

--- Java/src/main/java/Strings/CheckIfStringDecomposable/CheckIfStringDecomposable.java
+++ Java/src/main/java/Strings/CheckIfStringDecomposable/CheckIfStringDecomposable.java
@@ -2,39 +2,54 @@ package Strings.CheckIfStringDecomposable;
 
 public class CheckIfStringDecomposable {
     public static void main(String [] args) {
-        String [] input = {"000111000", "00011111222", "011100022233"};
+        String [] input = {"66666666666677722","000111000", "00011111222", "011100022233"};
         for (String i : input)
             System.out.println("Is String Decomposable: " + isDecomposable(i) + "\n");
     }
 
     public static boolean isDecomposable(String input) {
 
+        // Step1 - Create index to track movement
         boolean containsLength2 = false;
-
         int startIndex = 0;
         char startChar = input.charAt(startIndex);
         char endChar;
         char nextChar = 0;
         StringBuilder sb = new StringBuilder();
+
+        // Step2 - Traverse input string one letter at a time
         for (int endIndex=0; endIndex < input.length() - 1; endIndex++) {
             endChar = input.charAt(endIndex);
             nextChar = input.charAt(endIndex + 1);
 
-            if (startChar == endChar) {
-                sb.append(endChar);
-            }
+            // Add current letter to StringBuilder
+            sb.append(endChar);
 
+
+            // Step3 - Check if the nextChar is still valid
             if (startChar != nextChar) {
                 System.out.println("SubString: " + sb.toString());
-                if (sb.length() == 1)
+                if (sb.length() == 1) {
                     return false;
-                if (sb.length() == 2)
+                } else if (sb.length() == 2) {
                     containsLength2 = true;
-                if (sb.length() > 3)
-                    return false;
+                } else if (sb.length() > 3) {
+
+                    if (sb.length() % 3 != 0) {
+                        int remainder = sb.length() - 2;
+                        containsLength2 = true;
+
+                        if (remainder % 3 != 0 || remainder < 3) {
+                            System.out.println("Remainder: " + remainder);
+                            return false;
+                        }
+                    }
+
+                }
 
 
-                // Updates indexes
+
+                // Step4 - Updates indexes
                 sb = new StringBuilder();
                 startIndex = endIndex + 1;
                 startChar = input.charAt(startIndex);
@@ -45,8 +60,24 @@ public class CheckIfStringDecomposable {
 
         sb.append(nextChar);
         System.out.println("Final SubString: " + sb.toString());
-        if (sb.length() == 1 || sb.length() > 3)
+        if (sb.length() == 1) {
             return false;
+        } else if (sb.length() == 2) {
+            containsLength2 = true;
+        } else if (sb.length() > 3) {
+
+            if (sb.length() % 3 != 0) {
+                int remainder = sb.length() - 2;
+                containsLength2 = true;
+
+                if (remainder % 3 != 0 || remainder < 3) {
+                    System.out.println("Remainder: " + remainder);
+                    return false;
+                }
+            }
+
+        }
+
 
         if (!containsLength2)
             return false;
