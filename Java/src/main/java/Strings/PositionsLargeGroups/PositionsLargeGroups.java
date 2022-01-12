838cbfbe0869348fb363e8aeb007ea92e3d8b760
Tim M <timothy.romero.manas@gmail.com>
2022-01-11 20:22:23 -0500

    Finished docs

--- Java/src/main/java/Strings/PositionsLargeGroups/PositionsLargeGroups.java
+++ Java/src/main/java/Strings/PositionsLargeGroups/PositionsLargeGroups.java
@@ -11,24 +11,30 @@ public class PositionsLargeGroups {
     }
 
     public static List<List<Integer>> largeGroupPositions(String input) {
-        List<List<Integer>> output = new ArrayList<>();
-        List<Integer> positionList = new ArrayList<>();
 
+        // Step1 - Create output variable
+        List<List<Integer>> output = new ArrayList<>();
+        List<Integer> positionList;
+
+        // Step2 - Use Strinbuilder to build String
         StringBuilder sb = new StringBuilder();
         int startIndex = 0;
 
+        // Step3 - Traverse letter by letter
         for (int i=0; i<input.length(); i++) {
             char letter = input.charAt(i);
             char startLetter = input.charAt(startIndex);
 
+            // Step4 - Check if the letter is the same as the starting letter
             if (startLetter == letter) {
-                sb.append(letter);
+                sb.append(letter);              // Add to stringbuilder
 
+                // Check if the last element in list. Add to Output if length >= 3
                 if (i == input.length() - 1) {
                     positionList = new ArrayList<>();
                     positionList.add(startIndex);
                     positionList.add(i);
-                    if (Math.abs(startIndex - i) + 1 >= 3)
+                    if (sb.toString().length() >= 3)
                         output.add(positionList);
                     System.out.println("Last Group Found: " + sb.toString() + " positionList:" + positionList);
                 }
@@ -38,11 +44,11 @@ public class PositionsLargeGroups {
                 positionList = new ArrayList<>();
                 positionList.add(startIndex);
                 positionList.add(i - 1);
-                if (Math.abs(startIndex - i)>= 3)
+                if (sb.toString().length() >= 3)    //Add to Output if length >= 3
                     output.add(positionList);
                 System.out.println("Group Found: " + sb.toString() + " positionList:" + positionList);
 
-                // Update startIndex
+                // Step5 - Update startIndex
                 startIndex = i;
                 sb = new StringBuilder();
                 sb.append(input.charAt(startIndex));
