6e0b4f705c81f52b7e6800ca3a037b1bf3380f9b
Tim M <timothy.romero.manas@gmail.com>
2022-01-16 21:38:24 -0500

    Finished Duplicate zeroes

--- Java/src/main/java/Lists/DuplicateZeroes/DuplicateZeroes.java
+++ Java/src/main/java/Lists/DuplicateZeroes/DuplicateZeroes.java
@@ -1,8 +1,10 @@
 package Lists.DuplicateZeroes;
 
+import java.util.Stack;
+
 public class DuplicateZeroes {
     public static void main(String [] args) {
-        int [][] input = {{0,1,7,6,0,2,0,7},{1,0,2,3,0,4,5,0}, {1,2,3}};
+        int [][] input = {{8,4,5,0,0,0,0,7},{0,1,7,6,0,2,0,7},{1,0,2,3,0,4,5,0}, {1,2,3}};
         for (int [] x : input) {
             duplicateZeros(x);
             System.out.println("Output: ");
@@ -13,27 +15,30 @@ public class DuplicateZeroes {
     }
 
     public static void duplicateZeros(int[] arr) {
-        // Find out how many zeroes are present
-        int numZeros = 0;
-        for (int i=0; i<arr.length-2; i++) numZeros = arr[i] == 0 ? numZeros + 1 : numZeros;
-        System.out.println("NumZeroes: " + numZeros);
+        // Step1 - Create stack to add new elements
+        Stack<Integer> stack = new Stack<>();
 
-        // Return if no zeroes present
-        if (numZeros == 0)
-            return;
+        // Step2 - Traverse input array
+        for (int i=0; i<arr.length; i++) {
+            int current = arr[i];
 
-        int lastIndex = arr.length - 1;
-        for (int x=(arr.length - 1) - numZeros; x >= 0; x--) {
-            System.out.println(arr[x]);
-            int current = arr[x];
-
+            // Step3 - If current is zero, add and additional zero to stack
+            // Dont forget to check for length
             if (current == 0) {
-                arr[lastIndex] = 0;
-                --lastIndex;
+                stack.push(current);
+                if (stack.size() == arr.length)
+                    break;
             }
 
-            arr[lastIndex] = current;
-            --lastIndex;
+            // Step4 - Add current number to stack
+            stack.push(current);
+            if (stack.size() == arr.length)
+                break;
+        }
+
+        // Step5 - Repopulate input arr with stack values
+        for (int i=arr.length - 1; i >= 0; i--) {
+            arr[i] = stack.pop();
         }
     }
 }
