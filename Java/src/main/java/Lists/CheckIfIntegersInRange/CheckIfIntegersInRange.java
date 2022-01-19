9b78d802dd6c4b5aea8bb716276ff98363893a9f
Tim M <timothy.romero.manas@gmail.com>
2022-01-18 20:43:42 -0500

    Check if integers in range

--- Java/src/main/java/Lists/CheckIfIntegersInRange/CheckIfIntegersInRange.java
+++ Java/src/main/java/Lists/CheckIfIntegersInRange/CheckIfIntegersInRange.java
@@ -5,28 +5,41 @@ public class CheckIfIntegersInRange {
         int [][] range = {{1,2}, {3,4}, {5,6}};
         int left = 2;
         int right = 5;
-        System.out.println("Is Covered: " + isCovered(range, left, right));
+        System.out.println("Is Covered: " + isCovered(range, left, right) + "\n");
 
 
         int [][] range1 = {{1,10}, {10,20}};
         int left1 = 21;
         int right1 = 21;
-        System.out.println("Is Covered: " + isCovered(range1, left1, right1));
+        System.out.println("Is Covered: " + isCovered(range1, left1, right1) + "\n");
+
+
+        int [][] range2 = {{36,50},{14,28},{4,31},{24,37},{13,36},{27,33},{23,32},{23,27},{1,35}};
+        int left2 = 35;
+        int right2 = 40;
+        System.out.println("Is Covered: " + isCovered(range2, left2, right2) + "\n");
+
     }
 
     public static  boolean isCovered(int[][] ranges, int left, int right) {
+        int current = left;
+        int index = 0;
+        while (current <= right) {
+            System.out.println("Current: " + current);
 
-        for (int row=0; row < ranges.length; row++) {
-            System.out.println("Row: " + ranges[row][0] + "," + ranges[row][1]);
+            if (index > ranges.length - 1)
+                return false;
 
-            if (left <= ranges[row][0] && ranges[row][0] <= right) {
-                System.out.println("WithinBounds: " + ranges[row][0]);
+            int [] range = ranges[index];
+
+            if (range[0] <= current && current <= range[1] ) {
+                System.out.println("Current Found range: " + range[0] + "," + range[1]);
+                ++current;
             } else {
-                if (left <= ranges[row][1] && ranges[row][1] <= right) {
-                    System.out.println("WithinBounds: " + ranges[row][1]);
-                } else
-                    return false;
+                System.out.println("Not Found");
+                ++index;
             }
+
         }
         return true;
     }
