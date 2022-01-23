91e49e4901517b94e458e7132a522041e5187b00
Tim M <timothy.romero.manas@gmail.com>
2022-01-23 00:18:11 -0500

    Adding Climbing Stairs

---
+++ Java/src/main/java/Misc/ClimbingStairs/ClimbingStairs.java
@@ -0,0 +1,60 @@
+package Misc.ClimbingStairs;
+
+public class ClimbingStairs {
+    public static void main(String [] args) {
+        int [] input = {5, 2, 3};
+        for (int i : input)
+            System.out.println("ClimbStairs: " + climbStairs(i));
+    }
+
+    static class Node {
+        int value;
+        public Node subtractOne;
+        public Node subtractTwo;
+
+        public Node(int value) {
+            this.value = value;
+        }
+    }
+
+    public static int climbStairs(int n) {
+        int numUnique = 0;
+
+        // Create Tree
+        Node root = new Node(n);
+        createTree(root);
+
+        // Traverse Tree
+        System.out.println("\nTraversing Tree");
+        traverseTree(root);
+
+        return numUnique;
+    }
+
+    private static void traverseTree(Node node) {
+
+        if (node == null)
+            return;
+
+        System.out.println(node.value);
+        traverseTree(node.subtractOne);
+        traverseTree(node.subtractOne);
+    }
+
+    private static void createTree(Node node) {
+
+        if (node == null)
+            return;
+        int current = node.value;
+        System.out.println("Current: " + current);
+
+        Node nodeSubTractOne = current - 1 >= 0 ? new Node(current - 1) : null ;
+        Node nodeSubTractTwo = current - 2 >= 0 ? new Node (current - 2) : null;
+
+        node.subtractOne = nodeSubTractOne;
+        node.subtractTwo = nodeSubTractTwo;
+
+        createTree(nodeSubTractOne);
+        createTree(nodeSubTractTwo);
+    }
+}
