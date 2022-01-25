164438e283e5649737e91e6d0aba8ad618101fe4
Tim M <timothy.romero.manas@gmail.com>
2022-01-24 19:11:31 -0500

    Finished Climbing Stairs

--- Java/src/main/java/Misc/ClimbingStairs/ClimbingStairs.java
+++ Java/src/main/java/Misc/ClimbingStairs/ClimbingStairs.java
@@ -1,8 +1,6 @@
 package Misc.ClimbingStairs;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Stack;
+import java.util.*;
 
 public class ClimbingStairs {
     public static void main(String [] args) {
@@ -11,20 +9,53 @@ public class ClimbingStairs {
             System.out.println("ClimbStairs: " + climbStairs(i) + "\n");
     }
 
-
-
     public static int climbStairs(int n) {
+        // Step1 - Create Map to cache the number of child elements
+        Map<Integer, Integer> map = new TreeMap<>();
+
+        // Step2 - Traverse the tree from n > 0
+        int numUnique = traverseStairs(n, map);
+        System.out.println("Map: " + map);
+        return numUnique;
+    }
+
+    private static int traverseStairs(int n, Map<Integer, Integer> map) {
+
+        // Step1 - Check if n is positive, return 0 otherwise
+        if (n < 0)
+            return 0;
+        else if (n == 0 )
+            return 1;   // We set this as 1 because we Need to have 1 have 1
+
+        System.out.println(n);
+
+        // Step2 - Check if map has n, if not recursively traverse down
+        int left = map.containsKey(n-1) ? map.get(n-1) : traverseStairs(n-1, map);
+        int right = map.containsKey(n-2) ? map.get(n-2) : traverseStairs(n-2, map);
+
+        // Step3 - Add the left and right results and place in map for future reference
+        int numRoutes = left + right;
+        if (!map.containsKey(n))
+            map.put(n, numRoutes);
+
+        return numRoutes;
+    }
+
+
+    // =================================================================================================
+
+    public static int climbStairs_Naive(int n) {
         int numUnique = 0;
 
         // Create Tree
         Node root = new Node(n);
-        createTree(root);
+        createTree_Naive(root);
 
         // Traverse Tree
         System.out.println("\nTraversing Tree");
         List<String> list = new ArrayList<>();
         Stack<Integer> stack = new Stack();
-        traverseTree(root, list, stack);
+        traverseTree_Naive(root, list, stack);
 
         System.out.println("List: " + list);
         numUnique = list.size();
@@ -44,7 +75,7 @@ public class ClimbingStairs {
     }
 
 
-    private static void traverseTree(Node node, List<String> list, Stack<Integer> stack) {
+    private static void traverseTree_Naive(Node node, List<String> list, Stack<Integer> stack) {
 
         if (node == null) {
             return;
@@ -52,32 +83,32 @@ public class ClimbingStairs {
 
         // Add current to stack
         stack.push(node.value);
-        System.out.println(stack.toString());
+//        System.out.println(stack.toString());
 
         if (node.value == 0)
             list.add(stack.toString());
 
         // Traverse Left Node
-        traverseTree(node.subtractOne, list, stack);
+        traverseTree_Naive(node.subtractOne, list, stack);
         // Pop only if child left Node is not null
         if (node.subtractOne != null)
             stack.pop();
 
         // Traverse Right Node
-        traverseTree(node.subtractTwo, list, stack);
+        traverseTree_Naive(node.subtractTwo, list, stack);
         // Pop only if child right Node is not null
         if (node.subtractTwo != null)
             stack.pop();
     }
 
-    private static void createTree(Node node) {
+    private static void createTree_Naive(Node node) {
 
         if (node == null) {
             return;
         }
 
         int current = node.value;
-        System.out.println("Current: " + current);
+//        System.out.println("Current: " + current);
 
         Node nodeSubTractOne = current - 1 >= 0 ? new Node(current - 1) : null;
         Node nodeSubTractTwo = current - 2 >= 0 ? new Node (current - 2) : null;
@@ -85,7 +116,7 @@ public class ClimbingStairs {
         node.subtractOne = nodeSubTractOne;
         node.subtractTwo = nodeSubTractTwo;
 
-        createTree(nodeSubTractOne);
-        createTree(nodeSubTractTwo);
+        createTree_Naive(nodeSubTractOne);
+        createTree_Naive(nodeSubTractTwo);
     }
 }
