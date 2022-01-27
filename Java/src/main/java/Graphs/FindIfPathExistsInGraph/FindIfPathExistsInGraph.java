9d01093f361d43414a07a9540ee26d66c8ccae84
Tim M <timothy.romero.manas@gmail.com>
2022-01-26 22:32:08 -0500

    Adding Find If Path exists in Graph

--- Java/src/main/java/Graphs/FindIfPathExistsInGraph/FindIfPathExistsInGraph.java
+++ Java/src/main/java/Graphs/FindIfPathExistsInGraph/FindIfPathExistsInGraph.java
@@ -1,5 +1,7 @@
 package Graphs.FindIfPathExistsInGraph;
 
+import java.util.*;
+
 public class FindIfPathExistsInGraph {
     public static void main(String [] args) {
         int n = 3;
@@ -12,11 +14,49 @@ public class FindIfPathExistsInGraph {
         int [][] edges1 = {{0,1},{1,2},{3,5},{5,4},{4,3}};
         int source1 = 0;
         int destination1 = 5;
-        System.out.println("Is Valid Path: " + validPath(n, edges, source, destination));
+        System.out.println("Is Valid Path: " + validPath(n1, edges1, source1, destination1));
     }
 
     public static boolean validPath(int n, int[][] edges, int source, int destination) {
+
+        // Create map to store the relationships of each node
+        Map<Integer, List<Integer>> map = new TreeMap<>();
+        for (int i=0; i<edges.length;i++) {
+            int node = edges[i][0];
+            int neighbour = edges[i][1];
+
+            List<Integer> tempList = new ArrayList<>();
+            if (map.containsKey(node)) {
+                tempList = map.get(node);
+                tempList.add(neighbour);
+            }
+
+            tempList.add(node);
+            map.put(node,tempList);
+        }
+        System.out.println("Map: " + map);
+
+        // Traverse graph starting from source
+        Stack<Integer> stack = new Stack<>();
+        Set<Integer> set = new TreeSet<>();
+        set.add(source);
+        traverseGraph(map, set, stack, source, destination);
+
         return true;
     }
 
+    private static void traverseGraph(Map<Integer, List<Integer>> map, Set<Integer> set, Stack<Integer> stack, int current, int destination) {
+        List<Integer> neighbourList = map.get(current);
+    }
+
 }
+
+
+/**
+ Notes
+ 1. How do you traverse using an array ?
+ 2. What Data Structure do you store the graph?
+    > Use a map to map out the neighbours to uses
+ 3. Should you convert input array to graph ?
+    > Use a map
+ */
