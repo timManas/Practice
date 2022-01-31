08cef5125a9d9a88640426e9552d0ef742cd7f5a
Tim M <timothy.romero.manas@gmail.com>
2022-01-31 12:00:50 -0500

    Fixed some issues

--- Java/src/main/java/Misc/GuessNumHigherOrLower/GuessNumHigherOrLower.java
+++ Java/src/main/java/Misc/GuessNumHigherOrLower/GuessNumHigherOrLower.java
@@ -3,12 +3,30 @@ package Misc.GuessNumHigherOrLower;
 public class GuessNumHigherOrLower {
 
     public static void main(String [] args) {
-        int n = 10;
-        int p = 6;
-        System.out.println("Guess Number: " + guessNumber(n, p));
+//        int n = 10;
+//        int p = 6;
+//        System.out.println("Guess Number: " + guessNumber(n, p));
+//
+//        int n1 = 1;
+//        int p1 = 1;
+//        System.out.println("Guess Number: " + guessNumber(n1, p1));
+//
+//        int n2 = 2;
+//        int p2 = 1;
+//        System.out.println("Guess Number: " + guessNumber(n2, p2));
+//
+//        int n3 = 2;
+//        int p3 = 2;
+//        System.out.println("Guess Number: " + guessNumber(n3, p3));
+
+        int n4 = 2126753390;
+        int p4 = 1702766719;
+        System.out.println("Guess Number: " + guessNumber(n4, p4));
     }
 
     public static int guessNumber(int n, int p) {
+
+
 
         int guessResult = -1;
         int pick = 0;
@@ -17,22 +35,21 @@ public class GuessNumHigherOrLower {
         int mid = (start + end) / 2;
 
         while (guessResult != 0 || start < end) {
-
             guessResult = guess(mid, p);
+            System.out.println("mid: " + mid + "    guessResult: " + guessResult);
 
-            if (guessResult == -1) {
-                start = mid;
-                end = n;
+
+            if (guessResult == -1) {        // mid is lower
+                end = mid - 1;
                 mid = (start + end) / 2;
             } else if (guessResult == 0) {
                 pick = mid;
                 break;
-            } else if (guessResult ==  1) {
-                start = 0;
-                end = mid;
+            } else if (guessResult ==  1) {     // mid is higher
+                start = mid + 1;
+                end = n;
                 mid = (start + end) / 2;
             }
-
         }
 
         return pick;
@@ -40,9 +57,9 @@ public class GuessNumHigherOrLower {
 
     private static int guess(int guess, int pick) {
         if (guess < pick)
-            return -1;
-        if (guess > pick)
             return 1;
+        if (guess > pick)
+            return -1;
 
         return 0;
     }
