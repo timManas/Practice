7609cf43ac3a2fe2c0cb4dfa84c3abe632e5081d
Tim M <timothy.romero.manas@gmail.com>
2022-02-17 22:34:47 -0500

    Finished Arranging coins

--- Java/src/main/java/Misc/ArrangingCoins/ArrangingCoins.java
+++ Java/src/main/java/Misc/ArrangingCoins/ArrangingCoins.java
@@ -2,21 +2,28 @@ package Misc.ArrangingCoins;
 
 public class ArrangingCoins {
     public static void main (String [] args) {
-        int [] input = {5, 8};
+        int [] input = {2147483647,10,5,8};
         for (int i : input)
             System.out.println("Arrange Coins: " + arrangeCoins(i) + "\n");
     }
 
     public static int arrangeCoins(int n) {
-        int rows = 0;
+        int rows = 0;           // For everytime total is greater than n, we increment rows
+        int total = 0;          // Total amount using geometric series
+        int current = 1;          // current is the number to add
 
-        int total = 0;
-        int index = 1;
+        // Step2 - Traverse from 1 to
         while(total <= n) {
-            total += index;
-            ++index;
+            total += current;
+            System.out.println("index: " + current  + "   Total: " + total);
+
+            ++current;
             ++rows;
-            System.out.println("Total: " + total);
+
+            // If we  breach the int.max value, we stop the loop
+            if (total < 0) {
+                break;
+            }
         }
 
         return total == n ? rows : rows - 1;
