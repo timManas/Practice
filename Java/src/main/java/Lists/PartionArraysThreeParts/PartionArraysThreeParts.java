9bad5517b96f5c1b73bb46a93c11b47e63d4ea68
Tim M <timothy.romero.manas@gmail.com>
2022-02-18 10:18:17 -0500

    ADDING Partition Arrays Three parts

---
+++ Java/src/main/java/Lists/PartionArraysThreeParts/PartionArraysThreeParts.java
@@ -0,0 +1,17 @@
+package Lists.PartionArraysThreeParts;
+
+public class PartionArraysThreeParts {
+    public static void main(String [] args) {
+        int [][] input = {{0,2,1,-6,6,-7,9,1,2,0,1}, {0,2,1,-6,6,7,9,-1,2,0,1}, {3,3,6,5,-2,2,5,1,-9,4}};
+        for (int [] i : input)
+            System.out.println("Can split equal 3 parts: " + canThreePartsEqualSum(i) + "\n");
+    }
+
+    public static boolean canThreePartsEqualSum(int[] arr) {
+
+
+
+        return true;
+    }
+
+}
