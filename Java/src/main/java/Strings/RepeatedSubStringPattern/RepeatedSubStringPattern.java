178183225e86891b9403d1b514872b2c208c8a0e
Tim M <timothy.romero.manas@gmail.com>
2022-02-27 23:28:30 -0500

    Adding Repeated substring pattern

---
+++ Java/src/main/java/Strings/RepeatedSubStringPattern/RepeatedSubStringPattern.java
@@ -0,0 +1,21 @@
+package Strings.RepeatedSubStringPattern;
+
+public class RepeatedSubStringPattern {
+    public static void main(String [] args) {
+
+        String [] input = {"abab", "aba", "abcabcabcabc"};
+        for (String i : input)
+            System.out.println("Repeated SubString: " + repeatedSubstringPattern(i));
+
+
+    }
+
+    public static boolean repeatedSubstringPattern(String s) {
+
+        for (int i=s.length() / 2; i >= 0; i--) {
+
+
+        }
+        return false;
+    }
+}
