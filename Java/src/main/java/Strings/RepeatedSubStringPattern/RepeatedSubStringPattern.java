71b60096a60d2ca35e3481467d4109fb87719f42
Tim M <timothy.romero.manas@gmail.com>
2022-02-28 11:05:02 -0500

    Adding Repeated SubString Pattern

--- Java/src/main/java/Strings/RepeatedSubStringPattern/RepeatedSubStringPattern.java
+++ Java/src/main/java/Strings/RepeatedSubStringPattern/RepeatedSubStringPattern.java
@@ -1,21 +1,44 @@
 package Strings.RepeatedSubStringPattern;
 
+import java.util.Deque;
+import java.util.LinkedList;
+
 public class RepeatedSubStringPattern {
     public static void main(String [] args) {
 
-        String [] input = {"abab", "aba", "abcabcabcabc"};
+        String [] input = {"ababab","abab", "aba", "abcabcabcabc"};
         for (String i : input)
-            System.out.println("Repeated SubString: " + repeatedSubstringPattern(i));
+            System.out.println("Repeated SubString: " + repeatedSubstringPattern(i) + "\n");
 
 
     }
 
     public static boolean repeatedSubstringPattern(String s) {
 
-        for (int i=s.length() / 2; i >= 0; i--) {
+        Deque<Character> queue = new LinkedList<>();
 
+        for (int i=0; i<s.length(); i++) {
+            char current = s.charAt(i);
 
+            // Add to queue if Empty
+            if (queue.isEmpty()) {
+                System.out.println("Adding: " + current);
+                queue.addLast(current);
+                continue;
+            }
+
+            if (current == queue.peekFirst()) {
+                System.out.println("Removing: " + queue.removeFirst());
+            } else {
+                System.out.println("Adding: " + current);
+                queue.addLast(current);
+            }
+
+            System.out.println("Queue: " + queue);
         }
-        return false;
+
+
+
+        return queue.isEmpty();
     }
 }
