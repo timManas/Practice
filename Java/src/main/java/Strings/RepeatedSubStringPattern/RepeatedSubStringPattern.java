8c6aec0984f0644e478056e9711b380d57cca828
Tim M <timothy.romero.manas@gmail.com>
2022-03-01 18:44:14 -0500

    Adding Repeated substring pattern

--- Java/src/main/java/Strings/RepeatedSubStringPattern/RepeatedSubStringPattern.java
+++ Java/src/main/java/Strings/RepeatedSubStringPattern/RepeatedSubStringPattern.java
@@ -6,39 +6,35 @@ import java.util.LinkedList;
 public class RepeatedSubStringPattern {
     public static void main(String [] args) {
 
-        String [] input = {"ababab","abab", "aba", "abcabcabcabc"};
+        String [] input = {"bab bab bab bab bab","ababababab","abac","ababab","abab", "aba", "abcabcabcabc"};
         for (String i : input)
             System.out.println("Repeated SubString: " + repeatedSubstringPattern(i) + "\n");
 
 
     }
 
-    public static boolean repeatedSubstringPattern(String s) {
+    public static boolean repeatedSubstringPattern(String input) {
 
-        Deque<Character> queue = new LinkedList<>();
+        // Step1 - Traversde input Array
+        for (int i=input.length() / 2; i > 0; i--) {
+            String copyS = input;            // Make a copy, for checking purposes
 
-        for (int i=0; i<s.length(); i++) {
-            char current = s.charAt(i);
-
-            // Add to queue if Empty
-            if (queue.isEmpty()) {
-                System.out.println("Adding: " + current);
-                queue.addLast(current);
+            // Step2 - Check if we can evenly divide the input size with i
+            if (copyS.length() % i != 0)
                 continue;
-            }
 
-            if (current == queue.peekFirst()) {
-                System.out.println("Removing: " + queue.removeFirst());
-            } else {
-                System.out.println("Adding: " + current);
-                queue.addLast(current);
-            }
+            // Step3 - Get the SubString
+            String subString = copyS.substring(0, i);
+            System.out.println("i:" + i + "     SubString: " + subString);
 
-            System.out.println("Queue: " + queue);
+            // Step4 - Split into even sizes, if size == 0. We have a repeated pattern
+            String subStringList = copyS.replace(subString, "");
+            if (subStringList.trim().length() == 0)
+                return true;
         }
 
 
 
-        return queue.isEmpty();
+        return false;
     }
 }
