a01e78bba249c502c3f58824064f9ebd28ee8b1a
Tim M <timothy.romero.manas@gmail.com>
2022-03-05 11:32:57 -0500

    Adding Rectangle overlap

--- Java/src/main/java/Misc/RectangleOverlap/RectangleOverlap.java
+++ Java/src/main/java/Misc/RectangleOverlap/RectangleOverlap.java
@@ -4,30 +4,43 @@ import java.util.Arrays;
 
 public class RectangleOverlap {
     public static void main(String [] args) {
-        int [] rect1 = {0,0,2,2};
-        int [] rect2 = {1,1,3,3};
-        System.out.println("IsOverlap: " + isRectangleOverlap(rect1, rect2));
+//        int [] rect1 = {0,0,2,2};
+//        int [] rect2 = {1,1,3,3};
+//        System.out.println("IsOverlap: " + isRectangleOverlap(rect1, rect2) + "\n");
+//
+//        int [] rect1a = {0,0,1,1};
+//        int [] rect2a = {1,0,2,1};
+//        System.out.println("IsOverlap: " + isRectangleOverlap(rect1a, rect2a) + "\n");
 
-        int [] rect1a = {0,0,1,1};
-        int [] rect2a = {1,0,2,1};
-        System.out.println("IsOverlap: " + isRectangleOverlap(rect1a, rect2a));
-
+        int [] rect1b = {7,8,13,15};
+        int [] rect2b = {10,8,12,20};
+        System.out.println("IsOverlap: " + isRectangleOverlap(rect1b, rect2b) + "\n");
     }
 
     public static boolean isRectangleOverlap(int[] rec1, int[] rec2) {
         // Find the X range
-        int [] rec1RangeX = {rec1[0], rec1[2]};
-        int [] rec1RangeY = {rec1[1], rec1[3]};
+        int [] rec1RangeX = {Math.min(rec1[0], rec1[2]), Math.max(rec1[0], rec1[2])};
+        int [] rec1RangeY = {Math.min(rec1[1], rec1[3]), Math.max(rec1[1], rec1[3])};
 
         // Find the Y range
-        int [] rec2RangeX = {rec2[0], rec2[2]};
-        int [] rec2RangeY = {rec2[1], rec2[3]};
-
-
+        int [] rec2RangeX = {Math.min(rec2[0], rec2[2]), Math.max(rec2[0], rec2[2])};
+        int [] rec2RangeY = {Math.min(rec2[1], rec2[3]), Math.max(rec2[1], rec2[3])};
 
         System.out.println("Rec1 X: " + Arrays.toString(rec1RangeX) + "     Y: " + Arrays.toString(rec1RangeY));
         System.out.println("Rec1 X: " + Arrays.toString(rec2RangeX) + "     Y: " + Arrays.toString(rec2RangeY));
 
-        return true;
+        // Check the X Axis
+        if (rec1RangeX[0] <= rec2RangeX[0] && rec2RangeX[0] < rec1RangeX[1] && rec1RangeY[0] <= rec2RangeY[0] && rec2RangeY[0] < rec1RangeY[1])
+            return true;
+        if (rec1RangeX[0] <= rec2RangeX[1] && rec2RangeX[1] < rec1RangeX[1] && rec1RangeY[0] <= rec2RangeY[1] && rec2RangeY[1] < rec1RangeY[1])
+            return true;
+
+        // Check the Y Axis
+//        if (rec2RangeY[0] < rec2RangeY[0] && rec2RangeY[0] < rec2RangeY[1])
+//            return true;
+//        if (rec2RangeY[0] < rec2RangeY[1] && rec2RangeY[1] < rec2RangeY[1])
+//            return true;
+
+        return false;
     }
 }
