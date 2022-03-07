9ea51b31785f6e3dd14a02b55efee246e88a83b0
Tim M <timothy.romero.manas@gmail.com>
2022-03-07 18:54:05 -0500

    Adding Rectangle overlpa

--- Java/src/main/java/Misc/RectangleOverlap/RectangleOverlap.java
+++ Java/src/main/java/Misc/RectangleOverlap/RectangleOverlap.java
@@ -12,12 +12,21 @@ public class RectangleOverlap {
 //        int [] rect2a = {1,0,2,1};
 //        System.out.println("IsOverlap: " + isRectangleOverlap(rect1a, rect2a) + "\n");
 
-        int [] rect1b = {7,8,13,15};
-        int [] rect2b = {10,8,12,20};
+        int [] rect1b = {2,17,6,20};
+        int [] rect2b = {3,8,6,20};
         System.out.println("IsOverlap: " + isRectangleOverlap(rect1b, rect2b) + "\n");
     }
 
     public static boolean isRectangleOverlap(int[] rec1, int[] rec2) {
+
+        // X points TopLeft, TopRight, BottomLeft, BottomRight
+        int [][] rec1Points = {{rec1[0], rec1[3]}, {rec1[2], rec1[3]}, {rec1[0], rec1[1]}, {rec1[2], rec1[1]}};
+        int [][] rec2Points = {{rec2[0], rec2[3]}, {rec2[2], rec2[3]}, {rec2[0], rec2[1]}, {rec2[2], rec2[1]}};
+
+        System.out.println("Rec1 points: " + Arrays.deepToString(rec1Points));
+        System.out.println("Rec2 points: " + Arrays.deepToString(rec2Points));
+
+
         // Find the X range
         int [] rec1RangeX = {Math.min(rec1[0], rec1[2]), Math.max(rec1[0], rec1[2])};
         int [] rec1RangeY = {Math.min(rec1[1], rec1[3]), Math.max(rec1[1], rec1[3])};
@@ -30,9 +39,9 @@ public class RectangleOverlap {
         System.out.println("Rec1 X: " + Arrays.toString(rec2RangeX) + "     Y: " + Arrays.toString(rec2RangeY));
 
         // Check the X Axis
-        if (rec1RangeX[0] <= rec2RangeX[0] && rec2RangeX[0] < rec1RangeX[1] && rec1RangeY[0] <= rec2RangeY[0] && rec2RangeY[0] < rec1RangeY[1])
+        if (rec1RangeX[0] < rec2RangeX[0] && rec2RangeX[0] < rec1RangeX[1] && rec1RangeY[0] < rec2RangeY[0] && rec2RangeY[0] < rec1RangeY[1])
             return true;
-        if (rec1RangeX[0] <= rec2RangeX[1] && rec2RangeX[1] < rec1RangeX[1] && rec1RangeY[0] <= rec2RangeY[1] && rec2RangeY[1] < rec1RangeY[1])
+        if (rec1RangeX[0] < rec2RangeX[1] && rec2RangeX[1] <= rec1RangeX[1] && rec1RangeY[0] < rec2RangeY[1] && rec2RangeY[1] <= rec1RangeY[1])
             return true;
 
         // Check the Y Axis
