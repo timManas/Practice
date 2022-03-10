c71ecfd48c6d184aac86d9c86243cb2f2b09b5a0
Tim M <timothy.romero.manas@gmail.com>
2022-03-09 22:07:05 -0500

    Adding sentence similarity

--- Java/src/main/java/Strings/SentenceSimilarity/SentenceSimilarity.java
+++ Java/src/main/java/Strings/SentenceSimilarity/SentenceSimilarity.java
@@ -2,6 +2,8 @@ package Strings.SentenceSimilarity;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 
 public class SentenceSimilarity {
     public static void main(String [] args) {
@@ -9,6 +11,7 @@ public class SentenceSimilarity {
         String [] sentence2 = {"fine","drama","talent"};
         String [][] temp = {{"great","fine"}, {"drama","acting"}, {"skills","talent"}};
         List<List<String>> similarPairs = convertTo2DList(temp);
+        System.out.println("Are Sentence Similar: " + areSentencesSimilar(sentence1, sentence2, similarPairs));
     }
 
     private static List<List<String>> convertTo2DList(String[][] temp) {
@@ -25,7 +28,33 @@ public class SentenceSimilarity {
 
     public static boolean areSentencesSimilar(String[] sentence1, String[] sentence2, List<List<String>> similarPairs) {
 
+        // Create map to store the similar pairs
+        Map<String, String> map = new TreeMap<>();
+        for (List<String> pairs : similarPairs) {
+            map.put(pairs.get(0), pairs.get(1));
+            map.put(pairs.get(1), pairs.get(0));
+        }
+        System.out.println("Map: " + map);
 
-        return true;
+        StringBuilder sb1 = new StringBuilder();
+        StringBuilder sb2 = new StringBuilder();
+
+        for (String word : sentence1) {
+            String converted = map.containsKey(word) ? map.get(word) : word;
+            sb1.append(converted);
+        }
+
+        for (String word : sentence2) {
+            String converted = map.containsKey(word) ? map.get(word) : word;
+            sb2.append(converted);
+        }
+
+        System.out.println("sb1: " + sb1);
+        System.out.println("sb2: " + sb2);
+
+
+
+
+        return sb1;
     }
 }
