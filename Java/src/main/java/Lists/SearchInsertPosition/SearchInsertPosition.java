8d23206eb7a10cb11cdd50391968714b1dc5ca4f
Tim M <timothy.romero.manas@gmail.com>
2022-03-14 12:27:27 -0400

    Creating search insert position

--- Java/src/main/java/Lists/SearchInsertPosition/SearchInsertPosition.java
+++ Java/src/main/java/Lists/SearchInsertPosition/SearchInsertPosition.java
@@ -13,6 +13,10 @@ public class SearchInsertPosition {
         int [] nums2 = {1,3,5,6};
         int target2 = 7;
         System.out.println("search Insert: " + searchInsert(nums2, target2) + "\n");
+
+        int [] nums3 = {1,3,5,6};
+        int target3 = 0;
+        System.out.println("search Insert: " + searchInsert(nums3, target3) + "\n");
     }
 
     public static int searchInsert(int[] nums, int target) {
@@ -21,23 +25,20 @@ public class SearchInsertPosition {
 
     private static int getPosition(int[] nums, int target, int start, int end) {
 
+        if (start > end)
+            return -1;
+
         int middle = (start + end) / 2;
         System.out.println("middle position: "  + middle + "    element: " + nums[middle] + "   start: " + start + "    end: " + end);
-
 
         if (nums[middle] == target)
             return middle;
         else if (nums[middle] < target) {
-
-            if (start >= middle)
-                return middle;
-
-            return getPosition(nums, target, start, middle - 1);
+            int result = getPosition(nums, target, middle + 1, end);
+            return result <= -1 ? middle + 1 : result;
         } else if (nums[middle] > target) {
-            if (start <= middle)
-                return middle;
-
-            return getPosition(nums, target, middle + 1, end);
+            int result = getPosition(nums, target, start, middle - 1);
+            return result <= -1 ? middle + 1 : result;
         }
 
         return -1;
