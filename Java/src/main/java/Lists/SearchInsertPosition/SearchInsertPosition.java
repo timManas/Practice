1a34aed9aa934de7c1a491f9ae496799080a7e8d
Tim M <timothy.romero.manas@gmail.com>
2022-03-15 18:59:14 -0400

    Finished Search Insert position

--- Java/src/main/java/Lists/SearchInsertPosition/SearchInsertPosition.java
+++ Java/src/main/java/Lists/SearchInsertPosition/SearchInsertPosition.java
@@ -20,25 +20,38 @@ public class SearchInsertPosition {
     }
 
     public static int searchInsert(int[] nums, int target) {
-        return getPosition(nums, target, 0, nums.length - 1);
+
+        // Find the closet position
+        int index = getPosition(nums, target, 0, nums.length - 1);
+        System.out.println("resulting index: " + index);
+
+        // Check position is valid
+        if (index > 0 && target < nums[index - 1])
+            return index - 1;
+
+        return index;
     }
 
     private static int getPosition(int[] nums, int target, int start, int end) {
 
+        // Stop loop if start and end no longer are valid
         if (start > end)
             return -1;
 
+        // Step1 - Find the mid point
         int middle = (start + end) / 2;
         System.out.println("middle position: "  + middle + "    element: " + nums[middle] + "   start: " + start + "    end: " + end);
 
+        // Step2 - Use recursion to find the closet position
+        // return -1 if the start and end are no longer valid
         if (nums[middle] == target)
             return middle;
         else if (nums[middle] < target) {
             int result = getPosition(nums, target, middle + 1, end);
-            return result <= -1 ? middle + 1 : result;
+            return result <= -1 ? middle + 1: result;
         } else if (nums[middle] > target) {
             int result = getPosition(nums, target, start, middle - 1);
-            return result <= -1 ? middle + 1 : result;
+            return result <= -1 ? middle + 1: result;
         }
 
         return -1;
