dfec371282c20f39ebcc558c64d52771bc5fb9a7
Tim M <timothy.romero.manas@gmail.com>
2022-03-22 21:15:52 -0400

    Finished Check at most one segment of ones

--- Java/src/main/java/Strings/CheckIfBinaryStringHasAtMostOneSegmentOfOnes/CheckIfBinaryStringHasAtMostOneSegmentOfOnes.java
+++ Java/src/main/java/Strings/CheckIfBinaryStringHasAtMostOneSegmentOfOnes/CheckIfBinaryStringHasAtMostOneSegmentOfOnes.java
@@ -9,20 +9,31 @@ public class CheckIfBinaryStringHasAtMostOneSegmentOfOnes {
 
     public static boolean checkOnesSegment(String s) {
 
+        // Step1 - If length is 1
         if (s.length() == 1) {
             return s.equalsIgnoreCase("1");
         }
 
-        char prev = s.charAt(0);
+        // Step2 - Find if continguous
+        boolean hasOneSegment = false;
         for (int i=1; i<s.length(); i++) {
-            char letter = s.charAt(i);
+            char current = s.charAt(i);
+            System.out.println("Current: " + current);
 
-            if (letter == '1' && prev == '1')
-                return true;
+            // Step3 - Check if letter is 1 or 0
+            // If letter is 0 after the one. We know there already exist atleast one
+            // Then we check if there are more 1. If there are, we return zero
+            if (current == '1') {
+                if (hasOneSegment) {
+                    return false;
+                }
+            } else {
+                hasOneSegment = true;
+            }
 
-            prev = letter;
+
         }
 
-        return false;
+        return true;
     }
 }
