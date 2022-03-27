b5f3f8f8727acce23e83553830c6ec30615206ef
Tim M <timothy.romero.manas@gmail.com>
2022-03-26 23:31:35 -0400

    Adding Valid word square

--- Java/src/main/java/Strings/ValidWordSquare/ValidWordSquare.java
+++ Java/src/main/java/Strings/ValidWordSquare/ValidWordSquare.java
@@ -21,8 +21,11 @@ public class ValidWordSquare {
 //        String [] words4 = {"abc","b"};
 //        System.out.println("Valid Word Square: " + validWordSquare(convertToList(words4)) + "\n");
 
-        String [] words5 = {"abcd","bnrt","crm","dt"};
-        System.out.println("Valid Word Square: " + validWordSquare(convertToList(words5)) + "\n");
+//        String [] words5 = {"abcd","bnrt","crm","dt"};
+//        System.out.println("Valid Word Square: " + validWordSquare(convertToList(words5)) + "\n");
+
+        String [] words6 = {"aa","ad","ad"};
+        System.out.println("Valid Word Square: " + validWordSquare(convertToList(words6)) + "\n");
     }
 
     private static List<String> convertToList(String[] words) {
@@ -32,21 +35,26 @@ public class ValidWordSquare {
     }
 
     public static boolean validWordSquare(List<String> words) {
-
+        // Step1 - Create off set which allows us to get the respective positions
         int offset = 0;
 
+        // Step2 - Find the longestword. This will be used as the to run against the offset
         int longestWordLength = 0;
         for (String word : words) longestWordLength = word.length() > longestWordLength ? word.length() : longestWordLength;
 
-        while (offset < longestWordLength) {
+
+        // Step3 - Find the valid word square
+        while (offset < longestWordLength || offset < words.size()) {
             int rowIndex = 0;
             int colIndex = 0;
 
+            // Step4 - Check the character from the row matches with the respective character from the column
             while (rowIndex < longestWordLength || colIndex < longestWordLength) {
 
                 char row = ' ';
                 char col = ' ';
 
+                // Step5 - Find the row and col offset
                 try {
                     row = words.get(rowIndex).charAt(offset);
                 } catch (Exception e) {}
@@ -57,10 +65,11 @@ public class ValidWordSquare {
 
                 System.out.println("Row: " + row + "    Col: " + col);
 
-
+                // Step6 - Return false if row and col are not the same
                 if (row != col)
                     return false;
 
+                // Increment
                 rowIndex++;
                 colIndex++;
             }
