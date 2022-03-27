5a1c8bed69cdd7cf4de13784195ae7188a68523e
Tim M <timothy.romero.manas@gmail.com>
2022-03-26 23:26:53 -0400

    Adding valid word square

--- Java/src/main/java/Strings/ValidWordSquare/ValidWordSquare.java
+++ Java/src/main/java/Strings/ValidWordSquare/ValidWordSquare.java
@@ -5,20 +5,24 @@ import java.util.List;
 
 public class ValidWordSquare {
     public static void main(String [] args) {
-        String [] words = {"abcd","bnrt","crmy","dtye"};
-        System.out.println("Valid Word Square: " + validWordSquare(convertToList(words)) + "\n");
+//        String [] words = {"abcd","bnrt","crmy","dtye"};
+//        System.out.println("Valid Word Square: " + validWordSquare(convertToList(words)) + "\n");
+//
+//        String [] words1 = {"abcd","bnrt","crm","dt"};
+//        System.out.println("Valid Word Square: " + validWordSquare(convertToList(words1)) + "\n");
+//
+//        String [] words2 = {"ball","area","read","lady"};
+//        System.out.println("Valid Word Square: " + validWordSquare(convertToList(words2)) + "\n");
+//
+//        String [] words3 = {"ball","asee","let","lep"};
+//        System.out.println("Valid Word Square: " + validWordSquare(convertToList(words3)) + "\n");
+//        System.out.println("Valid Word Square: " + validWordSquare(convertToList(words3)) + "\n");
 
-        String [] words1 = {"abcd","bnrt","crm","dt"};
-        System.out.println("Valid Word Square: " + validWordSquare(convertToList(words1)) + "\n");
+//        String [] words4 = {"abc","b"};
+//        System.out.println("Valid Word Square: " + validWordSquare(convertToList(words4)) + "\n");
 
-        String [] words2 = {"ball","area","read","lady"};
-        System.out.println("Valid Word Square: " + validWordSquare(convertToList(words2)) + "\n");
-
-        String [] words3 = {"ball","asee","let","lep"};
-        System.out.println("Valid Word Square: " + validWordSquare(convertToList(words3)) + "\n");
-
-        String [] words4 = {"abc","b"};
-        System.out.println("Valid Word Square: " + validWordSquare(convertToList(words4)) + "\n");
+        String [] words5 = {"abcd","bnrt","crm","dt"};
+        System.out.println("Valid Word Square: " + validWordSquare(convertToList(words5)) + "\n");
     }
 
     private static List<String> convertToList(String[] words) {
@@ -31,15 +35,28 @@ public class ValidWordSquare {
 
         int offset = 0;
 
-        while (offset < words.size()) {
+        int longestWordLength = 0;
+        for (String word : words) longestWordLength = word.length() > longestWordLength ? word.length() : longestWordLength;
+
+        while (offset < longestWordLength) {
             int rowIndex = 0;
             int colIndex = 0;
 
-            while (rowIndex < words.get(offset).length() && colIndex < words.size()) {
+            while (rowIndex < longestWordLength || colIndex < longestWordLength) {
 
-                char row = offset < words.get(rowIndex).length() ? words.get(rowIndex).charAt(offset) : ' ';
-                char col = colIndex < words.get(offset).length() ? words.get(offset).charAt(colIndex) : ' ';
+                char row = ' ';
+                char col = ' ';
+
+                try {
+                    row = words.get(rowIndex).charAt(offset);
+                } catch (Exception e) {}
+
+                try {
+                    col = words.get(offset).charAt(colIndex);
+                } catch (Exception e) {}
+
                 System.out.println("Row: " + row + "    Col: " + col);
+
 
                 if (row != col)
                     return false;
@@ -50,8 +67,6 @@ public class ValidWordSquare {
             System.out.println("-------");
             offset++;
         }
-
-
 
         return true;
     }
