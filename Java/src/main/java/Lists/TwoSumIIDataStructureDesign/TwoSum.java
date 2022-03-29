f9064c874992f90a018be1f913087d05529bab57
Tim M <timothy.romero.manas@gmail.com>
2022-03-29 18:36:22 -0400

    Adding TwO sUM

--- Java/src/main/java/Lists/TwoSumIIDataStructureDesign/TwoSum.java
+++ Java/src/main/java/Lists/TwoSumIIDataStructureDesign/TwoSum.java
@@ -17,23 +17,44 @@ public class TwoSum {
 
     public boolean find(int target) {
 
+        // Step1 - Check size
         if (list.size() <= 1)
             return false;
 
         for (int i=0; i < list.size(); i++) {
+            // Step2 - Get Sublist to find the missing element
             List<Integer> subList = list.subList(i + 1, list.size());
             int current = list.get(i);
             int difference = target - current;
 
             System.out.println("current: " + current + "    diff: " + difference + "    target: " + target + "      subList:" + subList);
 
-            if (subList.contains(difference))
+            // Step3 - Use Binary Search
+            int indexDiff = findElement(subList, difference, 0, subList.size() - 1);
+            if (indexDiff != -1)
                 return true;
-
-
-
         }
 
         return false;
+    }
+
+    private int findElement(List<Integer> list, int difference, int start, int end) {
+
+        if (start > end || list.isEmpty())
+            return -1;
+
+        int i = (start + end) / 2;
+        int mid = list.get(i);
+
+
+        if (mid == difference) {
+            return i;
+        } else if (mid < difference) {
+            return findElement(list, difference, i + 1, end);
+        } else if (mid > difference) {
+            return findElement(list, difference, start, i - 1);
+        }
+
+        return -1;
     }
 }
