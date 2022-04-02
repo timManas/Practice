e55d0f3395b9066c707b822fb70ca9d07704fbf5
Tim M <timothy.romero.manas@gmail.com>
2022-04-01 21:48:22 -0400

    Finished Can place flowerbed

--- Java/src/main/java/Misc/CanPlaceFlowers/CanPlaceFlowers.java
+++ Java/src/main/java/Misc/CanPlaceFlowers/CanPlaceFlowers.java
@@ -25,43 +25,79 @@ public class CanPlaceFlowers {
         int n4 = 1;
         System.out.println("Can Place Flowers: " + canPlaceFlowers(flowerbed4, n4) + "\n");
 
+        int [] flowerbed5 = {0,0,1,1,0,0};
+        int n5 = 1;
+        System.out.println("Can Place Flowers: " + canPlaceFlowers(flowerbed5, n5) + "\n");
+
+        int [] flowerbed6 = {0,0,0,1,1,0,0,0};
+        int n6 = 1;
+        System.out.println("Can Place Flowers: " + canPlaceFlowers(flowerbed6, n6) + "\n");
+
+        int [] flowerbed7 = {0,0,0,0,1,1,0,0,0,0};
+        int n7 = 1;
+        System.out.println("Can Place Flowers: " + canPlaceFlowers(flowerbed7, n7) + "\n");
+
+        int [] flowerbed8 = {0,0,0};
+        int n8 = 2;
+        System.out.println("Can Place Flowers: " + canPlaceFlowers(flowerbed8, n8) + "\n");
+
     }
 
+
+    // I fucking hate this solution but its mine
     public static boolean canPlaceFlowers(int[] flowerbed, int n) {
 
+        // Step1 - Check if the length are greater than 2.
+        if (flowerbed.length == 1 && flowerbed[0] == 0)
+            return true;
+
+        // Step2 - Create a list to store flowebed
         List<Integer> list = new ArrayList<>();
 
+        // Step3 - Traverse the flowebed and store all the zeroe occurences in list
         int start = 0;
-        int prev = 0;
+        int prev = -1;
         for (int i=0; i < flowerbed.length; i++) {
             int current = flowerbed[i];
 
+            // Step4 - Check if current is 0 or 1
             if (current == 0) {
                 if (prev == current) {
-                    // Do nothing
+
+                    // If the last element is 0. Increase count since we  an place a flower at the end
+                    if (i == flowerbed.length - 1) {
+                        if (start == 0) {
+                            // Increase count again if the beginning is zero
+                            start = -1;
+                        }
+                        list.add(i - start + 2);
+                    }
+
                 } else {
                     start = i;
                 }
             } else {
                 if (prev == 0){
 
-//                    if (start == 0)
-//                        start =-1;
+                    if (start == 0) {
+                        start = -1;
+                    }
 
                     list.add(i - start);
                 }
             }
-
             prev = current;
         }
         System.out.println("list: " + list);
 
+        // Step4 - See if can subtract until we get to zero n
         int remaining = n;
         for (int numZeroes : list) {
             if (numZeroes % 2 == 0) {
                 numZeroes -= 1;
             }
 
+            // Step4b - Find the allowed ones using division. 3 can only store 1 flower. And 5 can store 2 flowes
             int allowedOnes = numZeroes / 2;
             remaining -= allowedOnes;
 
@@ -71,8 +107,6 @@ public class CanPlaceFlowers {
 
             System.out.println("numZeroes: " + numZeroes + "    allowedOnes: " + allowedOnes + "    remaining: " + remaining);
         }
-
-
 
         return remaining <= 0;
     }
