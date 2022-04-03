2cc86d826646369018aa57f6d766bfa4d8f9e87e
Tim M <timothy.romero.manas@gmail.com>
2022-04-03 19:17:39 -0400

    Adding remove one element to make array incr

--- Java/src/main/java/Lists/RemoveOneElementToMakeArrayStrictlyIncr/RemoveOneElementToMakeArrayStrictlyIncr.java
+++ Java/src/main/java/Lists/RemoveOneElementToMakeArrayStrictlyIncr/RemoveOneElementToMakeArrayStrictlyIncr.java
@@ -7,44 +7,58 @@ import java.util.TreeSet;
 
 public class RemoveOneElementToMakeArrayStrictlyIncr {
     public static void main(String [] args) {
-        int [][] input = {{1,2,10,5,7}, {2,3,1,2}, {1,1,1}, {100,21,3}};
+        int [][] input = {{1,2,10,5,7}, {2,3,1,2}, {1,1,1}, {100,21,3},{100,21,100}};
         for (int [] i : input)
             System.out.println("can be increasing: " + canBeIncreasing(i) + "\n");
     }
 
     public static boolean canBeIncreasing(int[] nums) {
-
         if (nums.length == 1)
             return false;
         if (nums.length == 2)
             return true;
 
-        // Step1 - Create set to see if all unique elements. Also sort them
-        Set<Integer> set = new TreeSet<>();
-        for (int i : nums) {
-            if (set.contains(i))
-                return false;
-            set.add(i);
-        }
-        System.out.println("Set: " + set);
 
-        // Step2 - Count numDiff
-        int numDiff = 0;
-        int index = 0;
-        List<Integer> sortedList = new ArrayList<>(set);
-        for (int i=0; i < set.size();i++) {
-            int num = nums[i];
-            int sortedNum = sortedList.get(index);
+        List<Integer> numsList = new ArrayList<>();
+        for (int i : nums) numsList.add(i);
 
-            if (num != sortedNum) {
-                numDiff++;
+        List<Integer> subList = new ArrayList<>();
+
+        for (int i=0; i<numsList.size(); i++) {
+            subList.clear();
+
+            int current = numsList.get(i);
+            int next = i == numsList.size() - 1 ? -1 : numsList.get(i + 1);
+            System.out.println("current: " + current + "    next: " + next);
+
+            if (current < next)
                 continue;
+
+            List<Integer> left = numsList.subList(0, i);
+            List<Integer> right = numsList.subList(i + 1, numsList.size());
+            subList.addAll(left);
+            subList.addAll(right);
+
+            // Check if subList Increasing
+            int prev = subList.get(0);
+            boolean isIncreasing = true;
+            for (int x=1; x<subList.size(); x++) {
+                if (prev >= subList.get(x)) {
+                    isIncreasing = false;
+                    break;
+                }
             }
 
-            index++;
-        }
-        System.out.println("Count Diff: " + numDiff);
+            if (isIncreasing)
+                return true;
 
-        return numDiff == 1;
+            System.out.println("left: " + left + "      right: " + right);
+
+        }
+
+
+        return false;
     }
+
+
 }
