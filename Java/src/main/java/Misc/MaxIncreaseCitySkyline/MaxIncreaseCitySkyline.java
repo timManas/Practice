a565029418a7697c1894dd9451d0e57489238db3
Tim M <timothy.romero.manas@gmail.com>
2022-04-21 21:33:58 -0400

    Adding max increase city skyline

---
+++ Java/src/main/java/Misc/MaxIncreaseCitySkyline/MaxIncreaseCitySkyline.java
@@ -0,0 +1,12 @@
+package Misc.MaxIncreaseCitySkyline;
+
+public class MaxIncreaseCitySkyline {
+    public static void main(String [] args) {
+        int [][] grid = {{3,0,8,4},{2,4,5,7},{9,2,6,3},{0,3,1,0}};
+        System.out.println("Max Increase: " + maxIncreaseKeepingSkyline(grid));
+    }
+
+    public static int maxIncreaseKeepingSkyline(int[][] grid) {
+
+    }
+}
