7cdf07807b873a08ab6ec781bb658907c29fa7ef
Tim M <timothy.romero.manas@gmail.com>
2022-04-23 00:45:42 -0400

    Adding algorithmn to keep increase in skyline

--- Java/src/main/java/Misc/MaxIncreaseCitySkyline/MaxIncreaseCitySkyline.java
+++ Java/src/main/java/Misc/MaxIncreaseCitySkyline/MaxIncreaseCitySkyline.java
@@ -1,5 +1,9 @@
 package Misc.MaxIncreaseCitySkyline;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class MaxIncreaseCitySkyline {
     public static void main(String [] args) {
         int [][] grid = {{3,0,8,4},{2,4,5,7},{9,2,6,3},{0,3,1,0}};
@@ -8,13 +12,26 @@ public class MaxIncreaseCitySkyline {
 
     public static int maxIncreaseKeepingSkyline(int[][] grid) {
         int max = 0;
+        List<Integer> rowHighest = new ArrayList<>();
+        List<Integer> colHighest = new ArrayList<>();
 
+        // Populate rowHighest
         for (int row=0; row < grid.length; row++) {
-            for (int col=0; col < grid[row].length; col++) {
-                int current = grid[row][col];
-                System.out.println("Current: " + current);
-            }
+            int [] rowArr = Arrays.copyOf(grid[row], grid.length);
+            Arrays.sort(rowArr);
+            rowHighest.add(rowArr[rowArr.length - 1]);
         }
+        System.out.println("rowHighest: " + rowHighest);
+
+        // Populate colHighest
+        for (int col=0; col < grid.length; col++) {
+            int largest = -1;
+            for (int row=0; row < grid[col].length; row++) {
+                largest = largest < grid[row][col] ? grid[row][col] : largest;
+            }
+            colHighest.add(largest);
+        }
+        System.out.println("colHighest: " + colHighest);
 
         return max;
     }
