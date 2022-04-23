ced5bb68a48ded683d95fe22f9e094b0ce6fa42d
Tim M <timothy.romero.manas@gmail.com>
2022-04-23 01:01:46 -0400

    Adding max increase keeping skyline

--- Java/src/main/java/Misc/MaxIncreaseCitySkyline/MaxIncreaseCitySkyline.java
+++ Java/src/main/java/Misc/MaxIncreaseCitySkyline/MaxIncreaseCitySkyline.java
@@ -12,26 +12,56 @@ public class MaxIncreaseCitySkyline {
 
     public static int maxIncreaseKeepingSkyline(int[][] grid) {
         int max = 0;
-        List<Integer> rowHighest = new ArrayList<>();
-        List<Integer> colHighest = new ArrayList<>();
 
-        // Populate rowHighest
+        // Step1 - Create list to store the max of each row and max of each column
+        List<Integer> rowMaxList = new ArrayList<>();
+        List<Integer> colMaxList = new ArrayList<>();
+
+        // Step2 - Populate rowHighest
         for (int row=0; row < grid.length; row++) {
-            int [] rowArr = Arrays.copyOf(grid[row], grid.length);
-            Arrays.sort(rowArr);
-            rowHighest.add(rowArr[rowArr.length - 1]);
+            int largest = -1;
+            for (int col=0; col < grid.length; col++) {
+                largest = largest < grid[row][col] ? grid[row][col] : largest;
+            }
+            rowMaxList.add(largest);
         }
-        System.out.println("rowHighest: " + rowHighest);
+        System.out.println("rowHighest: " + rowMaxList);
 
-        // Populate colHighest
+        // Step3 - Populate colHighest
         for (int col=0; col < grid.length; col++) {
             int largest = -1;
             for (int row=0; row < grid[col].length; row++) {
                 largest = largest < grid[row][col] ? grid[row][col] : largest;
             }
-            colHighest.add(largest);
+            colMaxList.add(largest);
         }
-        System.out.println("colHighest: " + colHighest);
+        System.out.println("colHighest: " + colMaxList);
+
+
+        // Step4 - Find the replaced value
+        // The max is difference between the replaced value and the current value
+        for (int row=0; row < grid.length; row++) {
+            for (int col=0; col < grid[row].length; col++) {
+                int current = grid[row][col];
+                int rowMax = rowMaxList.get(row);
+                int colMax = colMaxList.get(col);
+
+                // If currentValue is equal to the max, we  move on
+                // Why ? Because its already the max. Changing this would change the skyline
+                if (rowMax == current || colMax == current) {
+                    System.out.print(current + ",");
+                    continue;
+                }
+
+                // Step5 - Find the replaced value. This is the value we put in the array
+                // The max is the difference between the current and the replaced value
+                int replacedValue = Math.min(rowMax, colMax);
+                max += Math.abs(replacedValue - current);
+
+                System.out.print(Math.min(rowMax, colMax) + ",");
+            }
+            System.out.println();
+        }
 
         return max;
     }
