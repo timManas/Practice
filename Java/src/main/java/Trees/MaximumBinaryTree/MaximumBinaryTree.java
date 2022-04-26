8dd83fe419ebc146ef12c52d24e14bc8cb6c8b2f
Tim M <timothy.romero.manas@gmail.com>
2022-04-26 19:28:08 -0400

    Adding Max binary tree:

---
+++ Java/src/main/java/Trees/MaximumBinaryTree/MaximumBinaryTree.java
@@ -0,0 +1,15 @@
+package Trees.MaximumBinaryTree;
+
+public class MaximumBinaryTree {
+    public static void main(String [] args) {
+        int [] nums = {3,2,1,6,0,5};
+        TreeNode node = constructMaximumBinaryTree(nums);
+    }
+
+    private static void printNode(TreeNode constructMaximumBinaryTree) {
+    }
+
+    public static TreeNode constructMaximumBinaryTree(int[] nums) {
+
+    }
+}
