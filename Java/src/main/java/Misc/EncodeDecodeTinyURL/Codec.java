3a309d39235cb3d4854128db354b80dc29549511
Tim M <timothy.romero.manas@gmail.com>
2022-05-03 21:38:32 -0400

    Adding Encode and decode tiny url

--- Java/src/main/java/Misc/EncodeDecodeTinyURL/Codec.java
+++ Java/src/main/java/Misc/EncodeDecodeTinyURL/Codec.java
@@ -1,14 +1,21 @@
 package Misc.EncodeDecodeTinyURL;
 
+import com.sun.xml.internal.rngom.parse.host.Base;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+
 public class Codec {
 
     // Encodes a URL to a shortened URL.
-    public String encode(String longUrl) {
-
+    public String encode(String url) {
+        String encodedString = Base64.getEncoder().encodeToString(url.getBytes());
+        return encodedString;
     }
 
     // Decodes a shortened URL to its original URL.
-    public String decode(String shortUrl) {
-
+    public String decode(String url) {
+        byte[] decodedString = Base64.getDecoder().decode(url);
+        return new String(decodedString);
     }
 }
