c3eb1a53f602f29aca5ad094c1c98a4eabf1e2f9
Tim M <timothy.romero.manas@gmail.com>
2022-05-12 23:24:03 -0400

    Adding Design tree

--- Java/src/main/java/Trees/DesignExpressionTree/DesignExpressionTree.java
+++ Java/src/main/java/Trees/DesignExpressionTree/DesignExpressionTree.java
@@ -2,9 +2,14 @@ package Trees.DesignExpressionTree;
 
 public class DesignExpressionTree {
     public static void main (String [] args) {
-        String [] s = {"3","4","+","2","*","7","/"};
-         TreeBuilder treeBuilder = new TreeBuilder();
+        String [][] postfix = {{"3","4","+","2","*","7","/"}, {"4","5","2","7","+","-","*"}};
 
+        for (String [] i : postfix) {
+            TreeBuilder obj = new TreeBuilder();
+            Node expTree = obj.buildTree(i);
+            int ans = expTree.evaluate();
+            System.out.println("Ans: " + ans);
+        }
     }
 
 
