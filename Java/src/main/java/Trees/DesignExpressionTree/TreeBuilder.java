387f5faed1d833c8f1cc2c8088e2e5b25d17cd90
Tim M <timothy.romero.manas@gmail.com>
2022-05-13 23:56:09 -0400

    Created initial prototype

--- Java/src/main/java/Trees/DesignExpressionTree/TreeBuilder.java
+++ Java/src/main/java/Trees/DesignExpressionTree/TreeBuilder.java
@@ -1,5 +1,7 @@
 package Trees.DesignExpressionTree;
 
+import java.util.Stack;
+
 public class TreeBuilder {
     Node buildTree(String[] postfix) {
         /**
@@ -7,8 +9,23 @@ public class TreeBuilder {
 
          */
 
+        Stack<String> stack = new Stack<>();
         for (String i : postfix) {
-            if (i.)
+            if (i.equalsIgnoreCase("+") ||
+                    i.equalsIgnoreCase("-") ||
+                    i.equalsIgnoreCase("*") ||
+                    i.equalsIgnoreCase("/")) {
+
+                int num1 = Integer.parseInt(stack.pop());
+                int num2 = Integer.parseInt(stack.pop());
+                System.out.println("num1: " + num1 + "      num2: " + num2 + "      total: " + (num1 + num2));
+
+            } else {
+               stack.push(i);
+            }
+
+
+
         }
     }
 }
