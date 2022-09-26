fbf740860ee826c9bd3bf0b35cca962d8b71a1de
Tim M <timothy.romero.manas@gmail.com>
2022-09-26 16:52:37 -0400

    Adding license key formatting

---
+++ JavaScript/backend/src/Strings/LicenseKeyFormatting/LicenseKeyFormatting.js
@@ -0,0 +1,9 @@
+var licenseKeyFormatting = function (string, k) {}
+
+let s = '5F3Z-2e-9-w'
+let k = 4
+let output = licenseKeyFormatting(s, k)
+
+s = '2-5g-3-J'
+k = 2
+output = licenseKeyFormatting(s, k)
