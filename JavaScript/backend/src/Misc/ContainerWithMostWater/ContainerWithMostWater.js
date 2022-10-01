88f5c7fb92892fdc5ab3570e15d7c025d5e61123
Tim M <timothy.romero.manas@gmail.com>
2022-10-01 17:18:53 -0400

    Adding container with most water?

---
+++ JavaScript/backend/src/Misc/ContainerWithMostWater/ContainerWithMostWater.js
@@ -0,0 +1,10 @@
+var maxArea = function (height) {}
+
+let input = [
+  [1, 8, 6, 2, 5, 4, 8, 3, 7],
+  [1, 1],
+]
+for (let i = 0; i < input.length; i++) {
+  let arr = input[i]
+  console.log('Max Area: ' + maxArea(arr))
+}
