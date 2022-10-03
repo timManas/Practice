3cbd664323d69c2ca73f738d56446d903e9f144a
Tim M <timothy.romero.manas@gmail.com>
2022-10-02 20:36:11 -0400

    Adding 3Sum

--- JavaScript/backend/src/Math/3Sum/3Sum.js
+++ JavaScript/backend/src/Math/3Sum/3Sum.js
@@ -1,17 +1,41 @@
-var threeSum = function(nums) {
-    nums.sort(function(a,b) {return a-b})
-    console.log("nums: " + nums)
+var threeSum = function (nums) {
+  let output = []
+  nums.sort(function (a, b) {
+    return a - b
+  })
+  console.log('nums: ' + nums)
 
-    for (let x=0; x < nums.length - 2; x++) {
-        for (let y=x+1; y < nums.length - 1; y++) {
-            console.log("x: " + nums[x] + "     y:" + nums[y])
+  for (let x = 0; x < nums.length - 2; x++) {
+    for (let y = x + 1; y < nums.length - 1; y++) {
+      let remainder = -nums[x] - nums[y]
+
+      for (let z = y + 1; z < nums.length; z++) {
+        console.log(
+          'x: ' +
+            nums[x] +
+            '     y:' +
+            nums[y] +
+            '    z:' +
+            nums[z] +
+            '   remainder: ' +
+            remainder
+        )
+        if (nums[z] == remainder) {
+          console.log('Found ----')
+          output.push([nums[x], nums[y], remainder])
         }
+      }
     }
+  }
+  console.log('output: ' + output)
+  return output
+}
 
-};
-
-
-let input = [[-3,0,1,2,-1,-4], [0,1,1], [0,0,0]] 
-for (let i=0; i < input.length; i++) {
-    console.log("All tripletS: " + threeSum(input[i]) + "\n")
+let input = [
+  [-1, 0, 1, 2, -1, -4],
+  [0, 1, 1],
+  [0, 0, 0],
+]
+for (let i = 0; i < input.length; i++) {
+  console.log('All tripletS: ' + threeSum(input[i]) + '\n')
 }
