2bf8599b5935fe441d698ddfff0af5b06629a0c6
Tim M <timothy.romero.manas@gmail.com>
2022-10-05 22:21:08 -0400

    Adding rotate image

---
+++ JavaScript/backend/src/Misc/RotateImage/RotateImage.js
@@ -0,0 +1,16 @@
+var rotate = function (matrix) {}
+
+let matrix = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+]
+console.log('Rotated: ' + rotate(matrix))
+
+matrix = [
+  [5, 1, 9, 11],
+  [2, 4, 8, 10],
+  [13, 3, 6, 7],
+  [15, 14, 12, 16],
+]
+console.log('Rotated: ' + rotate(matrix))
