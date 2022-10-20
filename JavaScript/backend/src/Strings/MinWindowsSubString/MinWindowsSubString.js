424b45ae5ff330ecbea387264dea2f5ef8324234
Tim M <timothy.romero.manas@gmail.com>
2022-10-19 20:27:32 -0400

    Adding some changes

--- JavaScript/backend/src/Strings/MinWindowsSubString/MinWindowsSubString.js
+++ JavaScript/backend/src/Strings/MinWindowsSubString/MinWindowsSubString.js
@@ -3,20 +3,71 @@ var minWindow = function (s, t) {
   if (s.length < t.length) return false
 
   let sArr = s.split('')
-  let tArr = t.split('').sort()
-  console.log('tArr: ' + tArr)
+
+  let subString = []
 
   for (let i = 0; i < sArr.length; i++) {
+    tArr = t.split('').sort()
+
     let letter = s[i]
     console.log('Current letter: ' + letter)
 
-    sArr[i] = '-'
-    if (tArr.includes(letter)) {
-      sArr[i] = letter
+    // If letter is not a target, move on
+    if (!tArr.includes(letter)) {
+      continue
+    }
+
+    // Set the indexes for left and right
+    let current = i
+    let increment = 1
+    tArr.splice(tArr.indexOf(letter), 1)
+
+    // Create the temp Array
+    let temp = [letter]
+    while (true) {
+      let left = sArr[current - increment]
+      let right = sArr[current + increment]
+
+      console.log('left: ' + left + '   right: ' + right)
+
+      // Check Left
+      if (left != undefined) {
+        if (tArr.includes(left)) {
+          tArr.splice(tArr.indexOf(left), 1)
+        }
+        temp.unshift(left)
+      }
+
+      // Check if length breached
+      console.log('temp: ' + temp)
+      if (tArr.length == 0) {
+        console.log('----temp: ' + temp.join(''))
+        subString.push(temp.join(''))
+        break
+      }
+
+      // Check Right
+      if (right != undefined) {
+        if (tArr.includes(right)) {
+          tArr.splice(tArr.indexOf(right), 1)
+        }
+        temp.push(right)
+      }
+
+      // Check if length breached
+      if (tArr.length == 0) {
+        console.log('----temp: ' + temp.join(''))
+        subString.push(temp.join(''))
+        break
+      }
+
+      increment++
     }
   }
+  subString.sort()
+  console.log('subStirng: ' + subString)
 
-  console.log('sArr: ' + sArr)
+  return subString[0]
 }
 
 let s = 'ADOBECODEBANC'
