c1619c3fc5ed897a495b2ad0ed1b099ef7868935
Tim M <timothy.romero.manas@gmail.com>
2022-10-25 21:32:39 -0400

    Adding longest substriing for 2 distinct charS

---
+++ JavaScript/backend/src/Strings/LongestSubStringAtMost2DistinctCharacters/LongestSubStringAtMost2DistinctCharacters.js
@@ -0,0 +1,8 @@
+var lengthOfLongestSubstringTwoDistinct = function (s) {
+  console.log('s: ' + s)
+}
+
+let input = ['eceba', 'ccaabbb']
+for (let i of input) {
+  console.log('Length Of SubString:' + lengthOfLongestSubstringTwoDistinct(i))
+}
