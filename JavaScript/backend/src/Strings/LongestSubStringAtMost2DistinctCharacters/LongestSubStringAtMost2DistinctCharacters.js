dbcc9f0de34d448b52f8152412b84afbb11efa64
Tim M <timothy.romero.manas@gmail.com>
2022-10-25 22:27:38 -0400

    Adding longest substring

--- JavaScript/backend/src/Strings/LongestSubStringAtMost2DistinctCharacters/LongestSubStringAtMost2DistinctCharacters.js
+++ JavaScript/backend/src/Strings/LongestSubStringAtMost2DistinctCharacters/LongestSubStringAtMost2DistinctCharacters.js
@@ -1,8 +1,50 @@
 var lengthOfLongestSubstringTwoDistinct = function (s) {
+  s = s.split('')
   console.log('s: ' + s)
+
+  let start = 0
+  let end = 0
+  while (start < s.length - 1) {
+    while (end < s.length) {
+      console.log('start: ' + start + '   end: ' + end)
+
+      let letterStart = s[start]
+      let letterEnd = s[end]
+      console.log(
+        'letterStart: ' + letterStart + '         letterEnd: ' + letterEnd
+      )
+
+      end++
+    }
+
+    start++
+  }
 }
 
 let input = ['eceba', 'ccaabbb']
 for (let i of input) {
-  console.log('Length Of SubString:' + lengthOfLongestSubstringTwoDistinct(i))
+  console.log(
+    'Length Of SubString:' + lengthOfLongestSubstringTwoDistinct(i) + '\n'
+  )
 }
+
+/**
+ 
+aaabcxxxxxcbmmm
+
+abcxx - 
+xx -
+xxx -> xx
+xxcbmm - 
+
+
+aaabccbxxxxxmmm
+aa
+aaa > aa 
+aabccbxx - 
+aabccbxxx > xx
+xxx > xx 
+xxx > xx
+xxmm - 
+
+ */
