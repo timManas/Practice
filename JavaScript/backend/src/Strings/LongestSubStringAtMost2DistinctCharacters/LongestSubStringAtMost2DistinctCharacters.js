1b2f85839f106e43da8eff8033c6fb80db82e598
Tim M <timothy.romero.manas@gmail.com>
2022-10-27 23:36:03 -0400

    Finished Longest substring

--- JavaScript/backend/src/Strings/LongestSubStringAtMost2DistinctCharacters/LongestSubStringAtMost2DistinctCharacters.js
+++ JavaScript/backend/src/Strings/LongestSubStringAtMost2DistinctCharacters/LongestSubStringAtMost2DistinctCharacters.js
@@ -1,61 +1,59 @@
 var lengthOfLongestSubstringTwoDistinct = function (s) {
-  // s = s.split('')
+  if (s.length == 2) return s.length
+
   console.log('s: ' + s)
 
-  let set = new Set()
+  let longest = ''
   let map = new Map()
 
   let start = 0
-  while (start < s.length - 1) {
+  let end = start
+  while (end < s.length) {
     console.log('start: ' + s[start])
 
-    let end = start
-    while (end < s.length) {
-      let letterEnd = s[end]
-      console.log('end: ' + letterEnd)
+    let letterEnd = s[end]
+    console.log('end: ' + letterEnd)
 
-      // Add to map
-      let count = 1
-      if (map.has(letterEnd)) {
-        count = map.get(letterEnd) + 1
-      }
-      map.set(letterEnd, count)
+    // Add to map
+    let count = 1
+    if (map.has(letterEnd)) {
+      count = map.get(letterEnd) + 1
+    }
+    map.set(letterEnd, count)
 
-      // Add to set
-      if (map.size == 2) {
-        set.add(s.substring(start, end + 1))
-        console.log(
-          '   Added: ' +
-            s.substring(start, end + 1) +
-            '     set: ' +
-            Array.from(set)
-        )
-      }
-
-      console.log(map)
-
-      // Remove element if more than 2
-      while (map.size >= 3) {
-        // Remove first Element
-        let letterStart = s[start]
-
-        // Remove from Map
-        map.set(letterStart, map.get(letterStart) - 1)
-        if (map.get(letterStart) == 0) {
-          map.delete(letterStart)
-        }
-
-        ++start
-      }
-
-      end++
+    // Add to set
+    if (map.size == 2) {
+      longest =
+        longest.length < s.substring(start, end + 1).length
+          ? s.substring(start, end + 1)
+          : longest
     }
 
-    start++
+    console.log(map)
+
+    // Remove element if more than 2
+    while (map.size >= 3) {
+      // Remove first Element
+      let letterStart = s[start]
+
+      // Remove from Map
+      map.set(letterStart, map.get(letterStart) - 1)
+      if (map.get(letterStart) == 0) {
+        map.delete(letterStart)
+      }
+
+      ++start
+    }
+
+    end++
   }
+
+  if (longest.length == 0) return map.get(s[0])
+
+  return longest.length
 }
 
-let input = ['eceba', 'ccaabbb']
+let input = ['eceba', 'ccaabbb', 'aaaa']
 for (let i of input) {
   console.log(
     'Length Of SubString:' + lengthOfLongestSubstringTwoDistinct(i) + '\n'
