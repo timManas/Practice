091f597ec957226b9709f8d44920c05b09336e7b
Tim M <timothy.romero.manas@gmail.com>
2022-10-27 23:20:46 -0400

    Adding LongestSubstring

--- JavaScript/backend/src/Strings/LongestSubStringAtMost2DistinctCharacters/LongestSubStringAtMost2DistinctCharacters.js
+++ JavaScript/backend/src/Strings/LongestSubStringAtMost2DistinctCharacters/LongestSubStringAtMost2DistinctCharacters.js
@@ -1,18 +1,52 @@
 var lengthOfLongestSubstringTwoDistinct = function (s) {
-  s = s.split('')
+  // s = s.split('')
   console.log('s: ' + s)
 
-  let start = 0
-  let end = 0
-  while (start < s.length - 1) {
-    while (end < s.length) {
-      console.log('start: ' + start + '   end: ' + end)
+  let set = new Set()
+  let map = new Map()
 
-      let letterStart = s[start]
+  let start = 0
+  while (start < s.length - 1) {
+    console.log('start: ' + s[start])
+
+    let end = start
+    while (end < s.length) {
       let letterEnd = s[end]
-      console.log(
-        'letterStart: ' + letterStart + '         letterEnd: ' + letterEnd
-      )
+      console.log('end: ' + letterEnd)
+
+      // Add to map
+      let count = 1
+      if (map.has(letterEnd)) {
+        count = map.get(letterEnd) + 1
+      }
+      map.set(letterEnd, count)
+
+      // Add to set
+      if (map.size == 2) {
+        set.add(s.substring(start, end + 1))
+        console.log(
+          '   Added: ' +
+            s.substring(start, end + 1) +
+            '     set: ' +
+            Array.from(set)
+        )
+      }
+
+      console.log(map)
+
+      // Remove element if more than 2
+      while (map.size >= 3) {
+        // Remove first Element
+        let letterStart = s[start]
+
+        // Remove from Map
+        map.set(letterStart, map.get(letterStart) - 1)
+        if (map.get(letterStart) == 0) {
+          map.delete(letterStart)
+        }
+
+        ++start
+      }
 
       end++
     }
@@ -29,14 +63,26 @@ for (let i of input) {
 }
 
 /**
- 
+
+1. 
+
+
+eceba
+ece - 
+ceba 
+
+ccaabbb
+cc - 
+caa -
+abb -
+bb -
+b
+
+
 aaabcxxxxxcbmmm
-
-abcxx - 
-xx -
-xxx -> xx
-xxcbmm - 
-
+aaab -
+aaabc > aabc > abc > bc 
+bcx > cx
 
 aaabccbxxxxxmmm
 aa
