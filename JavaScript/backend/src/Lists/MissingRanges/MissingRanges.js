622aaad014c25c81856fecc9dd9a7cdf3fd9bad9
Tim M <timothy.romero.manas@gmail.com>
2022-11-01 19:53:10 -0400

    Finished Missing ranges

--- JavaScript/backend/src/Lists/MissingRanges/MissingRanges.js
+++ JavaScript/backend/src/Lists/MissingRanges/MissingRanges.js
@@ -1,53 +1,59 @@
 var findMissingRanges = function (nums, lower, upper) {
   let output = []
 
-  let current = lower
-  let next = nums[0]
-  let key = '->'
+  // Step1 - If empty array, we just add the lower and upper
+  if (nums.length == 0) {
+    let limit = lower + '->' + upper
+    if (lower == upper) limit = '' + upper + ''
+    output.push(limit)
+    return output
+  }
 
+  // Step2 - Create the variables
+  let current = lower
+  let next = 0
+  let limit = '->'
+
+  // Step3 - Check if lower limits
   if (lower < nums[0]) {
     // Add to output
-    key = current + '->' + (nums[0] - 1)
+    limit = current + '->' + (nums[0] - 1)
 
-    if (current == nums[0] - 1) key = '' + lower + ''
-    output.push(key)
+    if (current == nums[0] - 1) limit = '' + lower + ''
+    output.push(limit)
   }
   console.log('output: ' + output)
 
+  // Step4 - Traverse the nums Array
   for (let i = 0; i < nums.length - 1; i++) {
     current = nums[i]
-    let next = nums[i + 1]
+    next = nums[i + 1]
+    console.log('current:' + current + '    next: ' + next)
 
-    let range = next - current
-    console.log(
-      'current:' + current + '    next: ' + next + '      range: ' + range
-    )
-
-    if (nums.includes(range)) {
+    // If current + 1 is in the array, we skip
+    if (nums.includes(current + 1) || nums.includes(next - 1)) {
       continue
     }
 
-    key = current + 1 + '->' + (next - 1)
-    if (current + 1 == next - 1) key = (current + 1).toString()
+    // Create the limit
+    limit = current + 1 + '->' + (next - 1)
+    if (current + 1 == next - 1) limit = (current + 1).toString()
 
-    output.push(key)
+    output.push(limit)
     console.log('output: ' + output)
   }
 
+  // Step5 -  Check upper limits limits
   if (upper == nums[nums.length - 1]) {
     return output
   }
 
   current = nums[nums.length - 1] + 1
-  key = current + '->' + upper
+  limit = current + '->' + upper
+  if (current == upper) limit = '' + current + ''
 
-  if (nums.length == 0) {
-    key = lower + '->' + upper
-
-    if (lower == upper) key = '' + upper + ''
-  }
-
-  output.push(key)
+  // Push to output
+  output.push(limit)
 
   return output
 }
@@ -83,6 +89,20 @@ console.log(
 nums = [-1]
 lower = -1
 upper = 0
+console.log(
+  'Smallest Range of Missing #: ' + findMissingRanges(nums, lower, upper) + '\n'
+)
+
+nums = []
+lower = -3
+upper = -1
+console.log(
+  'Smallest Range of Missing #: ' + findMissingRanges(nums, lower, upper) + '\n'
+)
+
+nums = [0, 9]
+lower = 0
+upper = 9
 console.log(
   'Smallest Range of Missing #: ' + findMissingRanges(nums, lower, upper) + '\n'
 )
