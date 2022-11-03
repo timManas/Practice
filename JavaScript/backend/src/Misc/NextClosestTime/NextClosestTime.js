466a3c165fe0129edd14de6ad436dbddd0e9693b
Tim M <timothy.romero.manas@gmail.com>
2022-11-03 19:32:19 -0400

    Adding next closest time

--- JavaScript/backend/src/Misc/NextClosestTime/NextClosestTime.js
+++ JavaScript/backend/src/Misc/NextClosestTime/NextClosestTime.js
@@ -1,16 +1,17 @@
 var nextClosestTime = function (time) {
   // Get Digits & Sort
-  time = time.substring(0, 2) + '' + time.substring(3, 5)
-  let digits = time.split('').sort()
-  console.log('digits: ' + digits)
+  let digits = time.substring(0, 2) + '' + time.substring(3, 5)
+  digits = time.split('').sort()
+  console.log('digits: ' + time)
 
   // Start from the Right moving up
   for (let i = time.length - 1; i >= 0; i--) {
     let current = time[i]
+    if (current == ':') continue
   }
 }
 
-let input = ['19:34', '23:59']
+let input = ['19:34', '23:59', '19:59']
 for (let i = 0; i < input.length; i++) {
   console.log('closestTime: ' + nextClosestTime(input[i]) + '\n')
 }
