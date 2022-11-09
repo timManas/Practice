59d5e75ead9768a452ba39af4393f1330f87cddc
Tim M <timothy.romero.manas@gmail.com>
2022-11-08 19:12:01 -0500

    Adding Next closest time

--- JavaScript/backend/src/Misc/NextClosestTime/NextClosestTime.js
+++ JavaScript/backend/src/Misc/NextClosestTime/NextClosestTime.js
@@ -9,19 +9,19 @@ var nextClosestTime = function (time) {
   let validValues = []
 
   // Start from the Right moving up
-  let isSettled = false
+  let isSettled = true
   loop: for (let i = time.length - 1; i >= 0; i--) {
     let current = time[i]
     console.log('current: ' + current)
 
     let indexPrev = i == 0 ? null : i - 1
-    let prev = time[indexPrev]
+    let prev = parseInt(time[indexPrev])
 
     let indexNext =
       digits.indexOf(current) == digits.length - 1
         ? 0
         : digits.indexOf(current) + 1
-    let next = digits[indexNext]
+    let next = parseInt(digits[indexNext])
     console.log(
       'current: ' + current + '     prev: ' + prev + '   next:' + next
     )
@@ -40,49 +40,37 @@ var nextClosestTime = function (time) {
       // Allowed values 0-6
       validValues = [0, 1, 2, 3, 4, 5]
 
-      while (true) {
-        if (validValues.includes(next)) {
-          time[i] = next
-          console.log('------ Time:' + time)
-          if (indexNext > digits.indexOf(current)) isSettled = true // This needs to change
-          break
-        }
-
-        indexNext =
-          parseInt(digits.indexOf(current)) == digits.length - 1
-            ? 0
-            : indexNext + 1
-        next = parseInt(digits[indexNext])
-        console.log('next:' + next)
-      }
-
       if (isSettled) break loop
     } else if (i == 3) {
       // Allowed values 0-5:0-9
       validValues = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 
+      let shouldUpdatePrev = false
+
       while (true) {
         if (validValues.includes(next)) {
           time[i] = next
           console.log('------ Time:' + time)
-          if (indexNext > digits.indexOf(current)) isSettled = true // This needs to change
           break
         }
 
-        indexNext =
-          digits.indexOf(current) == digits.length - 1
-            ? 0
-            : digits.indexOf(current) + 1
+        if (indexNext == digits.length - 1) {
+          indexNext = 0
+          shouldUpdatePrev = true
+        } else {
+          ++indexNext
+        }
+
         next = parseInt(digits[indexNext])
         console.log('next:' + next)
       }
 
-      if (isSettled) break loop
+      if (!shouldUpdatePrev) break loop
     }
   }
 }
-
-let input = ['19:34', '23:59', '19:59', '24:00', '23:59']
+// '19:34','23:59','19:59',
+let input = ['24:00', '23:59']
 for (let i = 0; i < input.length; i++) {
   console.log('closestTime: ' + nextClosestTime(input[i]) + '\n')
 }
