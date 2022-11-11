7750b22bfee5a347cb28cad900bcae1374b7dc7a
Tim M <timothy.romero.manas@gmail.com>
2022-11-10 23:30:35 -0500

    Dev plan expressive words

--- JavaScript/backend/src/Strings/ExpressiveWords/ExpressiveWords.js
+++ JavaScript/backend/src/Strings/ExpressiveWords/ExpressiveWords.js
@@ -1,5 +1,41 @@
-var expressiveWords = function (s, words) {}
+var expressiveWords = function (s, words) {
+  console.log('s: ' + s + '       words: ' + words)
+  let numWords = 0
+
+  for (let word in words) {
+    console.log('Word: ' + word)
+
+    // Split the word
+
+    // Split s
+
+    numWords++
+  }
+
+  return numWords
+}
 
 let s = 'heeellooo'
-let words = ['hello', 'hi', 'helo']
+let words = ['hello', 'hi', 'helo', 'hellol', 'heeello']
 console.log('Expressive Word: ' + expressiveWords(s, words) + '\n')
+
+s = 'zzzzzyyyyy'
+words = ['zzyy', 'zy', 'zyy']
+console.log('Expressive Word: ' + expressiveWords(s, words) + '\n')
+
+/**
+ zzz zz yy yyy
+
+ Solution #1
+ 1. Split s into groups
+ 2. Reduce the number to the word
+ Ex: h | eee | l | l | ooo
+     h | e   | l | l | o
+     h | e   | l | o | 
+
+Ex: zzzzz | yyyyy
+    zz    | yy
+    z     | y
+    z     | yy
+
+ */
