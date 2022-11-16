d8b34a95cdb2caa1b1f1a6dc5246c35515962182
Tim M <timothy.romero.manas@gmail.com>
2022-11-15 20:02:22 -0500

    Finished Expressive words

--- JavaScript/backend/src/Strings/ExpressiveWords/ExpressiveWords.js
+++ JavaScript/backend/src/Strings/ExpressiveWords/ExpressiveWords.js
@@ -2,14 +2,16 @@ var expressiveWords = function (s, words) {
   console.log('s: ' + s + '       words: ' + words)
   let numWords = 0
 
+  // Step1 - Traverse each word
   for (let word of words) {
     console.log('Word: ' + word)
 
-    // Split s & word
+    // Step2 - Split s & word
     let sGroup = splitWord(s)
     let wordGroup = splitWord(word)
     console.log('   sGroup: ' + sGroup + '     wordGroup: ' + wordGroup)
 
+    // Step3 - Check if isExpressive
     if (isExpressive(sGroup, wordGroup)) {
       console.log('   -- Adding: word')
       numWords++
@@ -19,29 +21,38 @@ var expressiveWords = function (s, words) {
   return numWords
 }
 
+// This method checks certain criterias are met
+// Honestly, for the most part its just trial and error
+// Note, its not mentioned in the description BUT "cc" -> "ccc" (just has to be three or more)
 function isExpressive(sGroup, wordGroup) {
   let isExpressive = false
 
-  // Check grouping length
+  // Criteria1 - Check grouping length when split
   if (sGroup.length != wordGroup.length) return isExpressive
 
+  // Traverse each Grouping of each word
   for (let i = 0; i < sGroup.length; i++) {
     console.log('   sGroup: ' + sGroup[i] + '     wordGroup: ' + wordGroup[i])
-    if (sGroup[i] == wordGroup[i]) continue // If both sides are the same we continue forward. Ex: h & h
 
+    // Criteria#2 - // If both sides are the same we continue forward. Ex: h & h
+    if (sGroup[i] == wordGroup[i]) continue
+
+    // Critera#3 - Check if the letters are the same
     if (sGroup[i][0] != wordGroup[i][0]) return false
 
-    if (sGroup.length < wordGroup.length) return false
+    // Criteria#4 - Check sGroup[i] is bigger than wordGroup[i]
+    if (sGroup[i].length < wordGroup[i].length) return false
 
+    // Criteria#5 - Find the difference and check if diff is 1 and more
     let difference = parseInt(sGroup[i].length) - parseInt(wordGroup[i].length)
     console.log('   diff: ' + difference)
-
-    if (difference <= 1) return false
+    if ((difference <= 1) & (wordGroup[i].length == 1)) return false
   }
 
   return true
 }
 
+// Split the letters by grouping
 function splitWord(word) {
   let output = []
 
