ba45da87e3b7ade743bd283c09df5f1a8c70a0d2
Tim M <timothy.romero.manas@gmail.com>
2022-11-19 09:38:10 -0500

    Adding Find and replace string

--- JavaScript/backend/src/Strings/FindAndReplaceString/FindAndReplaceString.js
+++ JavaScript/backend/src/Strings/FindAndReplaceString/FindAndReplaceString.js
@@ -1,36 +1,25 @@
 var findReplaceString = function (s, indices, sources, targets) {
   console.log('s: ' + s)
   // Traverse the indices
-  let output = ''
-  for (let i = 0; i < s.length; i++) {
-    if (!indices.includes(i)) {
-      output += s[i]
-      console.log('     output: ' + output)
-      continue
-    }
-
-    let indice = indices[indices.indexOf(i)]
-
-    let subString = s.substring(
-      indice,
-      indice + sources[indices.indexOf(i)].length
-    )
+  let output = s
+  for (let i = 0; i < indices.length; i++) {
+    let indice = indices[i]
+    let subString = s.substring(indice, indice + sources[i].length)
     console.log(
       '   current: ' +
         indice +
         '   sources: ' +
-        sources[indices.indexOf(i)] +
+        sources[i] +
         '   target: ' +
-        targets[indices.indexOf(i)] +
+        targets[i] +
         '    subString: ' +
         subString
     )
 
-    if (subString == sources[indices.indexOf(i)]) {
-      output += targets[indices.indexOf(i)]
+    if (subString == sources[i]) {
+      output = output.replace(subString, targets[i])
     }
 
-    i = i + targets[indices.indexOf(i)].length
     console.log('     output: ' + output)
   }
 
