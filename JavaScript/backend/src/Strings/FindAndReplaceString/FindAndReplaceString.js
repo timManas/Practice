7d53068d78321a921300f0683b31f729f0996547
Tim M <timothy.romero.manas@gmail.com>
2022-11-19 09:57:24 -0500

    Adding Find and Replace String

--- JavaScript/backend/src/Strings/FindAndReplaceString/FindAndReplaceString.js
+++ JavaScript/backend/src/Strings/FindAndReplaceString/FindAndReplaceString.js
@@ -26,18 +26,100 @@ var findReplaceString = function (s, indices, sources, targets) {
   return output
 }
 
-let s = 'abcd'
-let indices = [0, 2]
-let sources = ['a', 'cd']
-let targets = ['eee', 'ffff']
-console.log(
-  'Replaced word: ' + findReplaceString(s, indices, sources, targets) + '\n'
-)
+// let s = 'abcd'
+// let indices = [0, 2]
+// let sources = ['a', 'cd']
+// let targets = ['eee', 'ffff']
+// console.log(
+//   'Replaced word: ' + findReplaceString(s, indices, sources, targets) + '\n'
+// )
 
-s = 'abcd'
-indices = [0, 2]
-sources = ['ab', 'ec']
-targets = ['eee', 'ffff']
+// s = 'abcd'
+// indices = [0, 2]
+// sources = ['ab', 'ec']
+// targets = ['eee', 'ffff']
+// console.log(
+//   'Replaced word: ' + findReplaceString(s, indices, sources, targets) + '\n'
+// )
+
+let s =
+  'ejvzndtzncrelhedwlwiqgdbdgctgubzczgtovufncicjlwsmfdcrqeaghuevyexqdhffikvecuazrelofjmyjjznnjdkimbklrh'
+let indices = [
+  25, 35, 60, 77, 69, 79, 15, 19, 58, 92, 27, 64, 4, 11, 51, 7, 72, 67, 30, 0,
+  74, 98, 17, 85, 48, 32, 38, 62, 43, 2, 9, 55, 87,
+]
+
+let sources = [
+  'gc',
+  'tov',
+  'vy',
+  're',
+  'ikv',
+  'lo',
+  'dw',
+  'iqgdbd',
+  'ue',
+  'kimbk',
+  'tgu',
+  'qd',
+  'ndt',
+  'elhe',
+  'crq',
+  'zn',
+  'ec',
+  'ff',
+  'bz',
+  'ej',
+  'ua',
+  'rh',
+  'lw',
+  'jj',
+  'mfd',
+  'cz',
+  'ufn',
+  'ex',
+  'cjl',
+  'vz',
+  'cr',
+  'agh',
+  'znnj',
+]
+
+let targets = [
+  'dxs',
+  'hn',
+  'vfc',
+  'wnr',
+  'tira',
+  'rko',
+  'oob',
+  'mlitiwj',
+  'zrj',
+  'onpp',
+  'ot',
+  'c',
+  'lm',
+  'hbsje',
+  'dgc',
+  'ruf',
+  'qi',
+  'h',
+  'vzn',
+  'ja',
+  'ow',
+  'te',
+  'km',
+  'imq',
+  'pia',
+  'ixp',
+  'xsod',
+  'm',
+  'eat',
+  'yf',
+  'lzu',
+  'dgy',
+  'dyrsc',
+]
 console.log(
   'Replaced word: ' + findReplaceString(s, indices, sources, targets) + '\n'
 )
