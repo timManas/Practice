816c6c3030477dedb1fddaeb57d8d3b339e529b4
Tim M <timothy.romero.manas@gmail.com>
2022-11-26 13:54:56 -0500

    Adding Max Distance From person

--- JavaScript/backend/src/Misc/MaxDistanceFromPerson/MaxDistanceFromPerson.js
+++ JavaScript/backend/src/Misc/MaxDistanceFromPerson/MaxDistanceFromPerson.js
@@ -1,1 +1,13 @@
-var maxDistToClosest = function (seats) {}
+var maxDistToClosest = function (seats) {
+  console.log('input: ' + input[i])
+}
+
+let input = [
+  [1, 0, 0, 0, 1, 0, 1],
+  [1, 0, 0, 0],
+  [0, 1],
+]
+
+for (let i = 0; i < input.length; i++) {
+  console.log('output: ' + maxDistToClosest(input[i]) + '\n')
+}
