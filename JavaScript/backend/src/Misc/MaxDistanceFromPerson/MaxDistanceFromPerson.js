e8c43bfcc647feffac0dc2e9a05d6074b1b44251
Tim M <timothy.romero.manas@gmail.com>
2022-11-27 23:12:57 -0500

    Finished maximize distance

--- JavaScript/backend/src/Misc/MaxDistanceFromPerson/MaxDistanceFromPerson.js
+++ JavaScript/backend/src/Misc/MaxDistanceFromPerson/MaxDistanceFromPerson.js
@@ -5,14 +5,39 @@ var maxDistToClosest = function (seats) {
   let onesArr = fetchOnesArr(seats)
   console.log('onesArr: ' + onesArr)
 
-  // Check the first position
-
-  // Traverse onesArr
-  for (let i = 0; i < onesArr.length; i++) {
-    // Find the median
+  // Step1 - Check the first position
+  if (!onesArr.includes(0)) {
+    let current = 0
+    let next = onesArr[0]
+    let median = next - current
+    max = max < median ? median : max
+    console.log(
+      '   current: ' + current + '   next: ' + next + '    median: ' + median
+    )
   }
 
-  // Check the last position
+  // Step2 - Traverse onesArr
+  for (let i = 0; i < onesArr.length - 1; i++) {
+    let current = onesArr[i]
+    let next = onesArr[i + 1]
+    //  Step2a - Find the median
+    let median = Math.floor((next - current) / 2)
+    max = max < median ? median : max
+    console.log(
+      '   current: ' + current + '   next: ' + next + '    median: ' + median
+    )
+  }
+
+  // Step3 - Check the last position
+  if (!onesArr.includes(seats.length - 1)) {
+    let current = onesArr[onesArr.length - 1]
+    let next = seats.length - 1
+    let median = next - current
+    max = max < median ? median : max
+    console.log(
+      '   current: ' + current + '   next: ' + next + '    median: ' + median
+    )
+  }
 
   return max
 }
@@ -30,6 +55,7 @@ let input = [
   [1, 0, 0, 0, 1, 0, 1],
   [1, 0, 0, 0],
   [0, 1],
+  [1, 0, 0, 1],
 ]
 
 for (let i = 0; i < input.length; i++) {
