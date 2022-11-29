13fdeaf1dbc8c992f07d8338e0879010611191d0
Tim M <timothy.romero.manas@gmail.com>
2022-11-29 18:50:33 -0500

    Finished Valid Paranthesis

--- JavaScript/backend/src/Strings/ValidParentheses/ValidParentheses.js
+++ JavaScript/backend/src/Strings/ValidParentheses/ValidParentheses.js
@@ -1,5 +1,42 @@
 var isValid = function (s) {
   console.log('s: ' + s)
+  let stack = []
+
+  // Step1 - Traverse letter by letter
+  for (let i = 0; i < s.length; i++) {
+    let bracket = s[i]
+    console.log('   char: ' + bracket)
+
+    // Step2 - If Empty stack, we just add
+    if (s.length == 0) {
+      stack.push(bracket)
+      continue
+    }
+
+    // Step3 -  Check what kind of Bracket it is
+    // Remove the last brack if there is a match
+    if (bracket == ')') {
+      if (stack[stack.length - 1] == '(') {
+        console.log('removing: ' + stack.pop())
+        continue
+      }
+    } else if (bracket == '}') {
+      if (stack[stack.length - 1] == '{') {
+        console.log('removing: ' + stack.pop())
+        continue
+      }
+    } else if (bracket == ']') {
+      if (stack[stack.length - 1] == '[') {
+        console.log('removing: ' + stack.pop())
+        continue
+      }
+    }
+
+    // Step4 - Add to stack
+    stack.push(bracket)
+  }
+
+  return stack.length == 0
 }
 
 let input = ['()', '()[]{}', '(]']
