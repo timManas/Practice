a788c2550935a4b56f74a6353d960e1955f9fcbc
Tim M <timothy.romero.manas@gmail.com>
2022-11-30 18:04:01 -0500

    Adding Merge k sorted list

---
+++ JavaScript/backend/src/Lists/MergeKSortedLists/MergeKSortedLists.js
@@ -0,0 +1,14 @@
+var mergeKLists = function (lists) {}
+
+let lists = [
+  [1, 4, 5],
+  [1, 3, 4],
+  [2, 6],
+]
+console.log('Merged List: ' + mergeKLists(lists))
+
+lists = []
+console.log('Merged List: ' + mergeKLists(lists))
+
+lists = [[]]
+console.log('Merged List: ' + mergeKLists(lists))
