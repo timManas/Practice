994e060f5d1f56aaa1bc6a3121257ea377adf619
Tim M <timothy.romero.manas@gmail.com>
2022-12-09 08:00:20 -0500

    Adding new trap rain water

---
+++ JavaScript/backend/src/Misc/TrapRainWater/TrapRainWater.js
@@ -0,0 +1,11 @@
+var trap = function (height) {
+  console.log('height: ' + height)
+}
+
+let input = [
+  [0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1],
+  [4, 2, 0, 3, 2, 5],
+]
+for (let i of input) {
+  console.log('trap: ' + trap(i) + '\n')
+}
