a15413267f637d8960fc072595cc4f6553a8529d
Tim M <timothy.romero.manas@gmail.com>
2023-01-10 21:13:04 -0500

    Adding Sort Matrix Diagonally

--- JavaScript/backend/src/Misc/SortMatrixDiagonally/SortMatrixDiagonally.js
+++ JavaScript/backend/src/Misc/SortMatrixDiagonally/SortMatrixDiagonally.js
@@ -2,7 +2,21 @@
  * @param {number[][]} mat
  * @return {number[][]}
  */
-var diagonalSort = function (mat) {}
+var diagonalSort = function (mat) {
+  // Traverse Horizontally
+  for (let x = 0; x < mat[0].length; x++) {
+    mat = sort(0, x, mat)
+  }
+
+  // Traverse Vertically
+  for (let y = 0; y < mat.length; y++) {
+    mat = sort(y, 0, mat)
+  }
+
+  return mat
+}
+
+function sort(x, y, mat) {}
 
 let mat = [
   [3, 3, 1, 1],
