91566cc5bfaf34f3d2459346fa609cbb515099d9
Tim M <timothy.romero.manas@gmail.com>
2023-01-24 22:47:03 -0500

    Adding All paths from source 2 target

---
+++ JavaScript/backend/src/Graphs/AllPathsFromSource2Target/AllPathsFromSource2Target.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[][]} graph
+ * @return {number[][]}
+ */
+var allPathsSourceTarget = function (graph) {
+  // Use DFS to start from 0 to n
+}
+
+let graph = [[1, 2], [3], [3], []]
+console.log('Paths: ' + allPathsSourceTarget(graph))
+
+let graph1 = [[4, 3, 1], [3, 2, 4], [3], [4], []]
+console.log('Paths: ' + allPathsSourceTarget(graph1))
