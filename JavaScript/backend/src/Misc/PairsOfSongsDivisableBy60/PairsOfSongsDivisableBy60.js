713029d2731391884458e8087ce47e825416f4f9
Tim M <timothy.romero.manas@gmail.com>
2023-03-01 23:15:29 -0500

    Finished Pairs

--- JavaScript/backend/src/Misc/PairsOfSongsDivisableBy60/PairsOfSongsDivisableBy60.js
+++ JavaScript/backend/src/Misc/PairsOfSongsDivisableBy60/PairsOfSongsDivisableBy60.js
@@ -1,10 +1,23 @@
 var numPairsDivisibleBy60 = function (time) {
+  let total = 0
+  for (let i = 0; i < time.length - 1; i++) {
+    for (let j = i + 1; j < time.length; j++) {
+      console.log('time[i]: ' + time[i] + '       time[j]: ' + time[j])
 
-    for(let i=0; i < time)
+      if ((time[i] + time[j]) % 60 == 0) {
+        console.log('Divisiable -- ')
+        ++total
+      }
+    }
+  }
 
+  return total
 }
 
-let input = [[30, 20, 150, 100, 40], [60, 60, 60]]
+let input = [
+  [30, 20, 150, 100, 40],
+  [60, 60, 60],
+]
 for (let i of input) {
-  console.log('Num Divisable: ' + numPairsDivisibleBy60(i))
+  console.log('Num Divisable: ' + numPairsDivisibleBy60(i) + '\n')
 }
