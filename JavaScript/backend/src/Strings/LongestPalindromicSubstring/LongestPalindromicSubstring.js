8fac3f943bd0e40052645cafde5d9627da46c3d2
Tim M <timothy.romero.manas@gmail.com>
2023-03-03 00:06:04 -0500

    Adding Palindrom

--- JavaScript/backend/src/Strings/LongestPalindromicSubstring/LongestPalindromicSubstring.js
+++ JavaScript/backend/src/Strings/LongestPalindromicSubstring/LongestPalindromicSubstring.js
@@ -1,4 +1,15 @@
-var longestPalindrome = function (s) {}
+var longestPalindrome = function (s) {
+  let longest = ''
+
+  for (let a = 0; a < s.length; a++) {
+    for (let b = a + 1; b < s.length; b++) {
+      console.log('current: ' + s[b])
+    }
+    console.log(' ')
+  }
+
+  return longest
+}
 
 let input = ['babad', 'cbbd']
 for (let i of input) {
