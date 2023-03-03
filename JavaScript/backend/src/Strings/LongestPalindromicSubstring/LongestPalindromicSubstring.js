a8590cb6b7a6bc62f7533458af1a28dd6ee03976
Tim M <timothy.romero.manas@gmail.com>
2023-03-03 00:30:37 -0500

    Finished Longest Palindromic substring

--- JavaScript/backend/src/Strings/LongestPalindromicSubstring/LongestPalindromicSubstring.js
+++ JavaScript/backend/src/Strings/LongestPalindromicSubstring/LongestPalindromicSubstring.js
@@ -1,17 +1,38 @@
 var longestPalindrome = function (s) {
   let longest = ''
 
+  let current = ''
+  let reverse = ''
+
   for (let a = 0; a < s.length; a++) {
-    for (let b = a + 1; b < s.length; b++) {
-      console.log('current: ' + s[b])
+    for (let b = a; b < s.length; b++) {
+      current = current.concat(s[b])
+      reverse = s[b].concat(reverse)
+
+      console.log(
+        'letter: ' +
+          s[b] +
+          '    current: ' +
+          current +
+          '    reverse: ' +
+          reverse +
+          '     substring: ' +
+          subString
+      )
+
+      if (current == reverse) {
+        longest = longest.length < current.length ? current : longest
+      }
     }
     console.log(' ')
+    current = ''
+    reverse = ''
   }
 
-  return longest
+  return longest == '' ? s : longest
 }
 
-let input = ['babad', 'cbbd']
+let input = ['babad', 'cbbd', 'bb']
 for (let i of input) {
   console.log('Longest: ' + longestPalindrome(i) + '\n')
 }
