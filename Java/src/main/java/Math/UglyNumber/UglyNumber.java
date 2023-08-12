02fb12359362cfcbe66fcbb0db7a61c856b74cc1
Tim M <timothy.romero.manas@gmail.com>
2023-08-11 21:39:58 -0400

    Finished Ugly number

--- Java/src/main/java/Math/UglyNumber/UglyNumber.java
+++ Java/src/main/java/Math/UglyNumber/UglyNumber.java
@@ -3,24 +3,37 @@ package Math.UglyNumber;
 public class UglyNumber {
 
     public static void main(String [] args) {
-        int [] input = {6,1,14};
+        int [] input = {6,1,14,8};
         for (int i : input)
             System.out.println("isUgly: " + i + "    " + isUgly(i) + "\n");
     }
 
     public static boolean isUgly(int n) {
-        if (n == 0 || n == 1)
-            return true;
+        System.out.println("n: " + n);
 
-        int div = 0;
-        if (n % 2 == 0) {
-            while (div > 1) {
-                div = n / 2;
-                System.out.println("div: " + div);
+        // Step1 - Check edge cases
+        if (n == 1)
+            return true;
+        if (n <= 0)
+            return false;
+
+        // Step2 - Check prime number from 2-5 and see if they are a factorial
+        // Anything past 5, we return false
+        int prime = 2;
+        while (prime <= 5) {
+            System.out.println("n: " + n + "  prime: " + prime);
+            if (n % prime == 0) {
+                n = n / prime;
+                continue;
             }
+
+            if (n==1)
+                break;
+
+            prime++;
         }
-            return true;
 
 
+        return n==1;
     }
 }
