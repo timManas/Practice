a7ac52d7ddc227f81447d8f1bfb9b76e49ec447f
Tim M <timothy.romero.manas@gmail.com>
2023-08-15 22:04:31 -0400

    Adding MinCost Climb Stairs

--- Java/src/main/java/Misc/MinCostClimbStairs/MinCostClimbStairs.java
+++ Java/src/main/java/Misc/MinCostClimbStairs/MinCostClimbStairs.java
@@ -3,20 +3,53 @@ package Misc.MinCostClimbStairs;
 public class MinCostClimbStairs {
 
     public static void main(String [] args) {
-//        int [][] input = {{10,15,20}, {1,100,1,1,1,100,1,1,100,1}};
-        int [][] input = {{0,2,2,1}};
+        int [][] input = {{10,15,20}, {1,100,1,1,1,100,1,1,100,1}};
+//        int [][] input = {{0,2,2,1}};
+//        int [][] input = {{2,2,1,0}};
 
         for (int [] i : input)
             System.out.println("Min Cost: " + minCostClimbingStairs(i) + "\n");
     }
 
     public static int minCostClimbingStairs(int[] cost) {
-        int minCost = 0;
+        int minCostDown = Math.min(costClimbStairsDown(cost, cost.length - 1), costClimbStairsDown(cost, cost.length - 2));
+        int minCostUp = Math.min(costClimbStairsUp(cost, 0), costClimbStairsUp(cost, 1));
+        return Math.min(minCostUp, minCostDown);
+    }
 
-        int i = cost.length;
-        int current = -1;
-        int n1 = cost[i-1];
-        int n2 = cost[i-2];
+    private static int costClimbStairsUp(int[] cost, int i) {
+        int minCost = cost[i];
+        int current;
+        int n1;
+        int n2;
+
+        while (i < cost.length - 2) {
+            current = cost[i];
+            n1 = cost[i + 1];
+            n2 = cost[i + 2];
+            System.out.println("Current: " + current + "    n1:" + n1 + "      n2: " + n2);
+
+            if (n1 < n2) {
+                i = i + 1;
+            } else {
+                i = i + 2;
+            }
+            minCost += cost[i];
+            System.out.println("minCost: " + minCost);
+
+        }
+        System.out.println("Current: " + cost[i] +"\n");
+
+        return minCost;
+
+    }
+
+
+    public static int costClimbStairsDown(int [] cost, int i) {
+        int minCost = cost[i];
+        int current;
+        int n1;
+        int n2;
 
         while (i >= 2) {
             current = i < cost.length ? cost[i] : 0;
@@ -30,12 +63,11 @@ public class MinCostClimbStairs {
                 i = i-2;
             }
             minCost += cost[i];
-
+            System.out.println("minCost: " + minCost);
         }
-
-        System.out.println("Current: " + cost[i]);
-
+        System.out.println("Current: " + cost[i] +"\n");
 
         return minCost;
     }
+
 }
