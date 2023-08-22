3fc71959f20f743857d096e93919ead8528d5d1f
Tim M <timothy.romero.manas@gmail.com>
2023-08-21 22:23:07 -0400

    Adding new testcases

--- Java/src/main/java/Misc/TrappingRainWater/TrappingRainWater.java
+++ Java/src/main/java/Misc/TrappingRainWater/TrappingRainWater.java
@@ -3,7 +3,13 @@ package Misc.TrappingRainWater;
 public class TrappingRainWater {
     public static void main(String [] args) {
         int [] height = {0,1,0,2,1,0,1,3,2,1,2,1};
-        System.out.println("trap: " + trap(height));
+        System.out.println("trap: " + trap(height) + "\n");
+
+        int [] height1 = {4,2,0,3,2,5};
+        System.out.println("trap: " + trap(height1) + "\n");
+
+        int [] height2 = {4,2,3};
+        System.out.println("trap: " + trap(height2) + "\n");
     }
 
     public static int trap(int[] height) {
