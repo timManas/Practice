d152db4212933e52467f1e82adbffbf667bc63ca
Tim M <timothy.romero.manas@gmail.com>
2023-08-22 20:51:08 -0400

    Finished GetMaxGenerated

--- Java/src/main/java/Math/GetMaxGenerated/GetMaxGenerated.java
+++ Java/src/main/java/Math/GetMaxGenerated/GetMaxGenerated.java
@@ -5,25 +5,39 @@ import java.util.Collections;
 
 public class GetMaxGenerated {
     public static void main(String [] args) {
-        int [] input = {7,2,3};
+        int [] input = {7,2,3,0};
         for (int i : input)
             System.out.println("max: " + getMaximumGenerated(i) + "\n");
     }
 
     public static int getMaximumGenerated(int n) {
-        int max = 0;
 
+        // Step1 - Check if n is small
+        if (n <= 1) return n;
+
+
+        int max = 1;
         int [] nums = new int[n + 1];
         nums[0] = 0;
         nums[1] = 1;
 
-        int i = 2;
 
+        // Step2 - Travers from i till whenever this finishes
+        int i = 1;
         while (i*2 <= n) {
             System.out.println("i: " + i);
 
-            nums[2 * i] = nums[i];
-            nums[2 * i + 1] = nums[i] + nums[i+1];
+            // Add to array only if within boundaries
+            if (2*i <= n) {
+                nums[2 * i] = nums[i];
+                max = max < nums[2*i] ? nums[2*i] : max;
+            }
+
+            // Add to array only if within boundaries
+            if (2*i + 1 <= n) {
+                nums[2 * i + 1] = nums[i] + nums[i+1];
+                max = max < nums[2*i + 1] ? nums[2*i + 1] : max;
+            }
 
             i++;
         }
