a4c3389922308e32d14292f9baf62a1e30dae0b6
Tim M <timothy.romero.manas@gmail.com>
2023-08-23 21:48:38 -0400

    Adding Count sorted vowels

---
+++ Java/src/main/java/Math/CountSortedVowels/CountSortedVowels.java
@@ -0,0 +1,18 @@
+package Math.CountSortedVowels;
+
+public class CountSortedVowels {
+
+    public static void main(String [] args) {
+        int [] input = {1,2,33};
+        for (int i : input)
+            System.out.println("count vol String: " + countVowelStrings(i) + "\n");
+    }
+
+    public static int countVowelStrings(int n) {
+        int count = 0;
+
+
+
+        return count;
+    }
+}
