d3df666dd5c61fcec722707a2cb8d6869e399220
Tim M <timothy.romero.manas@gmail.com>
2023-08-25 23:05:38 -0400

    Finished Count sorted vowels

--- Java/src/main/java/Math/CountSortedVowels/CountSortedVowels.java
+++ Java/src/main/java/Math/CountSortedVowels/CountSortedVowels.java
@@ -1,9 +1,6 @@
 package Math.CountSortedVowels;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.TreeMap;
+import java.util.*;
 
 public class CountSortedVowels {
 
@@ -14,37 +11,53 @@ public class CountSortedVowels {
     }
 
     public static int countVowelStrings(int n) {
-        int count = 0;
 
-        if (n <= 1) return 5;
-
-
-        int [] d = new int[n];
+        // Step1 - Create Dynamic array to store prev
+        int [] d = new int[n+1];
         d[0] = 0;
         d[1] = 5;
 
-        Map<String, Integer> map = new TreeMap<>();
-        map.put("a", 1);
-        map.put("e", 1);
-        map.put("i", 1);
-        map.put("o", 1);
-        map.put("u", 1);
+        if (n == 1) return 5;
 
+
+        // Step2 = Create queue to pull elements from
+        Deque<Integer> queue = new ArrayDeque<>();
+        queue.addFirst(1);
+        queue.addFirst(1);
+        queue.addFirst(1);
+        queue.addFirst(1);
+        queue.addFirst(1);
+
+        // Step3 - Create tempQueue to store values
+        Deque<Integer> tempQueue = new ArrayDeque<>();
+
+        // Step4 - Traverse from 2 to n
         for (int i=2; i<=n; i++) {
             System.out.println("i: " + i);
 
-            int total = 0;
-            for (Map.Entry<String, Integer> entry : map.entrySet()) {
+            // Get the prev value
+            int prev = d[i-1];          // we set the value to the last dynamic array value of i
+            tempQueue.addFirst(prev);
 
+            // Step5 - Keep pulling from queue until only 1 left. We dont care about the 1
+            int total = prev;
+            while (queue.size() > 1) {
+                int temp = prev - queue.removeFirst();
+                prev = temp;
+                tempQueue.addLast(temp);
+                total += prev;
             }
 
 
-            System.out.println("    arrays: " + Arrays.toString(d));
+            // Step6 - Reset the queues by making the tempQueue the mainQueue and clear
+            queue = new ArrayDeque<>(tempQueue);
+            tempQueue.clear();
 
+            // Step7 - Keep track of the total to store in the dp for the next iteration
+            d[i] = total;
         }
 
 
-
-        return count;
+        return d[n];
     }
 }
