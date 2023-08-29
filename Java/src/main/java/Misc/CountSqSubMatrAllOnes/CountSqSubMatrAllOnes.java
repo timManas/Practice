82fbd179e8a2b4d9630d06f99f8d59a53d5d58fb
Tim M <timothy.romero.manas@gmail.com>
2023-08-29 19:50:29 -0400

    Adding Count Sq sub matrix

--- Java/src/main/java/Misc/CountSqSubMatrAllOnes/CountSqSubMatrAllOnes.java
+++ Java/src/main/java/Misc/CountSqSubMatrAllOnes/CountSqSubMatrAllOnes.java
@@ -1,16 +1,17 @@
 package Misc.CountSqSubMatrAllOnes;
 
 import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class CountSqSubMatrAllOnes {
 
     public static void main (String [] args) {
-        int [][] matrix = { {0,1,1,1},
-                            {1,1,1,1},
-                            {0,1,1,1}};
-        System.out.println("countSquares: " + countSquares(matrix)+ "\n");
+//        int [][] matrix = { {0,1,1,1},
+//                            {1,1,1,1},
+//                            {0,1,1,1}};
+//        System.out.println("countSquares: " + countSquares(matrix)+ "\n");
 
 
         int [][] matrix1 = {{1,0,1},
@@ -41,11 +42,44 @@ public class CountSqSubMatrAllOnes {
         System.out.println("Matrix: " + Arrays.deepToString(matrix));
         System.out.println("largest: " + largest);
 
-        // Step1 - Identify the biggest matrix
+        // Step1 - Identify the consecutive one
+        List<Integer> sizeList = findOnes(matrix);
+
+
+        // Step2 - Traverse the matrix
 
 
         // Step2 - Calculate the sub blocks
 
         return count;
+    }
+
+    private static List<Integer> findOnes(int[][] matrix) {
+        List<Integer> list = new ArrayList<>();
+
+        int numConOnes = 0;
+        for (int i=0; i<matrix.length; i++) {
+
+            numConOnes = 0;
+            for (int j=0; j<matrix[0].length - 1;j++) {
+                int current = matrix[i][j];
+                int next = matrix[i][j+1];
+
+                if (current == 0) {
+                    numConOnes = 0;
+                } else if (next == 1) {
+                    if (numConOnes == 0) numConOnes++;
+                    numConOnes++;
+                }
+
+                if (j == matrix[0].length - 2) {
+                    list.add(numConOnes);
+                }
+
+            }
+        }
+
+        System.out.println("list: " + list);
+        return list;
     }
 }
