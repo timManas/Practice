4ed8fabb1a29cbbef4eff8e05742565e11193c1e
Tim M <timothy.romero.manas@gmail.com>
2023-08-30 22:08:48 -0400

    Adding Count Sq Sub Matr

--- Java/src/main/java/Misc/CountSqSubMatrAllOnes/CountSqSubMatrAllOnes.java
+++ Java/src/main/java/Misc/CountSqSubMatrAllOnes/CountSqSubMatrAllOnes.java
@@ -1,9 +1,7 @@
 package Misc.CountSqSubMatrAllOnes;
 
 import java.lang.reflect.Array;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class CountSqSubMatrAllOnes {
 
@@ -12,7 +10,6 @@ public class CountSqSubMatrAllOnes {
 //                            {1,1,1,1},
 //                            {0,1,1,1}};
 //        System.out.println("countSquares: " + countSquares(matrix)+ "\n");
-
 
         int [][] matrix1 = {{1,0,1},
                             {1,1,0},
@@ -26,11 +23,11 @@ public class CountSqSubMatrAllOnes {
                             {0,1,1,1}};
         System.out.println("countSquares: " + countSquares(matrix2)+ "\n");
 
-        int [][] matrix3 = {{0,1,0,1,1,0,1,1},
-                            {1,0,1,1,1,1,1,1},
-                            {1,1,1,1,1,1,1,1},
-                            {1,0,0,1,1,1,1,1},
-                            {0,1,1,1,1,1,1,1}};
+        int [][] matrix3 = {{1,1,1,0,1,0,1,0},      //[3,1,1]
+                            {1,0,1,1,1,1,1,1},      //[1,6]
+                            {1,1,1,1,1,1,1,1},      //[8]
+                            {1,1,1,1,1,0,1,1},      //[5,2]
+                            {0,1,1,1,1,1,1,1}};     //[7]
         System.out.println("countSquares: " + countSquares(matrix3)+ "\n");
 
 
@@ -38,48 +35,19 @@ public class CountSqSubMatrAllOnes {
 
     public static int countSquares(int[][] matrix) {
         int count = 0;
-        int largest = Math.max(matrix.length, matrix[0].length);
-        System.out.println("Matrix: " + Arrays.deepToString(matrix));
-        System.out.println("largest: " + largest);
 
-        // Step1 - Identify the consecutive one
-        List<Integer> sizeList = findOnes(matrix);
+        Map<Integer,Integer> map = new TreeMap<>();
 
+        for (int row=0; row<matrix.length; row++) {
+            for (int col=0; col<matrix[0].length;col++) {
+                System.out.print(matrix[row][col] + ",");
+            }
+            System.out.println();
+        }
 
-        // Step2 - Traverse the matrix
-
-
-        // Step2 - Calculate the sub blocks
 
         return count;
     }
 
-    private static List<Integer> findOnes(int[][] matrix) {
-        List<Integer> list = new ArrayList<>();
 
-        int numConOnes = 0;
-        for (int i=0; i<matrix.length; i++) {
-
-            numConOnes = 0;
-            for (int j=0; j<matrix[0].length - 1;j++) {
-                int current = matrix[i][j];
-                int next = matrix[i][j+1];
-
-                if (current == 0) {
-                    numConOnes = 0;
-                } else if (next == 1) {
-                    if (numConOnes == 0) numConOnes++;
-                    numConOnes++;
-                }
-
-                if (j == matrix[0].length - 2) {
-                    list.add(numConOnes);
-                }
-
-            }
-        }
-
-        System.out.println("list: " + list);
-        return list;
-    }
 }
