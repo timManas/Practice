358e4cf4aad72071a198a46fb65d3cfa98e4cdc3
Tim M <timothy.romero.manas@gmail.com>
2023-09-02 19:45:29 -0400

    Adding AllPossible binary trees

---
+++ Java/src/main/java/Trees/AllPossibleBinaryTrees/AllPossibleBinaryTrees.java
@@ -0,0 +1,13 @@
+package Trees.AllPossibleBinaryTrees;
+
+import java.util.List;
+
+public class AllPossibleBinaryTrees {
+    public static void main(String [] args) {
+
+    }
+
+    public static List<TreeNode> allPossibleFBT(int n) {
+
+    }
+}
