a9cd1bb46bcd07bd76e82666acfc83855cb339bb
Tim M <timothy.romero.manas@gmail.com>
2023-09-04 23:26:25 -0400

    Algorithm design

--- Java/src/main/java/Trees/AllPossibleBinaryTrees/AllPossibleBinaryTrees.java
+++ Java/src/main/java/Trees/AllPossibleBinaryTrees/AllPossibleBinaryTrees.java
@@ -1,13 +1,29 @@
 package Trees.AllPossibleBinaryTrees;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class AllPossibleBinaryTrees {
     public static void main(String [] args) {
-
+        int [] input = {7,3};
+        for (int i : input)
+            System.out.println("All Possible Binary Trees:" + allPossibleFBT(i));
     }
 
     public static List<TreeNode> allPossibleFBT(int n) {
+        List<TreeNode> list = new ArrayList<>();
 
+
+        // Traverse Node using
+
+        return list;
     }
 }
+
+
+/**
+ Perform DFS
+ - Pre Order Traversal
+
+
+ */
