8da6c4581f032e9440f1f228d568205237339237
Tim M <timothy.romero.manas@gmail.com>
2023-09-09 16:25:32 -0400

    Adding ReArrange Str between words

--- Java/src/main/java/Strings/ReArrangeStrBetweenWords/ReArrangeStrBetweenWords.java
+++ Java/src/main/java/Strings/ReArrangeStrBetweenWords/ReArrangeStrBetweenWords.java
@@ -1,6 +1,8 @@
 package Strings.ReArrangeStrBetweenWords;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class ReArrangeStrBetweenWords {
     public static void main(String [] args) {
@@ -11,8 +13,30 @@ public class ReArrangeStrBetweenWords {
 
     public static String reorderSpaces(String text) {
         StringBuilder sb = new StringBuilder();
-        String [] words = text.split("[^a-zA-Z]+");
-        System.out.println("words: " + Arrays.toString(words));
+        String [] wordsArr = text.split("[^a-zA-Z]+");
+        List<String> wordList = new ArrayList<>();
+        for (String word : wordsArr) if (!word.equalsIgnoreCase("")) wordList.add(word);
+        System.out.println("words: " + wordList);
+
+        int numSpaces = 0;
+        for (char i : text.toCharArray()) if (i == ' ') numSpaces++;
+
+
+        int spaceArea = wordList.size() - 1;
+
+        int minNumSpaces = 0;
+        int remainder = 0;
+        if (numSpaces % spaceArea == 0) {
+            minNumSpaces = numSpaces / spaceArea;
+
+        } else {
+
+
+
+        }
+
+
+
 
         return sb.toString();
     }
