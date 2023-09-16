96a2e46670f780b239b7cec82b655f6c9bba215d
Tim M <timothy.romero.manas@gmail.com>
2023-09-15 20:25:02 -0400

    Adding Rearranage chars to make target string

--- Java/src/main/java/Strings/RearrangeCharTargetStr/RearrangeCharTargetStr.java
+++ Java/src/main/java/Strings/RearrangeCharTargetStr/RearrangeCharTargetStr.java
@@ -12,11 +12,10 @@ public class RearrangeCharTargetStr {
     }
 
     public static int rearrangeCharacters(String s, String target) {
-        int count = 0;
-
         Map<Character, Integer> stringMap = new TreeMap<>();
         Map<Character, Integer> targetMap = new TreeMap<>();
 
+        // Populate target map
         for (int i=0; i<target.length();i++) {
             char letter = target.charAt(i);
 
@@ -27,6 +26,7 @@ public class RearrangeCharTargetStr {
         }
         System.out.println("TargetMap: " + targetMap);
 
+        // Populate string map
         for (int i=0; i<s.length();i++) {
             char letter = s.charAt(i);
 
@@ -34,17 +34,28 @@ public class RearrangeCharTargetStr {
                 continue;
 
             if (stringMap.containsKey(letter))
-                stringMap.put(letter, stringMap.get(letter));
+                stringMap.put(letter, stringMap.get(letter) + 1);
             else
                 stringMap.put(letter, 1);
         }
         System.out.println("String map: " + stringMap);
 
+        // Make sure we have same size for both
+        if (stringMap.size() != targetMap.size()) return 0;
+
+
+        // Find the smallest divisor
+        int smallestDivisor = Integer.MAX_VALUE;
+        for (Map.Entry<Character, Integer> mapEntry : targetMap.entrySet()) {
+            char targetKey = mapEntry.getKey();
+            int targetValue = mapEntry.getValue();
+
+            int divisor = stringMap.get(targetKey) / targetValue;
+            smallestDivisor = smallestDivisor > divisor ? divisor : smallestDivisor;
+        }
 
 
 
-
-
-        return count;
+        return smallestDivisor;
     }
 }
