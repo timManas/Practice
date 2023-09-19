e05f79b215d584e64e7a2c5c8683ee659d498b06
Tim M <timothy.romero.manas@gmail.com>
2023-09-19 00:15:31 -0400

    Finished MinRightShift to Sort

--- Java/src/main/java/Lists/MinRightShiftToSoryArray/MinRightShiftToSoryArray.java
+++ Java/src/main/java/Lists/MinRightShiftToSoryArray/MinRightShiftToSoryArray.java
@@ -9,14 +9,44 @@ public class MinRightShiftToSoryArray {
         for (int [] arr : input) {
             List<Integer> list = new ArrayList<>();
             for (int i : arr) list.add(i);
-            System.out.println("minShift: " + minimumRightShifts(list));
+            System.out.println("minShift: " + minimumRightShifts(list) + "\n");
         }
     }
 
     public static int minimumRightShifts(List<Integer> nums) {
+        if (nums.size() <= 1) return 0; // Default case
+
         int count = 0;
+        boolean hasShift = false;
 
+        // Step1 - Travers list from left to right
+        for (int i=0; i<nums.size()-1; i++) {
+            int current = nums.get(i);
+            int next = nums.get(i+1);
+            System.out.println("current: " + current + "    next: " + next);
 
+            // Step2- Find out if ascending
+            if (current < next) {
+
+                if (hasShift)
+                    count++;
+
+            } else {
+                hasShift = true;
+
+                // Step3 - Check if count is big. Otherwise we return 0 since we already shifted
+                if (count != 0)
+                    return -1;
+            }
+        }
+
+        // Check if all elements are ascending
+        if (!hasShift) return 0;
+
+        // Check last two elements
+        if (nums.get(0) < nums.get(nums.size()-1))
+            return -1;
+        count++;
 
         return count;
     }
