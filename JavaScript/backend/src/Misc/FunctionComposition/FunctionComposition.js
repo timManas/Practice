1f53eca32d56451f8105e8e6ba48ff7826abfca8
Tim M <timothy.romero.manas@gmail.com>
2023-09-24 19:41:46 -0400

    Finished Function composition

--- JavaScript/backend/src/Misc/FunctionComposition/FunctionComposition.js
+++ JavaScript/backend/src/Misc/FunctionComposition/FunctionComposition.js
@@ -3,25 +3,33 @@
  * @return {Function}
  */
 var compose = function (functions) {
-  for (let arg = 0; arg < functions.length; arg++) {
-    let func = functions[arg]
-    console.log(func)
-
-    let xx = func()
-    console.log(xx)
+  if (functions.length == 0) {
+    return function (x) {
+      return x
+    }
   }
 
-  return function (x) {}
+  return function (x) {
+    var current = x
+    for (var i = functions.length - 1; i >= 0; i--) {
+      current = functions[i](current)
+    }
+
+    return current
+  }
 }
 
 const fn = compose([(x) => x + 1, (x) => 2 * x])
-fn(4) // 9
+console.log('result: ' + fn(4)) // 9
 
-// fn = compose([(x) => x + 1, (x) => x * x, (x) => 2 * x])
-// fn(4)
+/**
+ Function Composition
+ Two things to note
+ 1. function(funcitons) ...actually refers to this line compose([(x) => x + 1, (x) => 2 * x])
+ 2. while function(x) ....actually refers to this line fn(4)
 
-// fn = compose([(x) => 10 * x, (x) => 10 * x, (x) => 10 * x])
-// fn(1)
+ Why is that ? 
+ 
 
-// fn = compose([])
-// fn(42)
+
+*/
