8fe1d7a23f3ad6f977bbf29d604d42fd08f19ac5
Tim M <timothy.romero.manas@gmail.com>
2023-09-29 22:26:13 -0400

    Adding Fib Sequence

---
+++ JavaScript/backend/src/Misc/FibonacciSequence/FibonacciSequence.js
@@ -0,0 +1,27 @@
+/**
+ * @return {Generator<number>}
+ */
+var fibGenerator = function* () {
+  var count = 0
+  var prev1 = 0
+  var prev2 = 0
+
+  current = 0
+  prev2 = 0
+  yield 0
+
+  current = 1
+  prev1 = 1
+  yield 1
+
+  while (true) {
+    let current = prev1 + prev2
+    prev2 = prev1
+    prev1 = current
+    yield current
+  }
+}
+
+const gen = fibGenerator()
+console.log(gen.next().value) // 0
+console.log(gen.next().value) // 1
