0973c857d5729adc22c1d6cdf6d089df7982872d
Tim M <timothy.romero.manas@gmail.com>
2023-09-29 22:28:11 -0400

    Finished Fib sequence

--- JavaScript/backend/src/Misc/FibonacciSequence/FibonacciSequence.js
+++ JavaScript/backend/src/Misc/FibonacciSequence/FibonacciSequence.js
@@ -25,3 +25,7 @@ var fibGenerator = function* () {
 const gen = fibGenerator()
 console.log(gen.next().value) // 0
 console.log(gen.next().value) // 1
+console.log(gen.next().value) // 1
+console.log(gen.next().value) // 2
+console.log(gen.next().value) // 3
+console.log(gen.next().value) // 5
