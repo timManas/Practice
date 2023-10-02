4c08d9703ede6dc866da2051386f194362738e5e
Tim M <timothy.romero.manas@gmail.com>
2023-10-02 16:52:33 -0400

    Finished Array Wrapper

--- JavaScript/backend/src/Misc/ArrayWrapper/ArrayWrapper.js
+++ JavaScript/backend/src/Misc/ArrayWrapper/ArrayWrapper.js
@@ -1,5 +1,6 @@
 class ArrayWrapper {
   constructor(arg) {
+    console.log('constructor called')
     this.arr = arg
     this.calculateTotal(this.arr)
   }
@@ -15,19 +16,30 @@ class ArrayWrapper {
 /**
  * @return {string}
  */
-
-ArrayWrapper.prototype.toString = function () {
-  console.log('arr: ' + this.arr)
-  return this.arr
-}
-
-ArrayWrapper.prototype.add = function () {
+ArrayWrapper.prototype.valueOf = function () {
   console.log('total: ' + this.total)
   return this.total
 }
 
+ArrayWrapper.prototype.toString = function () {
+  // Why doesent this work ?
+  //   return this.arr  -> I think its because this.arr still calls upon valueOf property ...
+
+  // But '[' + this.arr.join(',') + ']' returns a string instead and does not invoke valueOf
+
+  return '[' + this.arr.join(',') + ']'
+}
+
 const obj1 = new ArrayWrapper([1, 2])
 const obj2 = new ArrayWrapper([3, 4])
-console.log(obj1 + obj2) // 10
+
+console.log('obj1: ' + obj1)
+console.log('obj2: ' + obj2 + '\n')
+
+// console.log(obj1 + obj2) // 10
 console.log(String(obj1)) // "[1,2]"
 console.log(String(obj2)) // "[3,4]"
+
+console.log('\n\n')
+const obj3 = new ArrayWrapper([23, 98, 42, 70])
+console.log(String(obj3)) // "[1,2]"
