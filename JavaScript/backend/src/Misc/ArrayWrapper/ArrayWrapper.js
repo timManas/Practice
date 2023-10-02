41502cd5e5f9b8209efe5aef8d32c341a13dd724
Tim M <timothy.romero.manas@gmail.com>
2023-10-02 10:47:58 -0400

    Adding ArrayWrapper

--- JavaScript/backend/src/Misc/ArrayWrapper/ArrayWrapper.js
+++ JavaScript/backend/src/Misc/ArrayWrapper/ArrayWrapper.js
@@ -1,9 +1,29 @@
+class ArrayWrapper {
+  constructor(arg) {
+    this.arr = arg
+    this.calculateTotal(this.arr)
+  }
+
+  calculateTotal(arr) {
+    this.total = 0
+    for (let i = 0; i < arr.length; i++) {
+      this.total += arr[i]
+    }
+  }
+}
+
 /**
  * @return {string}
  */
 
 ArrayWrapper.prototype.toString = function () {
-  return output
+  console.log('arr: ' + this.arr)
+  return this.arr
+}
+
+ArrayWrapper.prototype.add = function () {
+  console.log('total: ' + this.total)
+  return this.total
 }
 
 const obj1 = new ArrayWrapper([1, 2])
