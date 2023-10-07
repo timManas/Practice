9e43d07e8929f31f8d7996a576b1e38b12980997
Tim M <timothy.romero.manas@gmail.com>
2023-10-06 21:57:25 -0400

    Adding Allow One Fun Call

---
+++ JavaScript/backend/src/Misc/AllowOneFuncCall/AllowOneFuncCall.js
@@ -0,0 +1,17 @@
+/**
+ * @param {Function} fn
+ * @return {Function}
+ */
+var once = function (fn) {
+  return function (...args) {
+    const sum = args.reduce((accumulator, currentValue) => {
+      return accumulator + currentValue
+    }, 0)
+  }
+}
+
+let fn = (a, b, c) => a + b + c
+let onceFn = once(fn)
+
+console.log(onceFn(1, 2, 3)) // 6
+console.log(onceFn(2, 3, 6)) // returns undefined without calling fn
