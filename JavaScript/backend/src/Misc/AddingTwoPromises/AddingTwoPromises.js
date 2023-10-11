2050b26288acbc95487283d99ed80a796ea82d51
Tim M <timothy.romero.manas@gmail.com>
2023-10-10 23:13:01 -0400

    Finished Adding Two promises

--- JavaScript/backend/src/Misc/AddingTwoPromises/AddingTwoPromises.js
+++ JavaScript/backend/src/Misc/AddingTwoPromises/AddingTwoPromises.js
@@ -3,6 +3,13 @@
  * @param {Promise} promise2
  * @return {Promise}
  */
-var addTwoPromises = async function (promise1, promise2) {}
+var addTwoPromises = async function (promise1, promise2) {
+  let promise1Result = await promise1
+  let promise2Result = await promise2
+
+  return new Promise((resolve, reject) => {
+    resolve(promise1Result + promise2Result)
+  })
+}
 
 addTwoPromises(Promise.resolve(2), Promise.resolve(2)).then(console.log) // 4
