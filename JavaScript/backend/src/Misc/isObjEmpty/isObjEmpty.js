35937c67783a8f65afd7ff58653c99d6a1d8ceb7
Tim M <timothy.romero.manas@gmail.com>
2023-10-15 13:34:38 -0400

    Finished IsObjEmpty

--- JavaScript/backend/src/Misc/isObjEmpty/isObjEmpty.js
+++ JavaScript/backend/src/Misc/isObjEmpty/isObjEmpty.js
@@ -1,14 +1,20 @@
-/**
- * @param {Object|Array} obj
- * @return {boolean}
- */
-var isEmpty = function (obj) {}
+var isEmpty = function (obj) {
+  if (Array.isArray(obj)) {
+    console.log('Array detected')
+    return obj.length == 0
+  }
+  console.log('Obj detected')
+  return Object.keys(obj).length == 0
+}
 
-let obj = { x: 5, y: 42 }
-console.log('isEmpty: ' + isEmpty(obj) + '\n')
+// let obj = { x: 5, y: 42 }
+// console.log('isEmpty: ' + isEmpty(obj) + '\n')
 
-obj = {}
-console.log('isEmpty: ' + isEmpty(obj) + '\n')
+// obj = {}
+// console.log('isEmpty: ' + isEmpty(obj) + '\n')
 
-obj = [null, false, 0]
+// obj = [null, false, 0]
+// console.log('isEmpty: ' + isEmpty(obj) + '\n')
+
+obj = []
 console.log('isEmpty: ' + isEmpty(obj) + '\n')
