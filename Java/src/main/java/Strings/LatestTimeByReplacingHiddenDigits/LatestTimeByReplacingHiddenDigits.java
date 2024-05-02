ea61acf9d8acc766c55cc73de7887765f349aad8
Tim M <timothy.romero.manas@gmail.com>
2024-05-01 21:07:49 -0400

    Adding Latest time

--- Java/src/main/java/Strings/LatestTimeByReplacingHiddenDigits/LatestTimeByReplacingHiddenDigits.java
+++ Java/src/main/java/Strings/LatestTimeByReplacingHiddenDigits/LatestTimeByReplacingHiddenDigits.java
@@ -2,28 +2,34 @@ package Strings.LatestTimeByReplacingHiddenDigits;
 
 public class LatestTimeByReplacingHiddenDigits {
     public static void main(String [] args) {
-        String [] input = {"2?:?0", "0?:3?", "0?:3?"};
+        String [] input = {"2?:?0", "0?:3?", "1?:22", "?0:15"};
         for (String i : input)
             System.out.println("Max Time:  " + maximumTime(i) + "\n");
     }
 
     public static String maximumTime(String time) {
+        StringBuilder sb = new StringBuilder(time);
 
-        if (time.charAt(0) == '?') {
+        if (sb.charAt(0) == '?') {
+            sb.replace(0, 1, "2");
         }
 
-        if (time.charAt(1) == '?') {
-
+        if (sb.charAt(1) == '?') {
+            if (sb.charAt(0) == '2') {
+                sb.replace(1, 2, "3");
+            } else {
+                sb.replace(1, 2, "9");
+            }
         }
 
-        if (time.charAt(3) == '?') {
-
+        if (sb.charAt(3) == '?') {
+            sb.replace(3, 4, "5");
         }
 
-        if (time.charAt(4) == '?') {
-
+        if (sb.charAt(4) == '?') {
+            sb.replace(4, 5, "9");
         }
 
-        return time;
+        return sb.toString();
     }
 }
