b541e908ccf2f31a9d5c1f84d5d732ad5d2e7832
Tim M <timothy.romero.manas@gmail.com>
2024-08-27 21:16:22 -0400

    Adding Sort the people

---
+++ Java/src/main/java/Lists/SortThePeople/SortThePeople.java
@@ -0,0 +1,17 @@
+package Lists.SortThePeople;
+
+import java.util.Arrays;
+
+public class SortThePeople {
+
+    public static void main(String [] args) {
+        String [] names = {"Mary","John","Emma"};
+        int [] heights = {180,165,170};
+        System.out.println("sortPeople: " + Arrays.toString(sortPeople(names, heights)));
+    }
+
+    public static String[] sortPeople(String[] names, int[] heights) {
+        
+
+    }
+}
