5972956b8e7068c62317eb1104a4c08d68c27e29
Tim M <timothy.romero.manas@gmail.com>
2024-08-27 21:46:30 -0400

    Finished Sort the people

--- Java/src/main/java/Lists/SortThePeople/SortThePeople.java
+++ Java/src/main/java/Lists/SortThePeople/SortThePeople.java
@@ -5,37 +5,45 @@ import java.util.*;
 public class SortThePeople {
 
     public static void main(String [] args) {
-        String [] names = {"Mary","John","Emma"};
-        int [] heights = {180,165,170};
-        System.out.println("sortPeople: " + Arrays.toString(sortPeople(names, heights)));
+//        String [] names = {"Mary","John","Emma"};
+//        int [] heights = {180,165,170};
+//        System.out.println("sortPeople: " + Arrays.toString(sortPeople(names, heights)));
+
+        String [] names1 = {"IEO","Sgizfdfrims","QTASHKQ","Vk","RPJOFYZUBFSIYp","EPCFFt","VOYGWWNCf","WSpmqvb"};
+        int [] heights1 = {17233,32521,14087,42738,46669,65662,43204,8224};
+        System.out.println("sortPeople: " + Arrays.toString(sortPeople(names1, heights1)));
     }
 
     public static String[] sortPeople(String[] names, int[] heights) {
-        String [] output = new String[3];
+        String [] output = new String[names.length];
 
-        Map<String, Integer> map = new TreeMap<String, Integer>();
-        for (int i=0; i<names.length; i++) map.put(names[i], heights[i]);
+        Map<Integer, String> map = new TreeMap<Integer, String>();
+        for (int i=0; i<names.length; i++) map.put(heights[i], names[i]);
 
         // Sort map by value
-        map = sortByValue(map);
+        map = sortByKey(map);
         System.out.println("sorted map: " + map);
 
         int index = 0;
-        for (String name : map.keySet())
+        for (String name : map.values())
             output[index++] = name;
 
 
         return output;
     }
 
-    public static Map<String, Integer> sortByValue(Map<String, Integer> map) {
-        List<Map.Entry<String, Integer>> list = new ArrayList<>(map.entrySet());
-        list.sort(Map.Entry.comparingByValue(Comparator.reverseOrder()));
+    public static Map<Integer, String> sortByKey(Map<Integer, String> map) {
+        List<Map.Entry<Integer, String>> list = new ArrayList<>(map.entrySet());
+        list.sort(Map.Entry.comparingByKey());
+        Collections.reverse(list);
 
-        Map<String, Integer> result = new LinkedHashMap<>();
-        for (Map.Entry<String, Integer> entry : list) {
+        Map<Integer, String> result = new LinkedHashMap<>();
+        for (Map.Entry<Integer, String> entry : list) {
             result.put(entry.getKey(), entry.getValue());
         }
         return result;
     }
+
+
+
 }
