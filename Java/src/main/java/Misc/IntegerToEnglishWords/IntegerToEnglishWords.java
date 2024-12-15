7593fc3fd25291bd611238a49b14479231caebe6
Tim M <timothy.romero.manas@gmail.com>
2024-12-15 17:38:52 -0500

    Int to Word

--- Java/src/main/java/Misc/IntegerToEnglishWords/IntegerToEnglishWords.java
+++ Java/src/main/java/Misc/IntegerToEnglishWords/IntegerToEnglishWords.java
@@ -5,8 +5,8 @@ package Misc.IntegerToEnglishWords;
 public class IntegerToEnglishWords {
 
     public static void main(String [] args) {
-        int [] input = {123, 12345, 1234567, 2147483647};
-        for (int i : input) System.out.println("Number to words: " + numberToWords(i) + "\n");
+        int [] input = {12345,  1001, 101, 0, 5, 12, 98, 100, 123, 1234567, 2147483647};
+        for (int i : input) System.out.println("Number to words: " + i + "      " + numberToWords(i) + "\n");
     }
 
     //2,147,483,647
@@ -15,142 +15,166 @@ public class IntegerToEnglishWords {
 
         // Divide Number into Equal Pieces
         if (num / 1000000000 > 0) {
-            int trillion = num / 1000000000;
+            int billion = num / 1000000000;
             num = num % 1000000000;
-            String strTrillions = getStringNumber(trillion);
-            System.out.println("trillion: " + trillion + "      " +"strTrillions: " + strTrillions);
-
+            String strBrillions = getStringNumber(billion);
+            System.out.println("billion: " + billion + "      " +"strBrillions: " + strBrillions);
+            sb.append(strBrillions + " Billion ");
 
             if (num / 1000000 > 0) {
                 int million = num / 1000000;
                 num = num % 1000000;
                 String strMillions = getStringNumber(million);
                 System.out.println("million: " + million + "      " + "strMillions: " + strMillions);
-
+                sb.append(strMillions + " Million ");
 
                 if (num / 1000 > 0) {
                     int thousands = num / 1000;
                     num = num % 1000;
                     String strThousands = getStringNumber(thousands);
                     System.out.println("thousands: " + thousands + "      " + "strHundreds: " + strThousands);
-
+                    sb.append(strThousands + " Thousand ");
 
                     if (num / 100 > 0) {
                         int hundreds = num / 100;
                         num = num % 100;
                         String strHundreds = getStringNumber(hundreds);
                         System.out.println("hundreds: " + hundreds + "      " + "strHundreds: " + strHundreds);
-
+                        sb.append(strHundreds + " Hundred ");
 
                         if (num / 10 > 0) {
-                            int doubles = num / 10;
-                            num = num % 10;
-                            String strDoubles = getStringNumber(doubles);
-                            String strSingles = getStringNumber(num);
-                            System.out.println("tens: " + doubles + "       " +"strDoubles: " + strDoubles);
-                            System.out.println("singles: " + num + "       "+ "strSingles: " + strSingles);
+                            String strDoubles = getStringNumber(num);
+                            System.out.println("tens: " + num + "       " +"strDoubles: " + strDoubles);
+                            sb.append(strDoubles);
                         }
                     }
                 }
             }
-
-            return sb.toString();
+            return sb.toString().trim();
         }
 
 
         if (num / 1000000 > 0) {
             int million = num / 1000000;
             num = num % 1000000;
-            System.out.println("million: " + million);
+            String strMillions = getStringNumber(million);
+            System.out.println("million: " + million + "      " + "strMillions: " + strMillions);
+            sb.append(strMillions + " Million ");
 
             if (num / 1000 > 0) {
                 int thousands = num / 1000;
                 num = num % 1000;
-                System.out.println("thousands: " + thousands);
+                String strThousands = getStringNumber(thousands);
+                System.out.println("thousands: " + thousands + "      " + "strHundreds: " + strThousands);
+                sb.append(strThousands + " Thousand ");
+
 
                 if (num / 100 > 0) {
                     int hundreds = num / 100;
                     num = num % 100;
-                    System.out.println("hundreds: " + hundreds);
+                    String strHundreds = getStringNumber(hundreds);
+                    System.out.println("hundreds: " + hundreds + "      " + "strHundreds: " + strHundreds);
+                    sb.append(strHundreds + " Hundred ");
 
                     if (num / 10 > 0) {
-                        int tens = num / 10;
-                        num = num % 10;
-                        System.out.println("tens: " + tens);
-                        System.out.println("singles: " + num);
+                        String strDoubles = getStringNumber(num);
+                        System.out.println("tens: " + num + "       " +"strDoubles: " + strDoubles);
+                        sb.append(strDoubles);
                     }
                 }
             }
-            return sb.toString();
+            return sb.toString().trim();
         }
 
         if (num / 1000 > 0) {
             int thousands = num / 1000;
             num = num % 1000;
-            System.out.println("thousands: " + thousands);
+            String strThousands = getStringNumber(thousands);
+            System.out.println("thousands: " + thousands + "      " + "strHundreds: " + strThousands);
+            sb.append(strThousands + " Thousand ");
+
 
             if (num / 100 > 0) {
                 int hundreds = num / 100;
                 num = num % 100;
-                System.out.println("hundreds: " + hundreds);
+                String strHundreds = getStringNumber(hundreds);
+                System.out.println("hundreds: " + hundreds + "      " + "strHundreds: " + strHundreds);
+                sb.append(strHundreds + " Hundred ");
 
                 if (num / 10 > 0) {
-                    int tens = num / 10;
-                    num = num % 10;
-                    System.out.println("tens: " + tens);
-                    System.out.println("singles: " + num);
+                    int doubles = num;
+                    String strDoubles = getStringNumber(doubles);
+                    System.out.println("tens: " + doubles + "       " +"strDoubles: " + strDoubles);
+                    sb.append(strDoubles);
                 }
+
+                return sb.toString().trim();
             }
-            return sb.toString();
+
+            if (num / 10 > 0) {
+                num = num % 10;
+                String strDoubles = getStringNumber(num);
+                System.out.println("tens: " + num + "       " +"strDoubles: " + strDoubles);
+                sb.append(strDoubles);
+                return sb.toString().trim();
+            }
+
+            if (num < 10 && num > 0) {
+                String strSingles = getStringNumber(num);
+                System.out.println("single: " + num + "       " +"strSingles: " + strSingles);
+                sb.append(strSingles);
+            }
+            return sb.toString().trim();
         }
 
         if (num / 100 > 0) {
             int hundreds = num / 100;
             num = num % 100;
-            System.out.println("hundreds: " + hundreds);
+            String strHundreds = getStringNumber(hundreds);
+            System.out.println("hundreds: " + hundreds + "      " + "strHundreds: " + strHundreds);
+            sb.append(strHundreds + " Hundred ");
 
             if (num / 10 > 0) {
-                int tens = num / 10;
-                num = num % 10;
-                System.out.println("tens: " + tens);
-                System.out.println("singles: " + num);
+                String strDoubles = getStringNumber(num);
+                System.out.println("tens: " + num + "       " +"strDoubles: " + strDoubles);
+                sb.append(strDoubles);
             }
-            return sb.toString();
+
+            if (num < 10 && num > 0) {
+                String strSingles = getStringNumber(num);
+                System.out.println("single: " + num + "       " +"strSingles: " + strSingles);
+                sb.append(strSingles);
+            }
+            return sb.toString().trim();
         }
 
         if (num / 10 > 0) {
-            int tens = num / 10;
-            num = num % 10;
-            System.out.println("tens: " + tens);
-            System.out.println("singles: " + num);
+            String strDoubles = getStringNumber(num);
+            System.out.println("tens: " + num + "       " +"strDoubles: " + strDoubles);
+            sb.append(strDoubles);
         }
 
-
-        return sb.toString();
+        if (num < 10) {
+            String strSingles = getStringNumber(num);
+            System.out.println("single: " + num + "       " +"strSingles: " + strSingles);
+            sb.append(strSingles);
+        }
+        return sb.toString().trim();
     }
 
     private static String getStringNumber(int num) {
-        System.out.println("    getStrNum: " + num);
+//        System.out.println("    getStrNum: " + num);
 
-        if (num / 100 > 0) {
-            return getStringNumber(num / 100) + getStringNumber(num % 100);
-        }
+        if (num == 90) return "Ninety";
+        if (num == 80) return "Eighty";
+        if (num == 70) return "Seventy";
+        if (num == 60) return "Sixty";
+        if (num == 50) return "Fifty";
+        if (num == 40) return "Forty";
+        if (num == 30) return "Thirty";
+        if (num == 20) return "Twenty";
+        if (num == 10) return "Ten";
 
-        if (num / 10 > 0) {
-            return getStringNumber(num / 10) + getStringNumber(num % 10);
-        }
-
-        if (num / 10 > 0) {
-            if (num / 90 == 1) return "Ninety";
-            if (num / 80 == 1) return "Eighty";
-            if (num / 70 == 1) return "Seventy";
-            if (num / 60 == 1) return "Sixty";
-            if (num / 50 == 1) return "Fifty";
-            if (num / 40 == 1) return "Fourty";
-            if (num / 30 == 1) return "Thirty";
-            if (num / 20 == 1) return "Twenty";
-            if (num / 10 == 1) return "Ten";
-        }
 
         if (num < 20) {
             if (num == 19) return "Nineteen";
@@ -162,7 +186,6 @@ public class IntegerToEnglishWords {
             if (num == 13) return "Thirteen";
             if (num == 12) return "Twelve";
             if (num == 11) return "Eleven";
-            if (num == 10) return "Ten";
         }
 
         if (num < 10) {
@@ -177,16 +200,33 @@ public class IntegerToEnglishWords {
             if (num == 1) return "One";
         }
 
+        if (num / 100 > 0) {
+            return getStringNumber(num / 100) + " Hundred "+ getStringNumber(num % 100);
+        }
 
+        if (num / 10 > 0) {
+            String digit = "";
+            int remainder = num % 10;
+            num = num / 10;
+                if (num == 9)
+                    digit = "Ninety ";
+                if (num == 8)
+                    digit = "Eighty ";
+                if (num == 7)
+                    digit = "Seventy ";
+                if (num == 6)
+                    digit = "Sixty ";
+                if (num == 5)
+                    digit = "Fifty ";
+                if (num == 4)
+                    digit = "Forty ";
+                if (num == 3)
+                    digit = "Thirty ";
+                if (num == 2)
+                    digit = "Twenty ";
 
-
-
-
-
-
-
-
-
+            return digit + getStringNumber(remainder % 10);
+        }
 
 
         return "Zero";
