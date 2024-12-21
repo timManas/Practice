81b118ec267df8294230206d65eba0269a89fbf6
Tim M <timothy.romero.manas@gmail.com>
2024-12-20 21:50:12 -0500

    Finished Merge Intervals

--- Java/src/main/java/Misc/MergeIntervals/MergeIntervals.java
+++ Java/src/main/java/Misc/MergeIntervals/MergeIntervals.java
@@ -3,9 +3,14 @@ package Misc.MergeIntervals;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Stack;
 
 public class MergeIntervals {
     public static void main(String [] args) {
+
+        int [][] intervals2 = {{2,3},{4,5},{6,7},{8,9},{1,10}};
+        System.out.println("Merged:" + Arrays.toString(merge(intervals2)) + "\n");
+
         int [][] intervals = {{1,3},{2,6},{8,10},{15,18}};
         System.out.println("Merged:" + Arrays.toString(merge(intervals)) + "\n");
 
@@ -14,41 +19,57 @@ public class MergeIntervals {
     }
 
     public static int[][] merge(int[][] intervals) {
-        List<int []> output = new ArrayList<>();
+        // Step 1 - Sort Array by first
+        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
 
-        int[] current;
-        int [] prev;
+        // Step2 - Create Stack
+        Stack<int []> stack = new Stack<>();
+        stack.push(intervals[0]);
+
+        // Step3 - Travere Intervals
         for (int i=1; i<intervals.length; i++) {
-            current = intervals[i];
-            prev = intervals[i-1];
+            int [] current = intervals[i];
+            int [] prev = stack.pop();
             System.out.println("Prev: " + Arrays.toString(prev) + " | Current: " + Arrays.toString(current));
 
-            boolean isOverlap = false;
-            if (prev[0] <= current[0] && current[0] <= prev[1]) {
-                isOverlap = true;
-            }
-
-            if (current[0] <= prev[1] && prev[1] <= current[1]) {
-                isOverlap = true;
-            }
-
-            // No OverLap with Continue
-            if (!isOverlap) {
-                output.add(current);
+            // If No OverLap with Continue
+            if (!isOverlap(prev, current)) {
+                stack.push(prev);
+                stack.push(current);
                 continue;
             }
 
-
+            // Create New Interval and put it in Stack
             int [] newInterval = {Math.min(prev[0], current[0]), Math.max(prev[1], current[1])};
-            output.add(newInterval);
+            stack.push(newInterval);
             System.out.println("    Overlapped: " + Arrays.toString(newInterval));
         }
 
-        // Conver to int[][]
-        int [][] result = new int[output.size()][];
-        for (int i=0; i<output.size(); i++)
-            result[i] = output.get(i);
+        // Step4 - Convert to int[][]
+        int [][] result = new int[stack.size()][];
+        for (int i=0; stack.size() > 0; i++) {
+            result[i] = stack.pop();
+            System.out.println(Arrays.toString(result[i]));
+        }
 
         return result;
+    }
+
+    private static boolean isOverlap(int[] prev, int[] current) {
+
+        if (prev[0] <= current[0] && current[0] <= prev[1]) {
+            return true;
+        }
+        if (current[0] <= prev[1] && prev[1] <= current[1]) {
+            return true;
+        }
+        if (prev[0] <= current[1] && current[1] <= prev[1]) {
+            return true;
+        }
+        if (current[0] <= prev[0] && prev[0] <= current[1]) {
+            return true;
+        }
+
+        return false;
     }
 }
