a382e8dfcaf15ab7f0be381c9fc74df738339cba
Tim M <timothy.romero.manas@gmail.com>
2024-12-22 11:31:38 -0500

    Adding Min Number Sub Array from Target

---
+++ Java/src/main/java/Lists/MinNumIncrSubArrFormTarget/MinNumIncrSubArrFormTarget.java
@@ -0,0 +1,21 @@
+package Lists.MinNumIncrSubArrFormTarget;
+
+public class MinNumIncrSubArrFormTarget {
+    public static void main(String [] args) {
+        int [] target = {1,2,3,2,1};
+        System.out.println("Min Number: " + minNumberOperations(target) + "\n");
+
+        int [] target1 = {3,1,1,2};
+        System.out.println("Min Number: " + minNumberOperations(target1) + "\n");
+
+        int [] target2 = {3,1,5,4,2};
+        System.out.println("Min Number: " + minNumberOperations(target2) + "\n");
+    }
+
+    public static int minNumberOperations(int[] target) {
+        int output = 0;
+
+
+        return output;
+    }
+}
