1708a98b8b881471ab917afe90a8ee7551dcabd4
Timothy Manas <timothy.romero.manas@gmail.com>
2026-07-30 23:58:35 -0400

    Adding ReverseString

---
+++ Java/src/main/java/Strings/ReverseStringII/ReverseStringII.java
@@ -0,0 +1,56 @@
+package Strings.ReverseStringII;
+
+public class ReverseStringII {
+    public static void main (String [] args) {
+        String s = "abcdefg";
+        int k = 2;
+        System.out.println("ReverseString: " + reverseStr(s, k) + "\n");
+
+        String s1 = "abcd";
+        int k1 = 2;
+        System.out.println("ReverseString: " + reverseStr(s1, k1) + "\n");
+
+        String s2 = "abcdefg";
+        int k2 = 3;
+        System.out.println("ReverseString: " + reverseStr(s2, k2) + "\n");
+
+        String s3 = "abcdefg";
+        int k3 = 8;
+        System.out.println("ReverseString: " + reverseStr(s3, k3) + "\n");
+
+    }
+
+    public static String reverseStr(String s, int k) {
+        StringBuilder strBldr = new StringBuilder();
+        System.out.println("orig: " + s);
+
+        if (s.length() < k)
+            return new StringBuilder(s).reverse().toString();
+
+        int i = 0;
+        while (i < s.length()) {
+            int e = i==0 ? k*2 : i * 2 ;
+            System.out.println("index: " + i + "    end: " + e);
+
+            if (e > s.length())
+                e = s.length();
+
+            String subStr = s.substring(i, e);
+            System.out.println("subStr: " + subStr);
+
+            int f = subStr.length() > k ? k : subStr.length();
+            String subsubStr = subStr.substring(0, f);
+
+            String reverseStr = new StringBuilder(subsubStr).reverse() + subStr.substring(f, subStr.length());
+            System.out.println("reversStr:  " + reverseStr);
+
+            strBldr.append(reverseStr);
+
+
+            i = i==0 ? k*2 : i*2;
+        }
+
+        return strBldr.toString();
+    }
+
+}
