5d41e27d5989b3db77207e0a1ca45a7e4ae91005
Timothy Manas <timothy.romero.manas@gmail.com>
2026-08-03 12:09:51 -0400

    Finished Shortest Completing Word

--- Java/src/main/java/Strings/ShortestCompletingWord/ShortestCompletingWord.java
+++ Java/src/main/java/Strings/ShortestCompletingWord/ShortestCompletingWord.java
@@ -14,24 +14,47 @@ public class ShortestCompletingWord {
         String licensePlate1 = "1s3 456";
         String [] words1 = {"looks","pest","stew","show"};
         System.out.println("Shortest Completing Word: " + shortestCompletingWord(licensePlate1, words1) + "\n");
+
+        String licensePlate2 = "Ah71752";
+        String [] words2 = {"suggest","letter","of","husband","easy","education","drug","prevent","writer","old"};
+        System.out.println("Shortest Completing Word: " + shortestCompletingWord(licensePlate2, words2) + "\n");
     }
 
     public static String shortestCompletingWord(String licensePlate, String[] words) {
         System.out.println("licensePlate: " + licensePlate);
         String licensePlateWordSorted = sortWord(licensePlate);
+        List<String> validStr = new ArrayList<>();
 
-        int shortestLength = words[0].length();
-        String shortestWord = words[0];
+        int shortestLength = Integer.MAX_VALUE;
+        String shortestWord = "";
         for (String word:words) {
             System.out.println("word: " + word);
-            String
+            StringBuilder sb = new StringBuilder(licensePlateWordSorted);
+
+            if (word.length() < licensePlateWordSorted.length())
+                continue;
+
+            for (char letter : word.toCharArray()) {
+                String letterStr = String.valueOf(letter);
+                if (sb.indexOf(letterStr) == -1)
+                    continue;
+                sb.deleteCharAt(sb.indexOf(letterStr));
+            }
+
+            if (sb.length() != 0)
+                continue;
 
 
-
+            validStr.add(word);
+            if (shortestLength > word.length()) {
+                shortestLength = word.length();
+                shortestWord = word;
+            }
         }
+        System.out.println("Valid Str: " + validStr);
+        System.out.println("shortestWord: " + shortestWord);
 
-
-        return null;
+        return shortestWord;
     }
 
     public static String sortWord(String input) {
