d56bba54b7a0cb1b5d2cf36d4bab87d169e3f447
Timothy Manas <timothy.romero.manas@gmail.com>
2026-09-21 18:55:27 -0400

    Finished max product two digits

---
+++ Java/src/main/java/Math/MaxProductTwoDigits/MaxProductTwoDigits.java
@@ -0,0 +1,21 @@
+package Math.MaxProductTwoDigits;
+
+import java.util.Arrays;
+
+public class MaxProductTwoDigits {
+    public static void main(String [] args) {
+        int [] input = {31, 22, 124};
+        for (int i : input)
+            System.out.println("maxProduct: " + maxProduct(i) + "\n");
+    }
+
+    public static int maxProduct(int n) {
+        int max = 0;
+        String [] digitArr = String.valueOf(n).split("");
+        Arrays.sort(digitArr);
+
+
+
+        return Integer.valueOf(digitArr[digitArr.length-1]) * Integer.valueOf(digitArr[digitArr.length-2]);
+    }
+}
---
+++ Python/project/src/Lists/ShortestDistanceTargetStrCircArray/ShortestDistanceTargetStrCircArray.py
@@ -0,0 +1,23 @@
+from numbers import Number
+
+
+def main():
+
+    solution = Solution()
+    words = ["hello", "i", "am", "leetcode", "hello"]
+    target = "hello"
+    startIndex = 1
+    print("Closest target: ", solution.closestTarget(words, target, startIndex), "\n")
+    return
+
+
+class Solution:
+    def closestTarget(self, words: list[str], target: str, startIndex: int) -> int:
+
+
+        int diff =
+
+        return 0
+
+if __name__ == '__main__':
+    main()
