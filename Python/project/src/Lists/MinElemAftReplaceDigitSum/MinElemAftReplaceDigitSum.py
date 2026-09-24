973ad0226e115c50cc0b158aa8e5eb9d018c8646
Timothy Manas <timothy.romero.manas@gmail.com>
2026-09-24 19:30:02 -0400

    Finished Min Element At Replacement

---
+++ Python/project/src/Lists/MinElemAftReplaceDigitSum/MinElemAftReplaceDigitSum.py
@@ -0,0 +1,26 @@
+from ast import List
+
+
+def main():
+    solution = Solution()
+    input = [[10,12,13,14], [1,2,3,4], [999,19,199]]
+    for num in input:
+        print("minElement: ", solution.minElement(num), "\n")
+    return
+
+class Solution:
+    def minElement(self, nums: List[int]) -> int:
+        minElement = 200
+        for i, num in enumerate(nums):
+
+            sum = 0
+            for digit in str(num):
+                sum += int(digit)
+
+            minElement = min(sum, minElement)
+
+
+        return minElement
+
+if __name__ == "__main__":
+    main()
